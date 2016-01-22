f850ae43bea709877fc83c02f1f763aaf5b5bea3
Sander Van Dooren <sander.vandooren@one-agency.be>
2016-01-22 08:59:37 +0100

    Revert "Issue #ISAICP-2207: Perhaps with some update magic?"
    This reverts commit a79cda3d012b4724819152df455304783a2617e7.

--- web/modules/custom/rdf_entity/src/Database/Driver/sparql/Connection.php
+++ web/modules/custom/rdf_entity/src/Database/Driver/sparql/Connection.php
@@ -59,7 +59,7 @@ class Connection {
   /**
    * Execute the actual query against the Sparql endpoint.
    */
-  public function query($query, $type = 'query') {
+  public function query($query) {
     if (!empty($this->logger)) {
       // @todo Fix this. Logger should have been auto started.
       // Probably related to the overwritten log object in $this->setLogger.
@@ -69,12 +69,7 @@ class Connection {
       $query_start = microtime(TRUE);
     }
 
-    if ($type == 'query') {
-      $results = $this->doQuery($query);
-    }
-    else {
-      $results = $this->doUpdate($query);
-    }
+    $results = $this->connection->query($query);
 
     if (!empty($this->logger)) {
       $query_end = microtime(TRUE);
@@ -207,25 +202,6 @@ class Connection {
   public function getConnectionOptions() {
     return $this->connectionOptions;
   }
-
-  /**
-   * @param $query
-   * @return object
-   */
-  public function doQuery($query) {
-    $results = $this->connection->query($query);
-    return $results;
-  }
-
-  /**
-   * @param $query
-   * @return object
-   */
-  public function doUpdate($query) {
-    $results = $this->connection->update($query);
-    return $results;
-  }
-
 
 }
 
