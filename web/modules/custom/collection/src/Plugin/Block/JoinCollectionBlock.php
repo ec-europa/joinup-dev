b47a78e65c0fa0f1fb303f5f92fb4d4ead2ecf3c
Pieter Frenssen <pieter@frenssen.be>
2016-01-29 10:22:01 +0200

    ISAICP-2196: Create a basic block that shows the Join Collection form.

---
+++ web/modules/custom/collection/src/Plugin/Block/JoinCollectionBlock.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\collection\Plugin\Block\JoinCollectionBlock.
+ */
+
+namespace Drupal\collection\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a block containing a form that allows to join a collection.
+ *
+ * @Block(
+ *  id = "join_collection_block",
+ *  admin_label = @Translation("Join collection block"),
+ * )
+ */
+class JoinCollectionBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current route match service.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface,
+   */
+  protected $currentRouteMatch;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface $user
+   */
+  protected $user;
+
+  /**
+   * Constructs a JoinCollectionBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $current_route_match.
+   *   The current route match service.
+   * @param \Drupal\Core\Session\AccountProxyInterface $user
+   *   The current user.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $current_route_match, AccountProxyInterface $user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentRouteMatch = $current_route_match;
+    $this->user = $user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Retrieve the collection from the route.
+    $collection = $this->currentRouteMatch->getParameter('collection');
+    if (empty($collection)) {
+      throw new \Exception('The "Join Collection" block can only be shown on collection pages.');
+    }
+
+    // Display the Join Collection form.
+    return \Drupal::formBuilder()->getForm('\Drupal\collection\Form\JoinCollectionForm', $this->user, $collection);
+  }
+
+}
