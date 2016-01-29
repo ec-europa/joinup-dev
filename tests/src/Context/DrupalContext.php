2b24956a6196ae711e418a2fd162532068e2b6f6
Pieter Frenssen <pieter@frenssen.be>
2016-01-29 14:22:40 +0200

    ISAICP-2196: Check for the presence of a class on the body element to determine whether a user is logged in.

---
+++ tests/src/Context/DrupalContext.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\joinup\Context\DrupalContext.
+ */
+
+namespace Drupal\joinup\Context;
+
+use Drupal\DrupalExtension\Context\DrupalContext as DrupalExtensionDrupalContext;
+
+/**
+ * Provides step definitions for interacting with Drupal.
+ */
+class DrupalContext extends DrupalExtensionDrupalContext {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loggedIn() {
+    $session = $this->getSession();
+    $session->visit($this->locatePath('/'));
+
+    // Check if the 'logged-in' class is present on the page.
+    $element = $session->getPage();
+    return $element->find('css', 'body.user-logged-in');
+  }
+
+}
