b0ab204a0d3200cf80a1e91ce53dbea5946131ac
Pieter Frenssen <pieter@frenssen.be>
2016-02-10 14:01:58 +0100

    ISAICP-2191: Create initial version of the Add Content block.

---
+++ web/profiles/joinup/src/Plugin/Block/AddContentBlock.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\joinup\Plugin\Block\AddContentBlock.
+ */
+
+namespace Drupal\joinup\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides an 'AddContentBlock' block.
+ *
+ * @Block(
+ *  id = "add_content_block",
+ *  admin_label = @Translation("Add content"),
+ * )
+ */
+class AddContentBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['collection'] = [
+      '#type' => 'link',
+      '#title' => $this->t('Add collection'),
+      '#url' => Url::fromRoute('entity.collection.propose_form'),
+      '#attributes' => ['class' => ['button', 'button--small']],
+    ];
+
+    return $build;
+  }
+
+}
