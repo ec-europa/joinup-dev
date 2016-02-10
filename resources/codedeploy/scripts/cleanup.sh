90b47b0e3c2bea7228908e5418a9f090861175a5
Frederic Dewinne <frederic@continuousphp.com>
2016-02-10 14:10:07 +0100

    add deployment scripts

---
+++ resources/codedeploy/scripts/cleanup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -Rf /var/www/html/*
---
+++ resources/codedeploy/scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /var/www/html
+
+while [ ! -f /usr/local/etc/subsite/subsite.ini ]
+do
+  sleep 2
+done
+
+bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install 2>&1 >> /var/log/subsite/install.log
+chown -R www-data:www-data /var/www/html/*
