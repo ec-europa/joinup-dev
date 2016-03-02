1ef54263544491c04238e11ec42afd0044e63dee
Pieter Frenssen <pieter@frenssen.be>
2016-03-02 14:35:40 +0100

    ISAICP-2191: Add a controller to propose Rdf entities.

---
+++ web/profiles/joinup/src/Controller/JoinupController.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\joinup\Controller\JoinupController.
+ */
+
+namespace Drupal\joinup\Controller;
+
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\rdf_entity\RdfEntityTypeInterface;
+
+/**
+ * Provides route responses for Joinup.
+ */
+class JoinupController extends ControllerBase {
+
+  /**
+   * Provides propose forms for various RDF entities.
+   *
+   * This is used for the propose form of collections, and will be used for
+   * interoperability solutions in the future.
+   *
+   * @param \Drupal\rdf_entity\RdfEntityTypeInterface $rdf_type
+   *   The RDF bundle entity for which to generate the propose form.
+   *
+   * @return array
+   *   A render array for the propose form.
+   */
+  public function proposeRdfEntity(RdfEntityTypeInterface $rdf_type) {
+    $rdf_entity = $this->entityTypeManager()->getStorage('rdf_entity')->create(array(
+      'rid' => $rdf_type->id(),
+    ));
+
+    $form = $this->entityFormBuilder()->getForm($rdf_entity, 'propose');
+    $form['#title'] = $this->t('<em>Propose @type</em>', [
+      '@type' => Unicode::strtolower($rdf_type->label()),
+    ]);
+    return $form;
+  }
+
+}
