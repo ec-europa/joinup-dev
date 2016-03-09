ad961a0adb99b1aab55719bd89440fc177b3fc9e
Ilias Dimopoulos <idimopoulos@hotmail.com>
2016-03-09 14:04:10 +0100

    ISAICP-2237: Add button to 'AddContentBlock' and setup the cache for each page.

--- web/profiles/joinup/src/Plugin/Block/AddContentBlock.php
+++ web/profiles/joinup/src/Plugin/Block/AddContentBlock.php
@@ -8,6 +8,7 @@
 namespace Drupal\joinup\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Url;
 
 /**
@@ -18,12 +19,52 @@ use Drupal\Core\Url;
  *  admin_label = @Translation("Add content"),
  * )
  */
-class AddContentBlock extends BlockBase {
+class AddContentBlock extends BlockBase
+{
+
+  /**
+   * The collection to join.
+   *
+   * @var \Drupal\rdf_entity\RdfInterface
+   */
+  protected $collection;
+
+  /**
+   * The current route match service.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $currentRouteMatch;
+
+  /**
+   * Constructs a JoinCollectionBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $current_route_match
+   *   The current route match service.
+   * @param \Drupal\Core\Session\AccountProxyInterface $user
+   *   The current user.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition)
+  {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentRouteMatch = \Drupal::routeMatch();
+
+    // @todo: This should be restricted to collection rdf_entities only.
+    // Retrieve the collection from the route.
+    $this->collection = $this->currentRouteMatch->getParameter('rdf_entity');
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function build() {
+  public function build()
+  {
     $build = [
       'collection' => [
         '#type' => 'link',
@@ -33,7 +74,26 @@ class AddContentBlock extends BlockBase {
       ],
     ];
 
+    if (!empty($this->collection) && $this->collection->bundle() == 'collection') {
+      $build['custom_page'] = [
+        '#type' => 'link',
+        '#title' => $this->t('Add custom page'),
+        '#url' => Url::fromRoute('collection.custom_page.add',
+          ['rdf_entity' => $this->currentRouteMatch->getRawParameter('rdf_entity')]),
+        '#attributes' => ['class' => ['button', 'button--small']],
+      ];
+    }
+
     return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    // This block varies per user.
+    $contexts = parent::getCacheContexts();
+    return Cache::mergeContexts($contexts, ['url']);
   }
 
 }
