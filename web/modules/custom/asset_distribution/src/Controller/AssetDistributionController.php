20145f81f0b4a8bfdc173606bb201101dbbdd8c2
Ilias Dimopoulos <idimopoulos@hotmail.com>
2016-04-05 01:35:43 +0200

    ISAICP-2401-dev: Revert changes to the controller.

--- web/modules/custom/asset_distribution/src/Controller/AssetDistributionController.php
+++ web/modules/custom/asset_distribution/src/Controller/AssetDistributionController.php
@@ -17,6 +17,28 @@ use Drupal\rdf_entity\RdfInterface;
 class AssetDistributionController extends ControllerBase {
 
   /**
+   * Controller for the base form.
+   *
+   * We need to override the functionality of the create form for pages
+   * that include the rdf_entity id in the url so that the the solution refers
+   * to this asset distribution.
+   *
+   * @param \Drupal\rdf_entity\RdfInterface $rdf_entity
+   *   The solution rdf_entity.
+   *
+   * @return array
+   *   Return the form array to be rendered.
+   */
+  public function add(RdfInterface $rdf_entity) {
+    $rdf_entity = $this->entityTypeManager()->getStorage('rdf_entity')->create(array(
+      'rid' => 'asset_distribution',
+    ));
+    /** @var \Drupal\Core\Form\FormBuilderInterface $form_builder */
+    $form = $this->entityFormBuilder()->getForm($rdf_entity);
+    return $form;
+  }
+
+  /**
    * Handles access to the distribution add form through solution pages.
    *
    * @param \Drupal\rdf_entity\RdfInterface $rdf_entity
@@ -25,13 +47,15 @@ class AssetDistributionController extends ControllerBase {
    * @return \Drupal\Core\Access\AccessResult
    *   The access result object.
    */
-  public function createAssetRepositoryAccess(RdfInterface $rdf_entity) {
+  public function createAssetDistributionAccess(RdfInterface $rdf_entity) {
     // Check that the passed in RDF entity is a collection, and that the user
     // has the permission to create custom pages.
     // @todo Collection owners and facilitators should also have the right to
     //   create custom pages for the collections they manage.
     // @see https://webgate.ec.europa.eu/CITnet/jira/browse/ISAICP-2450
-    if ($rdf_entity->bundle() == 'asset' && $this->currentUser()->hasPermission('create custom collection page')) {
+    if ($rdf_entity->bundle() == 'solution' && $this->currentUser()
+        ->hasPermission('propose asset_distribution rdf entity')
+    ) {
       return AccessResult::allowed();
     }
 
