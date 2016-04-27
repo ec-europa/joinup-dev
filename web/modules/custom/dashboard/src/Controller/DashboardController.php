f71c62b131248c26fbb451b94c07d21e28946d76
Pieter Frenssen <pieter@frenssen.be>
2016-04-27 12:42:06 +0200

    ISAICP-2494: Add documentation.

--- web/modules/custom/dashboard/src/Controller/DashboardController.php
+++ web/modules/custom/dashboard/src/Controller/DashboardController.php
@@ -4,6 +4,9 @@ namespace Drupal\dashboard\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Page controllers for the Dashboard module.
+ */
 class DashboardController extends ControllerBase {
 
   /**
