7f2a082d6e2d7d8368f67dca52acccea781b4a4d
tom@waat <tom@waat.eu>
2016-06-23 00:04:29 +0200

    ISAICP-2579: Create slider select component

--- web/themes/joinup/js/script.js
+++ web/themes/joinup/js/script.js
@@ -4,5 +4,20 @@
  */
 
  jQuery(document).ready(function ($) {
-  $('#edit-delete').addClass('button--default button--blue mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent');
+   $('#edit-delete').addClass('button--default button--blue mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent');
+
+   $.each($('.slider__select'), function (index, value) {
+     var select = $(this);
+     var selectLength = $(this).find('option').length;
+
+     var slider = $("<div id='slider' class='slider__slider'></div>").insertAfter(select).slider({
+        min: 1,
+        max: selectLength,
+        range: "min",
+        value: select[ 0 ].selectedIndex + 1,
+        slide: function (event, ui) {
+          select[ 0 ].selectedIndex = ui.value - 1;
+        }
+      });
+   });
  });
