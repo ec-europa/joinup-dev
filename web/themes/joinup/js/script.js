bcc363f916bd720e17d4ca615c3c738e174adc40
tom@waat <tom@waat.eu>
2016-08-02 11:35:10 +0200

    ISAICP-2579: Add slider in Owner field

--- web/themes/joinup/js/script.js
+++ web/themes/joinup/js/script.js
@@ -3,21 +3,39 @@
  * Joinup theme scripts.
  */
 
- jQuery(document).ready(function ($) {
-   $('#edit-delete').addClass('button--default button--blue mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent');
-
-   $.each($('.slider__select'), function (index, value) {
-     var select = $(this);
-     var selectLength = $(this).find('option').length;
-
-     var slider = $("<div id='slider' class='slider__slider'></div>").insertAfter(select).slider({
-        min: 1,
-        max: selectLength,
-        range: "min",
-        value: select[ 0 ].selectedIndex + 1,
-        slide: function (event, ui) {
-          select[ 0 ].selectedIndex = ui.value - 1;
-        }
+(function ($) {
+  Drupal.behaviors.deleteButton = {
+    attach: function (context, settings) {
+      $(context).find('#edit-delete').once('deleteButton').each(function () {
+        $(this).addClass('button--default button--blue mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent');
       });
-   });
- });
+    }
+  };
+
+  Drupal.behaviors.silderSelect = {
+    attach: function (context, settings) {
+      $(context).find('.slider__select').once('sliderSelect').each(function () {
+        var select = $(this);
+        var selectLength = $(this).find('option').length;
+
+        var slider = $("<div id='slider' class='slider__slider'></div>").insertAfter(select).slider({
+          min: 1,
+          max: selectLength,
+          range: "min",
+          value: select[ 0 ].selectedIndex + 1,
+          slide: function (event, ui) {
+            select[ 0 ].selectedIndex = ui.value - 1;
+          }
+          });
+      });
+    }
+  };
+
+  Drupal.behaviors.alterTableDrag = {
+    attach: function (context, settings) {
+      $(context).find('.tabledrag-handle').once('alterTableDrag').each(function () {
+        $(this).addClass('draggable__icon icon icon--draggable');
+      });
+    }
+  };
+})(jQuery);
