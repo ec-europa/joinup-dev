6d2cedfa578621ec128b364dd45b541b7eba6afe
luke@waat <luke@waat.eu>
2016-08-11 20:30:35 +0200

    ISAICP-2600: about/events/overview prototypes

---
+++ web/themes/joinup/prototype/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * Joinup theme scripts.
+ */
+
+(function ($) {
+  $('.filter__dropdown-toggle').click(function() {
+    $(this).toggleClass('is-active');
+    $(this).siblings('.filter__dropdown').toggleClass('is-active');
+  });
+})(jQuery);
