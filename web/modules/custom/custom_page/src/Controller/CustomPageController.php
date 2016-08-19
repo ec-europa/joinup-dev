5d8ecee028090701416905f989bf653b9cc68799
Pieter Frenssen <pieter@frenssen.be>
2016-08-19 09:57:09 +0200

    ISAICP-2369: Improve the title on the edit form for the navigation menu.

--- web/modules/custom/custom_page/src/Controller/CustomPageController.php
+++ web/modules/custom/custom_page/src/Controller/CustomPageController.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\custom_page\Controller;
 
+use Drupal\Component\Utility\Xss;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\og\OgAccessInterface;
 use Drupal\og\OgGroupAudienceHelper;
+use Drupal\og_menu\OgMenuInstanceInterface;
 use Drupal\rdf_entity\RdfInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -103,6 +105,28 @@ class CustomPageController extends ControllerBase {
       'type' => 'custom_page',
       OgGroupAudienceHelper::DEFAULT_FIELD => $rdf_entity->id(),
     ]);
+  }
+
+  /**
+   * Altered title callback for the navigation menu edit form.
+   *
+   * @param \Drupal\og_menu\OgMenuInstanceInterface $ogmenu_instance
+   *   The OG Menu instance that is being edited.
+   *
+   * @return array
+   *   The title as a render array.
+   *
+   * @see \Drupal\custom_page\Routing\RouteSubscriber::alterRoutes()
+   */
+  public function editFormTitle(OgMenuInstanceInterface $ogmenu_instance) {
+    // Provide a custom title for the OG Menu instance edit form. The default
+    // menu is suitable for webmasters, but we need a simpler title since this
+    // form is exposed to regular visitors.
+    $group = $ogmenu_instance->og_audience->entity;
+    return ['#markup' => t('Edit navigation menu of the %group @type', [
+      '%group' =>$ogmenu_instance->label(),
+      '@type' => $group->bundle(),
+    ]), '#allowed_tags' => Xss::getHtmlTagList()];
   }
 
 }
