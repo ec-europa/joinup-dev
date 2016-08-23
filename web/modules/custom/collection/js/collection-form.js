18da94610f9b33511c917bde3b226e6a6e6591ac
motatma <catalin@catalin.com>
2016-08-23 14:32:33 +0200

    ISAICP-2633 adapt javascript for new eLibrary collection select values

--- web/modules/custom/collection/js/collection-form.js
+++ web/modules/custom/collection/js/collection-form.js
@@ -24,11 +24,11 @@
             // If the collection is closed only options 0 and 1 should be
             // available.
             // Disabling option 2 if it exists.
-            var option2 = $('option[value="2"]', '#edit-field-ar-elibrary-creation');
+            var option2 = $('option[value="registered users"]', '#edit-field-ar-elibrary-creation');
             var optionText = option2.text();
             // Select option 1 if option 2 is selected.
             if (option2.is(':selected')) {
-              $('option[value="1"]', '#edit-field-ar-elibrary-creation').attr('selected', 'selected');
+              $('option[value="members"]', '#edit-field-ar-elibrary-creation').attr('selected', 'selected');
             }
             option2.attr('selected', false);
             option = option2.remove();
@@ -40,11 +40,11 @@
             // If the collection is opened only options 1 and 2 should be
             // available.
             // Disabling option 0 if it exists.
-            var option0 = $('option[value="0"]', '#edit-field-ar-elibrary-creation');
+            var option0 = $('option[value="facilitators"]', '#edit-field-ar-elibrary-creation');
             var optionText = option0.text();
             // Select option 1 if option 0 is selected.
             if (option0.is(':selected')) {
-              $('option[value="1"]', '#edit-field-ar-elibrary-creation').attr('selected', 'selected');
+              $('option[value="members"]', '#edit-field-ar-elibrary-creation').attr('selected', 'selected');
             }
             option0.attr('selected', false);
             option = option0.remove();
