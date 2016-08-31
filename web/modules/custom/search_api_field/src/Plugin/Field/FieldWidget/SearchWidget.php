a0a580fafe460a1ff8f917524e4bb3b422eb3504
Francesco Sardara <francesco.sardara@kanooh.be>
2016-08-31 16:40:17 +0200

    Issue #ISAICP-2736: remove some inused code and inject entity type manager service

--- web/modules/custom/search_api_field/src/Plugin/Field/FieldWidget/SearchWidget.php
+++ web/modules/custom/search_api_field/src/Plugin/Field/FieldWidget/SearchWidget.php
@@ -21,138 +21,49 @@ use Drupal\search_api_field\Plugin\Field\FieldType\SearchItem;
  *   }
  * )
  */
-class SearchWidget extends WidgetBase {
+class SearchWidget extends WidgetBase implements ContainerFactoryPluginInterface {
 
   /**
-   * Gets the URI without the 'internal:' or 'entity:' scheme.
+   * The entity type manager.
    *
-   * The following two forms of URIs are transformed:
-   * - 'entity:' URIs: to entity autocomplete ("label (entity id)") strings;
-   * - 'internal:' URIs: the scheme is stripped.
-   *
-   * This method is the inverse of ::getUserEnteredStringAsUri().
-   *
-   * @param string $uri
-   *   The URI to get the displayable string for.
-   *
-   * @return string
-   *   The displayable string.
-   *
-   * @see static::getUserEnteredStringAsUri()
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected static function getUriAsDisplayableString($uri) {
-    $scheme = parse_url($uri, PHP_URL_SCHEME);
+  protected $entityTypeManager;
 
-    // By default, the displayable string is the URI.
-    $displayable_string = $uri;
+  /**
+   * Constructs a SearchWidget object.
+   *
+   * @param array $plugin_id
+   *   The plugin_id for the widget.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the widget is associated.
+   * @param array $settings
+   *   The widget settings.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
 
-    // A different displayable string may be chosen in case of the 'internal:'
-    // or 'entity:' built-in schemes.
-    if ($scheme === 'internal') {
-      $uri_reference = explode(':', $uri, 2)[1];
-
-      // @todo '<front>' is valid input for BC reasons, may be removed by
-      //   https://www.drupal.org/node/2421941
-      $path = parse_url($uri, PHP_URL_PATH);
-      if ($path === '/') {
-        $uri_reference = '<front>' . substr($uri_reference, 1);
-      }
-
-      $displayable_string = $uri_reference;
-    }
-    elseif ($scheme === 'entity') {
-      list($entity_type, $entity_id) = explode('/', substr($uri, 7), 2);
-      // Show the 'entity:' URI as the entity autocomplete would.
-      $entity_manager = \Drupal::entityManager();
-      if ($entity_manager->getDefinition($entity_type, FALSE) && $entity = \Drupal::entityManager()
-          ->getStorage($entity_type)
-          ->load($entity_id)
-      ) {
-        $displayable_string = EntityAutocomplete::getEntityLabels(array($entity));
-      }
-    }
-
-    return $displayable_string;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
-   * Gets the user-entered string as a URI.
-   *
-   * The following two forms of input are mapped to URIs:
-   * - entity autocomplete ("label (entity id)") strings: to 'entity:' URIs;
-   * - strings without a detectable scheme: to 'internal:' URIs.
-   *
-   * This method is the inverse of ::getUriAsDisplayableString().
-   *
-   * @param string $string
-   *   The user-entered string.
-   *
-   * @return string
-   *   The URI, if a non-empty $uri was passed.
-   *
-   * @see static::getUriAsDisplayableString()
+   * {@inheritdoc}
    */
-  protected static function getUserEnteredStringAsUri($string) {
-    // By default, assume the entered string is an URI.
-    $uri = $string;
-
-    // Detect entity autocomplete string, map to 'entity:' URI.
-    $entity_id = EntityAutocomplete::extractEntityIdFromAutocompleteInput($string);
-    if ($entity_id !== NULL) {
-      // @todo Support entity types other than 'node'. Will be fixed in
-      //   https://www.drupal.org/node/2423093.
-      $uri = 'entity:node/' . $entity_id;
-    }
-    // Detect a schemeless string, map to 'internal:' URI.
-    elseif (!empty($string) && parse_url($string, PHP_URL_SCHEME) === NULL) {
-      // @todo '<front>' is valid input for BC reasons, may be removed by
-      //   https://www.drupal.org/node/2421941
-      // - '<front>' -> '/'
-      // - '<front>#foo' -> '/#foo'
-      if (strpos($string, '<front>') === 0) {
-        $string = '/' . substr($string, strlen('<front>'));
-      }
-      $uri = 'internal:' . $string;
-    }
-
-    return $uri;
-  }
-
-  /**
-   * Form element validation handler for the 'uri' element.
-   *
-   * Disallows saving inaccessible or untrusted URLs.
-   */
-  public static function validateUriElement($element, FormStateInterface $form_state, $form) {
-    $uri = static::getUserEnteredStringAsUri($element['#value']);
-    $form_state->setValueForElement($element, $uri);
-
-    // If getUserEnteredStringAsUri() mapped the entered value to a 'internal:'
-    // URI , ensure the raw value begins with '/', '?' or '#'.
-    // @todo '<front>' is valid input for BC reasons, may be removed by
-    //   https://www.drupal.org/node/2421941
-    if (
-      parse_url($uri, PHP_URL_SCHEME) === 'internal'
-      && !in_array($element['#value'][0], ['/', '?', '#'], TRUE)
-      && substr($element['#value'], 0, 7) !== '<front>'
-    ) {
-      $form_state->setError($element, t('Manually entered paths should start with /, ? or #.'));
-      return;
-    }
-  }
-
-  /**
-   * Form element validation handler for the 'title' element.
-   *
-   * Conditionally requires the link title if a URL value was filled in.
-   */
-  public static function validateTitleElement(&$element, FormStateInterface $form_state, $form) {
-    if ($element['uri']['#value'] !== '' && $element['title']['#value'] === '') {
-      $element['title']['#required'] = TRUE;
-      // We expect the field name placeholder value to be wrapped in t() here,
-      // so it won't be escaped again as it's already marked safe.
-      $form_state->setError($element['title'], t('@name field is required.', array('@name' => $element['title']['#title'])));
-    }
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['third_party_settings'],
+      $container->get('entity_type.manager')
+    );
   }
 
   /**
@@ -437,7 +348,7 @@ class SearchWidget extends WidgetBase {
    */
   protected function getFacets() {
     $storage = $this->fieldDefinition->getFieldStorageDefinition();
-    $facet_storage = \Drupal::entityTypeManager()->getStorage('facets_facet');
+    $facet_storage = $this->entityTypeManager->getStorage('facets_facet');
     $facets = $facet_storage->loadByProperties(['facet_source_id' => 'search_api_field:' . $storage->id()]);
     return $facets;
   }
