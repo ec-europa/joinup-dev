3dffcb75f2937282c17dfd713e54ebee254194e1
Sander Van Dooren <sander.vandooren@one-agency.be>
2016-09-14 17:44:09 +0200

    Issue #ISAICP-2623: Get user from membership.

--- web/modules/custom/solution/src/EventSubscriber/WorkflowTransitionEventSubscriber.php
+++ web/modules/custom/solution/src/EventSubscriber/WorkflowTransitionEventSubscriber.php
@@ -5,6 +5,7 @@ namespace Drupal\solution\EventSubscriber;
 use Drupal\message\Entity\Message;
 use Drupal\message\Entity\MessageTemplate;
 use Drupal\message_notify\MessageNotifier;
+use Drupal\og\Entity\OgMembership;
 use Drupal\state_machine\Event\StateChangeEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -37,17 +38,37 @@ class WorkflowTransitionEventSubscriber implements EventSubscriberInterface {
    * @throws \Drupal\message_notify\Exception\MessageNotifyException
    */
   public function onChangeToValidated(StateChangeEvent $event) {
-    $state = $event->getState();
     $entity = $event->getEntity();
-    // @todo Check type!
+    $bundle = $entity->bundle();
+    if ($bundle != 'solution') {
+      return;
+    }
     $message_template = MessageTemplate::load('workflow_transition');
-    $message = Message::create([
-      'template' => $message_template->id(),
-      'uid' => 1,
-      'field_message_content' => $entity->id(),
-    ]);
-    $message->save();
-    $this->messageNotifier->send($message, [], 'email');
+
+    $storage = \Drupal::entityManager()->getStorage('og_membership');
+
+    // Sent a message to all solution administrators.
+    $membership_query = $storage->getQuery();
+    $membership_query
+      ->condition('state', 'active')
+      ->condition('entity_id', $entity->id())
+      ->condition('roles', 'rdf_entity-solution-administrator');
+    $memberships_ids = $membership_query->execute();
+    $memberships = OgMembership::loadMultiple($memberships_ids);
+    /** @var OgMembership $membership */
+    foreach ($memberships as $membership) {
+      $uid = $membership->get('uid')->first()->getValue()['target_id'];
+      // Create the actual message and save it to the db.
+      $message = Message::create([
+        'template' => $message_template->id(),
+        'uid' => $uid,
+        'field_message_content' => $entity->id(),
+      ]);
+      $message->save();
+      // Send the saved message as an e-mail.
+      $this->messageNotifier->send($message, [], 'email');
+    }
+
   }
 
   /**
