e3a9e325daf56a0f6a0705e42c4cce23f8dc2f27
idimopoulos <idimopoulos@hotmail.com>
2016-10-11 00:11:53 +0200

    ISAICP-2611: Introduction to event subscriber. All events will load one method and this will automatically send the appropriate message.

--- web/modules/custom/joinup_notification/src/EventSubscriber/WorkflowTransitionEventSubscriber.php
+++ web/modules/custom/joinup_notification/src/EventSubscriber/WorkflowTransitionEventSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\joinup_notification\EventSubscriber;
 
+use Drupal\state_machine\Event\WorkflowTransitionEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\Event;
 use Drupal\Core\Config\ConfigManager;
@@ -19,21 +20,21 @@ class WorkflowTransitionEventSubscriber implements EventSubscriberInterface {
   /**
    * Drupal\message_notify\MessageNotifier definition.
    *
-   * @var Drupal\message_notify\MessageNotifier
+   * @var \Drupal\message_notify\MessageNotifier
    */
   protected $message_notify_sender;
 
   /**
    * Drupal\Core\Entity\EntityTypeManager definition.
    *
-   * @var Drupal\Core\Entity\EntityTypeManager
+   * @var \Drupal\Core\Entity\EntityTypeManager
    */
   protected $entity_type_manager;
 
   /**
    * Drupal\og\OgRoleManager definition.
    *
-   * @var Drupal\og\OgRoleManager
+   * @var \Drupal\og\OgRoleManager
    */
   protected $og_role_manager;
 
@@ -47,9 +48,31 @@ class WorkflowTransitionEventSubscriber implements EventSubscriberInterface {
   }
 
   /**
+   * On workflow transition.
+   *
+   * @param \Drupal\state_machine\Event\WorkflowTransitionEvent $event
+   *   The state change event.
+   *
+   * @throws \Drupal\message_notify\Exception\MessageNotifyException
+   */
+  public function messageSender(WorkflowTransitionEvent $event) {
+    $configuration = \Drupal::config('joinup_notification.settings')->get('notifications');
+  }
+
+  /**
    * {@inheritdoc}
    */
   static function getSubscribedEvents() {
+    $configuration = \Drupal::config('joinup_notification.settings')->get('notifications');
+    $events = [];
+    foreach ($configuration as $entity_type => $workflow_groups) {
+      foreach ($workflow_groups as $workflow_group => $transitions) {
+        foreach ($transitions as $transition => $roles) {
+          $event_name = $workflow_group . '.' . $transition . '.post_transition';
+          $events[$event_name][] = ['messageSender'];
+        }
+      }
+    }
 
     return $events;
   }
