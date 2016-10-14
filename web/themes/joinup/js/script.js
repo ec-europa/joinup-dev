18e74a3f629ada5b5fe1d43544c2e20a7f91cda9
Francesco Sardara <francesco.sardara@one-agency.be>
2016-10-14 14:46:07 +0200

    Issue #ISAICP-2857: no need to depend on Drupal for all the javascript code.

--- web/themes/joinup/js/script.js
+++ web/themes/joinup/js/script.js
@@ -3,14 +3,10 @@
  * Joinup theme scripts.
  */
 
-(function ($, Drupal) {
-  Drupal.behaviors.sidebarMenu = {
-    attach: function (context, settings) {
-      $(context).find('.sidebar-menu__button--desktop').once('sidebarMenu').each(function () {
-        $(this).on('click', function () {
-          $(this).siblings('.mdl-menu__container').toggleClass('desktop-hidden');
-        });
-      });
-    }
-  };
-})(jQuery, Drupal);
+(function ($) {
+  $(function () {
+    $('.sidebar-menu__button--desktop').on('click', function () {
+      $(this).siblings('.mdl-menu__container').toggleClass('desktop-hidden');
+    });
+  });
+})(jQuery);
