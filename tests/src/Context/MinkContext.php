3ccee61cfbe34aed47457c50711fe711bdc0f90f
Frederic Dewinne <frederic@continuousphp.com>
2016-10-23 21:52:21 +0200

    fix file attached issue

---
+++ tests/src/Context/MinkContext.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\joinup\Context;
+
+use Drupal\DrupalExtension\Context\MinkContext as DrupalExtensionMinkContext;
+
+/**
+ * Provides step definitions for interacting with Mink.
+ */
+class MinkContext extends DrupalExtensionMinkContext {
+  
+  public function attachFileToField($field, $path) {
+      $field = $this->fixStepArgument($field);
+
+      if ($this->getMinkParameter('files_path')) {
+          $fullPath = rtrim(realpath($this->getMinkParameter('files_path')), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.$path;
+          if (is_file($fullPath)) {
+              $path = $fullPath;
+          }
+      }
+
+      if (!$this->getSession()->getDriver() instanceof Behat\Mink\Driver\GoutteDriver) {
+          $tempZip = tempnam('', 'WebDriverZip');
+          $zip = new \ZipArchive();
+          $zip->open($tempZip, \ZipArchive::CREATE);
+          $zip->addFile($path, basename($path));
+          $zip->close();
+          $path = $this->getSession()->getDriver()->getWebDriverSession()->file([
+              'file' => base64_encode(file_get_contents($tempZip))
+          ]);
+      }
+
+      $this->getSession()->getPage()->attachFileToField($field, $path);
+
+      if (isset($tempZip)) {
+          unlink($tempZip);
+      }
+  }
+}
