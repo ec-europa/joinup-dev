8e1cc3fdcdcd0a83dab56bd976474c97e0e65320
Francesco Sardara <francesco.sardara@one-agency.be>
2016-11-29 18:09:32 +0100

    Issue #ISAICP-2928: use method available on entity type class to check if it is revisionable.

--- web/modules/custom/state_machine_revisions/src/EventSubscriber/WorkflowTransitionEventSubscriber.php
+++ web/modules/custom/state_machine_revisions/src/EventSubscriber/WorkflowTransitionEventSubscriber.php
@@ -39,7 +39,7 @@ class WorkflowTransitionEventSubscriber implements EventSubscriberInterface {
     // Since all content entities implement the RevisionableInterface, we have
     // to check if the entity has the revision support and if it is set to
     // create a new revision.
-    if (!$entity->getEntityType()->hasKey('revision') || !$entity->isNewRevision()) {
+    if (!$entity->getEntityType()->isRevisionable() || !$entity->isNewRevision()) {
       return;
     }
 
