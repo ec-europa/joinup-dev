a2690b4b1da4e3ce3f4435c1008f2c7648bfb754
Pieter Frenssen <pieter@frenssen.be>
2016-12-07 14:34:00 +0100

    ISAICP-2764: Clarify why the cleanup of the language terms is needed.

--- src/Phing/AfterFixturesImportCleanup.php
+++ src/Phing/AfterFixturesImportCleanup.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Contains \DrupalProject\build\Phing\AfterFixturesImportCleanup.
@@ -12,9 +13,18 @@ namespace DrupalProject\Phing;
 class AfterFixturesImportCleanup extends VirtuosoTaskBase {
 
   /**
-   * Set the permissions of the '/sparql' endpoint to allow update queries.
+   * Clean up the fixtures after import.
    */
   public function main() {
+    // We get our languages from the Metadata Registry. The Metadata Registry
+    // maintains two authority tables, one for individual languages and one for
+    // groups of languages called multilingual. For legacy reasons the two
+    // tables are published as a merger of the two.
+    // The multilingual language groups are not useful for us and we need to
+    // filter them out to avoid having the language lists polluted with entries
+    // labeled 'Multilingual Code'.
+    // @see http://publications.europa.eu/mdr/resource//documentation/schema/cat.html#element_languages
+    // @see https://webgate.ec.europa.eu/CITnet/jira/browse/ISAICP-2764
     $this->execute('sparql DELETE FROM <http://languages-skos>
 {
   ?entity ?field ?value.
