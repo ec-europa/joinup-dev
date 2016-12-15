82984a032259d33409a132f9d22ea4503d1a8190
idimopoulos <idimopoulos@hotmail.com>
2016-12-15 15:28:56 +0200

    ISAICP-2261: Test: Try to fix the sparql issue.

--- web/modules/custom/joinup_core/tests/src/JoinupWorkflowTestBase.php
+++ web/modules/custom/joinup_core/tests/src/JoinupWorkflowTestBase.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\Tests\joinup_core;
 
+use Drupal\Core\Database\Database;
 use Drupal\Tests\BrowserTestBase;
+use EasyRdf\Http;
 
 /**
  * Base setup for a joinup workflow test.
@@ -10,6 +12,20 @@ use Drupal\Tests\BrowserTestBase;
  * @group rdf_entity
  */
 class JoinupWorkflowTestBase extends BrowserTestBase {
+
+  /**
+   * The SPARQL database info.
+   *
+   * @var array
+   */
+  protected $sparqlConnectionInfo;
+
+  /**
+   * The SPARQL database connection.
+   *
+   * @var \Drupal\rdf_entity\Database\Driver\sparql\Connection
+   */
+  protected $sparql;
 
   /**
    * {@inheritdoc}
@@ -48,11 +64,82 @@ class JoinupWorkflowTestBase extends BrowserTestBase {
    * {@inheritdoc}
    */
   public function setUp() {
+    // The SPARQL connection has to be set up before.
+    if (!$this->setUpSparql()) {
+      $this->markTestSkipped('No Sparql connection available.');
+    }
+    // Test is not compatible with Virtuoso 6.
+    if ($this->detectVirtuoso6()) {
+      $this->markTestSkipped('Skipping: Not running on Virtuoso 6.');
+    }
+
     parent::setUp();
     $this->ogMembershipManager = \Drupal::service('og.membership_manager');
     $this->ogAccess = $this->container->get('og.access');
     $this->entityAccess = $this->container->get('entity_type.manager')->getAccessControlHandler('rdf_entity');
     $this->userProvider = $this->container->get('joinup_user.workflow.user_provider');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown() {
+    // Delete all data produced by testing module.
+    foreach (['published', 'draft'] as $graph) {
+      $query = <<<EndOfQuery
+DELETE {
+  GRAPH <http://example.com/dummy/$graph> {
+    ?entity ?field ?value
+  }
+}
+WHERE {
+  GRAPH <http://example.com/dummy/$graph> {
+    ?entity ?field ?value
+  }
+}
+EndOfQuery;
+      $this->sparql->query($query);
+    }
+
+    parent::tearDown();
+  }
+
+  /**
+   * Checks if the triple store is an Virtuoso 6 instance.
+   *
+   * @return bool
+   *   TRUE if it's a Virtuoso 6 server.
+   */
+  protected function detectVirtuoso6() {
+    $client = Http::getDefaultHttpClient();
+    $client->resetParameters(TRUE);
+    $client->setUri("http://{$this->sparqlConnectionInfo['host']}:{$this->sparqlConnectionInfo['port']}/");
+    $client->setMethod('GET');
+    $response = $client->request();
+    $server_header = $response->getHeader('Server');
+    if (strpos($server_header, "Virtuoso/06") === FALSE) {
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+  /**
+   * Setup the db connection to the triple store.
+   */
+  protected function setUpSparql() {
+    // If the test is run with argument db url then use it.
+    // export SIMPLETEST_SPARQL_DB='sparql://127.0.0.1:8890/'.
+    $db_url = getenv('SIMPLETEST_SPARQL_DB');
+    if (empty($db_url)) {
+      return FALSE;
+    }
+    $this->sparqlConnectionInfo = Database::convertDbUrlToConnectionInfo($db_url, dirname(dirname(__DIR__)));
+    $this->sparqlConnectionInfo['namespace'] = 'Drupal\\rdf_entity\\Database\\Driver\\sparql';
+    Database::addConnectionInfo('sparql_default', 'default', $this->sparqlConnectionInfo);
+
+    $this->sparql = Database::getConnection('default', 'sparql_default');
+
+    return TRUE;
   }
 
 }
