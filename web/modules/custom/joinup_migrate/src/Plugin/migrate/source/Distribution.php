10b23c346b8b2b3e46b8330ed06cc8514649a34d
Claudiu Cristea <clau.cristea@gmail.com>
2017-01-16 10:23:14 +0200

    ISAICP-2833: Migrate the licence type. Migrate field_ad_licence in 'solution'.

--- web/modules/custom/joinup_migrate/src/Plugin/migrate/source/Distribution.php
+++ web/modules/custom/joinup_migrate/src/Plugin/migrate/source/Distribution.php
@@ -24,6 +24,7 @@ class Distribution extends DistributionBase {
       'title' => $this->t('Name'),
       'created_time' => $this->t('Created time'),
       'body' => $this->t('Description'),
+      'licence' => $this->t('Licence'),
       'changed_time' => $this->t('Changed time'),
       'technique' => $this->t('Representation technique'),
     ] + parent::fields();
@@ -39,6 +40,11 @@ class Distribution extends DistributionBase {
 
     $query->addExpression("FROM_UNIXTIME({$this->alias['node']}.created, '%Y-%m-%dT%H:%i:%s')", 'created_time');
     $query->addExpression("FROM_UNIXTIME({$this->alias['node']}.changed, '%Y-%m-%dT%H:%i:%s')", 'changed_time');
+
+    $this->alias['content_field_distribution_licence'] = $query->leftJoin('content_field_distribution_licence', 'content_field_distribution_licence', "{$this->alias['node']}.vid = %alias.vid");
+    $this->alias['node_licence'] = $query->leftJoin('node', 'node_licence', "{$this->alias['content_field_distribution_licence']}.field_distribution_licence_nid = %alias.nid AND %alias.type = 'licence'");
+
+    $query->addExpression("{$this->alias['node_licence']}.nid", 'licence');
 
     return $query
       ->fields($this->alias['node'], ['title', 'vid'])
