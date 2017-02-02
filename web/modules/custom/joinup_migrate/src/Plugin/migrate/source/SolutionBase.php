809c34ef73b51b7e50a1e80855f695a55e4791c1
Claudiu Cristea <clau.cristea@gmail.com>
2017-02-02 16:29:25 +0200

    ISAICP-3091: Limit asset_solution > solution migrations to Interoperability Solutions belonging to a 'repository'.

--- web/modules/custom/joinup_migrate/src/Plugin/migrate/source/SolutionBase.php
+++ web/modules/custom/joinup_migrate/src/Plugin/migrate/source/SolutionBase.php
@@ -40,8 +40,12 @@ abstract class SolutionBase extends JoinupSqlBase {
     $this->alias['collection'] = $query->join('joinup_migrate_collection', 'c', "m.collection = %alias.collection");
     $this->alias['node'] = $query->join('node', 'n', "m.nid = %alias.nid");
     $this->alias['node_revision'] = $query->join('node_revisions', 'node_revision', "{$this->alias['node']}.vid = %alias.vid");
+    $this->alias['og_ancestry'] = $query->join('og_ancestry', 'og_ancestry', "{$this->alias['node']}.nid = %alias.nid");
+    $this->alias['node_og'] = $query->join('node', 'node_og', "{$this->alias['og_ancestry']}.group_nid = %alias.nid AND %alias.type = 'repository'");
 
-    return $query->fields($this->alias['node'], ['nid']);
+    return $query
+      ->fields($this->alias['node'], ['nid'])
+      ->condition("{$this->alias['node_og']}.type", 'repository');
   }
 
 }
