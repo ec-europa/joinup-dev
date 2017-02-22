91561b09f598ec194e9ad0ecbdc953dcc0844d8a
Pieter Frenssen <pieter@frenssen.be>
2017-02-22 16:38:40 +0100

    ISAICP-3098: Allow the field storage form validate handler to do its job except in the specific circumstance where it would thrown an exception.

--- web/modules/custom/rdf_entity/src/Form/RdfFieldStorageConfigEditForm.php
+++ web/modules/custom/rdf_entity/src/Form/RdfFieldStorageConfigEditForm.php
@@ -16,11 +16,41 @@ class RdfFieldStorageConfigEditForm extends FieldStorageConfigEditForm {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $type = $this->entity->get('entity_type');
-    // Skip validation of cardinality for Sparql backend fields.
+
+    // Skip validation of cardinality for Sparql backend fields if the field
+    // mapping is not provided. If a specific value is entered for the
+    // cardinality, the default field storage validate handler will do a check
+    // if there are any field instances in use in the database with a higher
+    // cardinality. If this is the case it will throw a validation error, so no
+    // data is lost. When a brand new Sparql based field is being created, the
+    // RDF field mapping is not yet stored in the third party settings, so the
+    // database cannot be queried yet. This will cause an exception to be
+    // thrown. Let's avoid this from happening.
     if ($this->entityTypeManager->getStorage($type) instanceof RdfEntitySparqlStorage) {
-      return;
+      // Check if the RDF field mapping already exists. If it doesn't skip the
+      // part of the storage form validation that checks the database by
+      // tricking it in thinking the entity is new.
+      if (!$this->hasRdfFieldMapping()) {
+        $this->entity->enforceIsNew(TRUE);
+        parent::validateForm($form, $form_state);
+        $this->entity->enforceIsNew(FALSE);
+        return;
+      }
     }
+
     parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * Returns whether the field has a populated RDF field mapping value.
+   *
+   * @return bool
+   *   Whether or not the RDF field mapping has been populated.
+   */
+  protected function hasRdfFieldMapping() {
+    /** @var \Drupal\field\FieldStorageConfigInterface $unchanged_entity */
+    $unchanged_entity = $this->entityTypeManager->getStorage('field_storage_config')->loadUnchanged($this->entity->id());
+    return !empty($unchanged_entity->getThirdPartySetting('rdf_entity', 'mapping', [])['value']);
   }
 
 }
