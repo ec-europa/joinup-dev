904f5fbcef922c0b5f3cfcebe8b001fffb646541
Claudiu Cristea <clau.cristea@gmail.com>
2017-03-07 20:14:02 +0200

    ISAICP-3090: Fixing hierarchy issue.

--- web/modules/custom/joinup_migrate/src/Plugin/migrate/process/TermReference.php
+++ web/modules/custom/joinup_migrate/src/Plugin/migrate/process/TermReference.php
@@ -5,6 +5,7 @@ namespace Drupal\joinup_migrate\Plugin\migrate\process;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Drupal\taxonomy\TermInterface;
 
 /**
  * Provides a processor for Joinup Rdf term references.
@@ -30,13 +31,22 @@ class TermReference extends ProcessPluginBase {
       return NULL;
     }
     if (!isset($this->cache[$this->configuration['vocabulary']][$value])) {
-      /** @var \Drupal\rdf_entity\Entity\RdfEntitySparqlStorage $storage */
+      /** @var \Drupal\rdf_taxonomy\TermRdfStorage $storage */
       $storage = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
-      if (!$terms = $storage->loadByProperties([
+      /** @var \Drupal\taxonomy\TermInterface[] $terms */
+      $terms = $storage->loadByProperties([
         'name' => (array) $value,
         'vid' => $this->configuration['vocabulary'],
-      ])
-      ) {
+      ]);
+
+      if (!empty($this->configuration['top_level_only'])) {
+        // Allow only top level terms.
+        $terms = array_filter($terms, function (TermInterface $term) use ($storage) {
+          return (bool) !$storage->loadParents($term->id());
+        });
+      }
+
+      if (!$terms) {
         $migrate_executable->saveMessage("Term '$value' does not exits in destination.");
         return NULL;
       }
