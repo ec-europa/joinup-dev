5f97c8800ce1f3b82e018c1793741fa401bd0129
Pieter Frenssen <pieter@frenssen.be>
2017-03-17 11:42:55 +0100

    ISAICP-3200: Store the eLibrary Creation options in a ritzy final SplEnum class instead of haphazardly scattering it around the code base.

---
+++ web/modules/custom/joinup_core/src/ELibraryCreationOptions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\joinup_core;
+
+/**
+ * Stores various options related to the creation of eLibrary content.
+ */
+final class ELibraryCreationOptions extends \SplEnum {
+
+  /**
+   * Default option.
+   */
+  const __default = self::FACILITATORS;
+
+  /**
+   * Elibrary option defining that only facilitators can create content.
+   */
+  const FACILITATORS = 0;
+
+  /**
+   * Elibrary option defining that members and facilitators can create content.
+   */
+  const MEMBERS = 1;
+
+  /**
+   * Elibrary option defining that any registered user can create content.
+   */
+  const REGISTERED_USERS = 2;
+
+}
