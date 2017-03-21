e33b02c24e3f0911bcb7502950bdbc50830497ea
Ilias Dimopoulos <idimopoulos@hotmail.com>
2017-03-21 11:34:04 +0100

    ISAICP-2693: Unify the custom page controller to use the generic community content controller.

--- web/modules/custom/custom_page/src/Controller/CustomPageController.php
+++ web/modules/custom/custom_page/src/Controller/CustomPageController.php
@@ -5,6 +5,7 @@ namespace Drupal\custom_page\Controller;
 use Drupal\Component\Utility\Xss;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\joinup_core\Controller\CommunityContentController;
 use Drupal\og\OgAccessInterface;
 use Drupal\og\OgGroupAudienceHelperInterface;
 use Drupal\og_menu\OgMenuInstanceInterface;
@@ -19,92 +20,25 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @package Drupal\custom_page\Controller
  */
-class CustomPageController extends ControllerBase {
+class CustomPageController extends CommunityContentController {
 
   /**
-   * The OG access handler.
+   * {@inheritdoc}
    *
-   * @var \Drupal\og\OgAccessInterface
+   * The custom pages are only allowed to be created for collections.
    */
-  protected $ogAccess;
-
-  /**
-   * Constructs a CustomPageController.
-   *
-   * @param \Drupal\og\OgAccessInterface $og_access
-   *   The OG access handler.
-   */
-  public function __construct(OgAccessInterface $og_access) {
-    $this->ogAccess = $og_access;
+  public function createAccess(RdfInterface $rdf_entity) {
+    if ($rdf_entity->bundle() !== 'collection') {
+      return AccessResult::forbidden();
+    }
+    return parent::createAccess($rdf_entity);
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('og.access')
-    );
-  }
-
-  /**
-   * Controller for the base form.
-   *
-   * We need to override the functionality of the create form for pages
-   * that include the rdf_entity id in the url so that the og audience field
-   * is auto completed.
-   *
-   * @param \Drupal\rdf_entity\RdfInterface $rdf_entity
-   *   The collection rdf_entity.
-   *
-   * @return array
-   *   Return the form array to be rendered.
-   */
-  public function add(RdfInterface $rdf_entity) {
-    $node = $this->createNewCustomPage($rdf_entity);
-    $form = $this->entityFormBuilder()->getForm($node);
-
-    return $form;
-  }
-
-  /**
-   * Handles access to the custom page add form through collection pages.
-   *
-   * Access is granted to moderators and group members that have the permission
-   * to create custom pages inside of their group, which in practice means this
-   * is granted to collection and solution facilitators.
-   *
-   * @param \Drupal\rdf_entity\RdfInterface $rdf_entity
-   *   The RDF entity for which the custom page is created.
-   *
-   * @return \Drupal\Core\Access\AccessResult
-   *   The access result object.
-   */
-  public function createCustomPageAccess(RdfInterface $rdf_entity) {
-    $user = $this->currentUser();
-    // Grant access if the user is a moderator.
-    if (in_array('moderator', $user->getRoles())) {
-      return AccessResult::allowed()->addCacheContexts(['user.roles']);
-    }
-    // Grant access depending on whether the user has permission to create a
-    // custom page according to their OG role.
-    return $this->ogAccess->userAccessGroupContentEntityOperation('create', $rdf_entity, $this->createNewCustomPage($rdf_entity), $user);
-  }
-
-  /**
-   * Creates a new custom page entity.
-   *
-   * @param \Drupal\rdf_entity\RdfInterface $rdf_entity
-   *   The collection with which the custom page will be associated.
-   *
-   * @return \Drupal\Core\Entity\EntityInterface
-   *   The unsaved custom page entity.
-   */
-  protected function createNewCustomPage(RdfInterface $rdf_entity) {
-    return $this->entityTypeManager()->getStorage('node')->create([
-      'type' => 'custom_page',
-      OgGroupAudienceHelperInterface::DEFAULT_FIELD => $rdf_entity->id(),
-    ]);
+  protected function getBundle() {
+    return 'custom_page';
   }
 
   /**
