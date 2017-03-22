b97c6a8cd4ee30f158464ebcea2ef6f1b8b25438
Ilias Dimopoulos <idimopoulos@hotmail.com>
2017-03-22 17:10:02 +0100

    ISAICP-2693: Update custom page controller to use the community content controller.

--- web/modules/custom/custom_page/src/Controller/CustomPageController.php
+++ web/modules/custom/custom_page/src/Controller/CustomPageController.php
@@ -31,7 +31,9 @@ class CustomPageController extends CommunityContentController {
     if ($rdf_entity->bundle() !== 'collection') {
       return AccessResult::forbidden();
     }
-    return parent::createAccess($rdf_entity);
+    // Grant access depending on whether the user has permission to create a
+    // custom page according to their OG role.
+    return $this->ogAccess->userAccessGroupContentEntityOperation('create', $rdf_entity, $this->createContentEntity($rdf_entity));
   }
 
   /**
