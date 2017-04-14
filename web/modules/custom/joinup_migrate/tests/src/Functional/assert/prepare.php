7c068341c6ad8ccde4439c231c58380af4657622
Claudiu Cristea <clau.cristea@gmail.com>
2017-04-14 17:24:21 +0300

    ISAICP-3272: Add 2 new collection test cases. Reorganise 'mapping' and 'prepare' assertions.

--- web/modules/custom/joinup_migrate/tests/src/Functional/assert/prepare.php
+++ web/modules/custom/joinup_migrate/tests/src/Functional/assert/prepare.php
@@ -5,9 +5,37 @@
  * Assertions for 'prepare' migration.
  */
 
+$expected_values = [
+  'More than one Community or Repository' => [
+    'type' => 'repository',
+    'nid' => '105945',
+    'policy2' => 'Defence',
+    'collection_owner' => 'joeroe@example.com',
+  ],
+  'New collection' => [
+    'policy2' => 'eProcurement',
+    'collection_owner' => 'doe@example.com',
+    'roles' => '{"admin":[],"facilitator":[],"member":{"6842":1378476956}}',
+  ],
+  'Collection from Repository' => [
+    'type' => 'repository',
+    'nid' => '59642',
+    'policy2' => 'Open government',
+    'collection_owner' => 'baby.doe@example.com',
+  ],
+  'Collection from Community' => [
+    'type' => 'community',
+    'nid' => '42436',
+    'policy2' => 'Collaboration',
+    'collection_owner' => 'jbloggs@example.com',
+    'roles' => '{"admin":[],"facilitator":{"6363":1458898156,"6364":1369819022,"7355":1396525296},"member":{"6363":1458898156,"6364":1369819022,"6549":1452513538,"6822":1332526658,"7077":1323431755,"7090":1396525012,"7207":1415570449,"7355":1396525296}}',
+  ],
+];
+
 // Migration counts.
-$this->assertTotalCount('prepare', 2);
-$this->assertSuccessCount('prepare', 2);
+$expected_values_count = count($expected_values);
+$this->assertTotalCount('prepare', $expected_values_count);
+$this->assertSuccessCount('prepare', $expected_values_count);
 
 // Expected logged messages.
 $this->assertMessage('prepare', "Collection 'No Repository or Community' should inherit data from D6 but has no 'community' or 'repository' records defined.");
@@ -20,36 +48,26 @@ $imported = $this->legacyDb->select('d8_prepare')
   ->execute()
   ->fetchAllAssoc('collection', PDO::FETCH_ASSOC);
 
-$this->assertSame([
-  'collection' => 'More than one Community or Repository',
-  'type' => 'repository',
-  'nid' => '105945',
-  'policy2' => 'Defence',
-  'policy' => NULL,
-  'abstract' => NULL,
-  'logo' => NULL,
-  'banner' => NULL,
-  'elibrary' => NULL,
-  'publisher' => NULL,
-  'contact' => NULL,
-  'collection_owner' => 'joeroe@example.com',
-  'status' => NULL,
-  'roles' => NULL,
-], $imported['More than one Community or Repository']);
+foreach ($expected_values as $collection => $expected_value) {
+  // Ensure defaults.
+  $expected_value += [
+    'collection' => $collection,
+    'type' => '',
+    'nid' => '0',
+    'policy2' => NULL,
+    'policy' => NULL,
+    'abstract' => NULL,
+    'logo' => NULL,
+    'banner' => NULL,
+    'elibrary' => NULL,
+    'publisher' => NULL,
+    'contact' => NULL,
+    'collection_owner' => NULL,
+    'status' => NULL,
+    'roles' => NULL,
+  ];
+  ksort($expected_value);
+  ksort($imported[$collection]);
 
-$this->assertSame([
-  'collection' => 'New collection',
-  'type' => '',
-  'nid' => '0',
-  'policy2' => 'eProcurement',
-  'policy' => NULL,
-  'abstract' => NULL,
-  'logo' => NULL,
-  'banner' => NULL,
-  'elibrary' => NULL,
-  'publisher' => NULL,
-  'contact' => NULL,
-  'collection_owner' => 'doe@example.com',
-  'status' => NULL,
-  'roles' => '{"admin":[],"facilitator":[],"member":{"6842":1378476956}}',
-], $imported['New collection']);
+  $this->assertSame($expected_value, $imported[$collection]);
+}
