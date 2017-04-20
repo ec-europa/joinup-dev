7979a9d10d81fe90805d093b3fd641bae7e63f7f
Francesco Sardara <francesco.sardara@one-agency.be>
2017-04-20 16:00:59 +0200

    Issue #ISAICP-3156: add a configurable property to use as text for the 'All' option.

--- web/modules/custom/joinup_core/src/Plugin/facets/widget/LinksInlineWidget.php
+++ web/modules/custom/joinup_core/src/Plugin/facets/widget/LinksInlineWidget.php
@@ -3,8 +3,11 @@
 namespace Drupal\joinup_core\Plugin\facets\widget;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\facets\FacetInterface;
 use Drupal\facets\Widget\WidgetPluginBase;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * The links widget.
@@ -22,6 +25,7 @@ class LinksInlineWidget extends WidgetPluginBase {
    */
   public function defaultConfiguration() {
     return [
+      'all_text' => 'All',
       'prefix_text' => '',
       'suffix_text' => '',
     ] + parent::defaultConfiguration();
@@ -33,6 +37,14 @@ class LinksInlineWidget extends WidgetPluginBase {
   public function buildConfigurationForm(array $form, FormStateInterface $form_state, FacetInterface $facet) {
     $form = parent::buildConfigurationForm($form, $form_state, $facet);
     $config = $this->getConfiguration();
+
+    $form['all_text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('All text'),
+      '#description' => $this->t('Shown for the facet reset link.'),
+      '#default_value' => $config['all_text'] ?: 'All',
+      '#required' => TRUE,
+    ];
 
     $form['prefix_text'] = [
       '#type' => 'textfield',
