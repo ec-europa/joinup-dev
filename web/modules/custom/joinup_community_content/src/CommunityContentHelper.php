ad8feaf3daa9c83d15c310cfca1ddf9a7d062a24
Pieter Frenssen <pieter@frenssen.be>
2017-04-21 18:00:29 +0300

    ISAICP-2468: Provide a way to retrieve the workflow states that need moderator attention.

---
+++ web/modules/custom/joinup_community_content/src/CommunityContentHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\joinup_community_content;
+
+/**
+ * Contains helper methods for dealing with community content.
+ */
+class CommunityContentHelper {
+
+  /**
+   * Returns the workflow states that require attention from a moderator.
+   *
+   * @param string $bundle
+   *   Optional ID of the community content bundle for which to return workflow
+   *   states. If omitted, workflow states for all bundles will be returned.
+   *
+   * @return string[]
+   *   An array of workflow state IDs.
+   */
+  public static function getModeratorAttentionNeededStates($bundle = NULL) {
+    $states = [
+      'discussion' => [
+        'proposed',
+      ],
+      'document' => [
+        'deletion_request',
+        'proposed',
+      ],
+      'event' => [
+        'deletion_request',
+        'proposed',
+      ],
+      'news' => [
+        'deletion_request',
+        'proposed',
+      ],
+    ];
+
+    if ($bundle) {
+      return $states[$bundle];
+    }
+
+    // If the $bundle parameter is omitted, return the merged states from all
+    // bundles.
+    return array_reduce($states, function ($all_states, $bundle_states) {
+      return array_unique(array_merge($all_states, $bundle_states));
+    }, []);
+  }
+
+}
