b5b20389289e1978c92159f45349165107ab5abe
idimopoulos <idimopoulos@hotmail.com>
2017-04-25 19:01:07 +0200

    ISAICP-3316: Remove the MissingRelationException.

--- web/modules/custom/joinup_core/src/Exception/MissingRelationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\joinup_core\Exception;
-
-/**
- * Exception class to throw when a required relation is missing.
- */
-class MissingRelationException extends \Exception {
-}
