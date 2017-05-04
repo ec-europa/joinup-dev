09ba3100317162f0f8d16b91b8e19ee67345c305
Ilias Dimopoulos <idimopoulos@hotmail.com>
2017-05-04 11:15:03 +0200

    ISAICP-3194: Add a separate violation when only one of the two fields of a datetime is filled.

---
+++ web/modules/custom/joinup_core/src/Element/Datetime.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\joinup_core\Element;
+
+use Drupal\Core\Datetime\Element\Datetime;
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a datetime element.
+ */
+class Datetime extends Datetime {
+
+  /**
+   * {@inheritdoc}
+   *
+   * We are merely adding a different error message on the field.
+   */
+  public static function validateDatetime(&$element, FormStateInterface $form_state, &$complete_form) {
+    $input_exists = FALSE;
+    $input = NestedArray::getValue($form_state->getValues(), $element['#parents'], $input_exists);
+    if ($input_exists) {
+
+      $title = !empty($element['#title']) ? $element['#title'] : 'datetime';
+      $date_format = $element['#date_date_element'] != 'none' ? static::getHtml5DateFormat($element) : '';
+      $time_format = $element['#date_time_element'] != 'none' ? static::getHtml5TimeFormat($element) : '';
+      $format = trim($date_format . ' ' . $time_format);
+
+      // If there's empty input and the field is not required, set it to empty.
+      if (empty($input['date']) && empty($input['time']) && !$element['#required']) {
+        $form_state->setValueForElement($element, NULL);
+      }
+      // If there's empty input and the field is required, set an error. A
+      // reminder of the required format in the message provides a good UX.
+      elseif (empty($input['date']) && empty($input['time']) && $element['#required']) {
+        $form_state->setError($element, t('The %field date is required. Please enter a date in the format %format.', ['%field' => $title, '%format' => static::formatExample($format)]));
+      }
+      else {
+        // If the date is valid, set it.
+        $date = $input['object'];
+        if ($date instanceof DrupalDateTime && !$date->hasErrors()) {
+          $form_state->setValueForElement($element, $date);
+        }
+        // If only one of the two fields are filled, set an error.
+        // @see: https://webgate.ec.europa.eu/CITnet/jira/browse/ISAICP-3194.
+        elseif (isset($input['date']) && isset($input['time']) && (empty($input['date']) xor empty($input['time']))) {
+          $form_state->setError($element, t('Both date and time fields should be filled in the %field field.', ['%field' => $title]));
+        }
+        // If the date is invalid, set an error. A reminder of the required
+        // format in the message provides a good UX.
+        else {
+          $form_state->setError($element, t('The %field date is invalid. Please enter a date in the format %format.', ['%field' => $title, '%format' => static::formatExample($format)]));
+        }
+      }
+    }
+  }
+
+}
