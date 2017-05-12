26f101fccb2ca2e389758f395f495e50f3e1006e
Ilias Dimopoulos <idimopoulos@hotmail.com>
2017-05-12 16:07:05 +0200

    ISAICP-3225: Provide a more user friendly way of encoding the urls.

---
+++ web/modules/custom/rdf_entity/src/UriEncoder.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\rdf_entity;
+
+/**
+ * Handler class for the rdf uris.
+ *
+ * @package Drupal\rdf_entity
+ */
+class UriEncoder {
+
+  /**
+   * Encodes the url in a user friendly way.
+   *
+   * @param string $uri
+   *   The uri to be encoded.
+   *
+   * @return string
+   *   The encoded uri.
+   */
+  public static function encodeUrl($uri) {
+    $encode_chars = self::charsToEscape();
+    $keys = array_keys($encode_chars);
+    $replace = array_values($encode_chars);
+    return str_replace($keys, $replace, $uri);
+  }
+
+  /**
+   * Decodes a uri using the characters defined in this class.
+   *
+   * @param string $uri
+   *   The uri to be decoded.
+   *
+   * @return string
+   *   The decoded uri.
+   */
+  public static function decodeUrl($uri) {
+    $encode_chars = self::charsToEscape();
+    $keys = array_keys($encode_chars);
+    $replace = array_values($encode_chars);
+    return str_replace($replace, $keys, $uri);
+  }
+
+  /**
+   * A list of characters to be replaced in a Uri.
+   *
+   * The characters that are escaped are the valid characters allowed in a Uri.
+   *
+   * @return array
+   *   The array of characters where the index is the character to be encoded
+   *    and the value is the replacement.
+   *
+   * @see: https://tools.ietf.org/html/rfc3986#section-2
+   */
+  protected static function charsToEscape() {
+    return [
+      '_' => '_a',
+      '-' => '_b',
+      '.' => '_c',
+      '~' => '_d',
+      ':' => '_e',
+      '/' => '_f',
+      '?' => '_g',
+      '#' => '_h',
+      '[' => '_i',
+      ']' => '_j',
+      '@' => '_k',
+      '!' => '_l',
+      '$' => '_m',
+      '&' => '_n',
+      '\'' => '_o',
+      '(' => '_p',
+      ')' => '_q',
+      '*' => '_r',
+      '+' => '_s',
+      ',' => '_t',
+      ';' => '_u',
+      '=' => '_v',
+      '`' => '_w',
+    ];
+  }
+
+}
