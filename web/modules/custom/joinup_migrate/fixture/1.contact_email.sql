952325b242f327bb5ec2d08fc2e02f1829fc26f7
Claudiu Cristea <clau.cristea@gmail.com>
2017-05-18 08:02:29 +0300

    ISAICP-3103: Fix GROUP BY constraint.

--- web/modules/custom/joinup_migrate/fixture/1.contact_email.sql
+++ web/modules/custom/joinup_migrate/fixture/1.contact_email.sql
@@ -1,10 +1,9 @@
 CREATE OR REPLACE VIEW d8_contact_email (
   name
 ) AS
-SELECT
+SELECT DISTINCT
   c.field_project_common_contact_value
 FROM content_field_project_common_contact c
 INNER JOIN node p ON c.vid = p.vid
 INNER JOIN d8_solution s ON p.nid = s.nid
-GROUP BY c.field_project_common_contact_value
 ORDER BY c.field_project_common_contact_value ASC
