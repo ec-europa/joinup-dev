c0564d53588981418903cbae1ba00ac5215472ab
Francesco Sardara <francesco.sardara@one-agency.be>
2017-05-23 15:30:46 +0200

    Issue #ISAICP-2807: implement the anonymous download form to collect mails.

--- web/modules/custom/asset_distribution/src/Form/AnonymousDownloadForm.php
+++ web/modules/custom/asset_distribution/src/Form/AnonymousDownloadForm.php
@@ -2,13 +2,54 @@
 
 namespace Drupal\asset_distribution\Form;
 
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CloseModalDialogCommand;
+use Drupal\Core\Entity\ContentEntityStorageInterface;
+use Drupal\Core\EventSubscriber\AjaxResponseSubscriber;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\FileInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * Retrieves the e-mail from anonymous users that are downloading distributions.
  */
 class AnonymousDownloadForm extends FormBase {
+
+  /**
+   * The download_event entity storage.
+   *
+   * @var \Drupal\Core\Entity\ContentEntityStorageInterface
+   */
+  protected $eventStorage;
+
+  /**
+   * The file being downloaded.
+   *
+   * @var \Drupal\file\FileInterface
+   */
+  protected $file;
+
+  /**
+   * Instantiates a new AnonymousDownloadForm object.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityStorageInterface $event_storage
+   *   The download event entity storage.
+   */
+  public function __construct(ContentEntityStorageInterface $event_storage) {
+    $this->eventStorage = $event_storage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')->getStorage('download_event')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -20,18 +61,26 @@ class AnonymousDownloadForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state, FileInterface $file = NULL) {
+    $this->file = $file;
+
+    $form['#id'] = Html::getId($this->getFormId());
+
     $form['email'] = [
       '#type' => 'email',
       '#title' => $this->t('E-mail address'),
       '#description' => $this->t('Your e-mail address as <em>john.doe@domain.com</em>'),
       '#maxlength' => 180,
+      '#required' => TRUE,
     ];
 
     $form['actions'] = ['#type' => 'actions'];
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
+      '#ajax' => [
+        'wrapper' => $form['#id'],
+      ],
     ];
 
     return $form;
@@ -41,7 +90,21 @@ class AnonymousDownloadForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // @todo Implement this.
+    // Prevent any non-Javascript submits.
+    if (!$this->getRequest()->request->get(AjaxResponseSubscriber::AJAX_REQUEST_PARAMETER)) {
+      throw new BadRequestHttpException();
+    }
+
+    $event = $this->eventStorage->create([
+      'uid' => 0,
+      'mail' => $form_state->getValue('email'),
+      'file' => $this->file->id(),
+    ]);
+    $event->save();
+
+    $response = new AjaxResponse();
+    $response->addCommand(new CloseModalDialogCommand());
+    $form_state->setResponse($response);
   }
 
 }
