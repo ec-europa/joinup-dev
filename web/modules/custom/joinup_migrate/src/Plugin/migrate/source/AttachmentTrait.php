187cc38e719bf74570bcc3a743ea6ae2f22d557a
Sander Van Dooren <sander.vandooren@gmail.com>
2017-05-30 23:07:52 +0200

    HOTFIX #ISAICP-3443: Disable attachment migration as it blocks uat.

--- web/modules/custom/joinup_migrate/src/Plugin/migrate/source/AttachmentTrait.php
+++ web/modules/custom/joinup_migrate/src/Plugin/migrate/source/AttachmentTrait.php
@@ -16,15 +16,7 @@ trait AttachmentTrait {
    *   The source row.
    */
   protected function setAttachment(Row &$row) {
-    $fids = $this->select('d8_attachment', 'a')
-      ->fields('a', ['nid', 'delta'])
-      ->condition('a.nid', $row->getSourceProperty('nid'))
-      ->orderBy('a.delta', 'ASC')
-      ->execute()
-      ->fetchAll();
-    $fids = $fids ? array_map(function ($fid) {
-      return array_values($fid);
-    }, $fids) : NULL;
+    $fids = NULL;
     $row->setSourceProperty('fids', $fids);
   }
 
