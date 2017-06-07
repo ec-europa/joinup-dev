31940ada516787708068cbe02f8bf476cf21d4e2
Claudiu Cristea <clau.cristea@gmail.com>
2017-06-07 22:26:13 +0300

    ISAICP-2972: Add custom page migrations.

---
+++ web/modules/custom/joinup_migrate/src/Plugin/migrate/source/CustomPage.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\joinup_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+
+/**
+ * Migrates custom pages.
+ *
+ * @MigrateSource(
+ *   id = "custom_page"
+ * )
+ */
+class CustomPage extends JoinupSqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'nid' => [
+        'type' => 'integer',
+        'alias' => 'n',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return [
+      'nid' => $this->t('ID'),
+      'vid' => $this->t('Revision ID'),
+      'type' => $this->t('Type'),
+      'title' => $this->t('Title'),
+      'created' => $this->t('Created time'),
+      'changed' => $this->t('Changed time'),
+      'uid' => $this->t('Author ID'),
+      'body' => $this->t('Body'),
+      'collection' => $this->t('Collection'),
+      'fids' => $this->t('Attachments'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    return $this->select('d8_custom_page', 'n')->fields('n', [
+      'nid',
+      'vid',
+      'type',
+      'title',
+      'created',
+      'changed',
+      'uid',
+      'body',
+      'collection',
+    ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $fids = $this->select('upload', 'u')
+      ->fields('u', ['fid'])
+      ->condition('u.vid', $row->getSourceProperty('vid'))
+      ->execute()
+      ->fetchCol();
+    $row->setSourceProperty('fids', $fids);
+
+    return parent::prepareRow($row);
+  }
+
+}
