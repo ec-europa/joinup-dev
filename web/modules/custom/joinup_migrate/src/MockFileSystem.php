24e62441822123b16015948034dce72ff49a4744
Claudiu Cristea <clau.cristea@gmail.com>
2017-06-13 14:46:13 +0200

    ISAICP-3430: Fix also the File URL integration test.
    Automatically mock the file system after 'prepare' migration has run.

--- web/modules/custom/joinup_migrate/src/MockFileSystem.php
+++ web/modules/custom/joinup_migrate/src/MockFileSystem.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\joinup_migrate;
 
-use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\Database;
 
 /**
  * Helper method to create a fake file system to import from (used for testing).
@@ -14,30 +14,16 @@ class MockFileSystem {
    *
    * Used for testing purposes.
    *
-   * @param string $base_dir
-   *   The base dir for the filesystem  mock.
-   * @param \Drupal\Core\Database\Connection $db
-   *   The database connection from where to read the data. It's the source DB.
+   * @return string
+   *   The absolute path to the mocked file system.
    */
-  public static function createTestingFiles($base_dir, Connection $db) {
-    $tables = [
-      'd8_file_collection_logo',
-      'd8_file_comment_attachment',
-      'd8_file_custom_page_attachment',
-      'd8_file_discussion_attachment',
-      'd8_file_distribution',
-      'd8_file_document_case',
-      'd8_file_document_document',
-      'd8_file_document_factsheet',
-      'd8_file_document_presentation',
-      'd8_file_documentation_release',
-      'd8_file_documentation_solution',
-      'd8_file_event_attachment',
-      'd8_file_event_logo',
-      'd8_file_news_attachment',
-      'd8_file_solution_logo',
-      'd8_file_user_photo',
-    ];
+  public static function createTestingFiles() {
+    /** @var \Drupal\Core\File\FileSystemInterface $file_system */
+    $file_system = \Drupal::service('file_system');
+    $public_directory = \Drupal::service('stream_wrapper.public')->getDirectoryPath();
+    $base_dir = "$public_directory/joinup_migrate/files";
+    $db = Database::getConnection('default', 'migrate');
+    $tables = $db->query("SHOW TABLES LIKE 'd8\_file\_%'")->fetchCol();
 
     $files = [];
     foreach ($tables as $table) {
@@ -53,8 +39,10 @@ class MockFileSystem {
       );
     }
 
-    /** @var \Drupal\Core\File\FileSystemInterface $file_system */
-    $file_system = \Drupal::service('file_system');
+    // Delete the base dir, if exists.
+    if (is_dir($base_dir)) {
+      file_unmanaged_delete_recursive($base_dir);
+    }
 
     foreach ($files as $file) {
       $path_parts = pathinfo($file);
@@ -72,6 +60,8 @@ class MockFileSystem {
         touch($file_path);
       }
     }
+
+    return $file_system->realpath($base_dir);
   }
 
 }
