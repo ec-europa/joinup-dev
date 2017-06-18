3250118503444784ab498b0d9cb97a4dad9c30d2
Claudiu Cristea <clau.cristea@gmail.com>
2017-06-19 00:08:26 +0300

    ISAICP-3504: Add the 'file:inline' migration.

--- web/modules/custom/joinup_migrate/tests/src/Functional/assert/file_inline.php
+++ web/modules/custom/joinup_migrate/tests/src/Functional/assert/file_inline.php
@@ -7,5 +7,5 @@
 
 // This image is embedded in content of comment with CID 15208 as
 // '/sites/default/files/ckeditor_files/images/sd-dss-admin-service-url.png'.
-$file = $this->loadEntityByLabel('file', 'public://inline-images/sd-dss-admin-service-url.png');
+$file = $this->loadEntityByLabel('file', 'sd-dss-admin-service-url.png');
 $this->assertNotEmpty($file);
