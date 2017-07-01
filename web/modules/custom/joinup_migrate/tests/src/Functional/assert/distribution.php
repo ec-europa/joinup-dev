607d930ba9958f9a83269a649245ebce9ae570bd
Claudiu Cristea <clau.cristea@gmail.com>
2017-07-02 00:02:49 +0300

    ISAICP-3542: Wrong var name in distribution test.

--- web/modules/custom/joinup_migrate/tests/src/Functional/assert/distribution.php
+++ web/modules/custom/joinup_migrate/tests/src/Functional/assert/distribution.php
@@ -10,7 +10,7 @@ $distribution = $this->loadEntityByLabel('rdf_entity', 'Release Notes 2.2.4', 'a
 $this->assertEquals('Release Notes 2.2.4', $distribution->label());
 $this->assertEquals('asset_distribution', $distribution->bundle());
 $this->assertEquals('default', $distribution->graph->value);
-$this->assertEquals(gmdate('Y-m-d\TH:i:s', 1426517881), $release->field_ad_creation_date->value);
+$this->assertEquals(gmdate('Y-m-d\TH:i:s', 1426517881), $distribution->field_ad_creation_date->value);
 $translation = $distribution->getTranslation('cs');
 $this->assertEquals('DRUH STÁTNÍ SLUŽBY/DOBA TRVÁNÍ PRACOVNÍHO POMĚRU', $translation->field_ad_description->value);
 $this->assertEquals('Číselník - TYP PŘEDPISU PRO VZNIK/ZÁNIK SYSTEMIZOVANÉHO MÍSTA', $translation->label());
