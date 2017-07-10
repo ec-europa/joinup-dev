ce386161bc4202c19294fe6b2773f2f367cc9722
Claudiu Cristea <clau.cristea@gmail.com>
2017-07-10 22:11:50 +0300

    ISAICP-3547: Collection author has already created a membership.

--- web/modules/custom/joinup_migrate/src/Plugin/migrate/source/OgUserRole.php
+++ web/modules/custom/joinup_migrate/src/Plugin/migrate/source/OgUserRole.php
@@ -39,6 +39,7 @@ class OgUserRole extends SourcePluginBase {
       'roles' => $this->t('OG roles'),
       'created' => $this->t('Created'),
       'state' => $this->t('State'),
+      'id' => $this->t('Membership ID'),
     ];
   }
 
@@ -60,9 +61,9 @@ class OgUserRole extends SourcePluginBase {
 
     $rows = [];
     foreach ($query->execute()->fetchAll() as $data) {
-      $collection = $data->collection;
+      $collection_name = $data->collection;
       $uid = (int) $data->uid;
-      $key = "$collection:$uid";
+      $key = "$collection_name:$uid";
 
       // In the case when we have multiple rows with "Owner == 'Y'", we pickup
       // only the first occurrence. For this reason we key the $rows array with
@@ -71,16 +72,16 @@ class OgUserRole extends SourcePluginBase {
         if (!isset($multiple_owner_rows_reported[$key])) {
           // Log only once, even there more than 2 owner rows per collection.
           $this->migration->getIdMap()->saveMessage([
-            'collection' => $collection,
+            'collection' => $collection_name,
             'uid' => $uid,
-          ], "Collection '$collection' has multiple rows with Owner == 'Y'");
+          ], "Collection '$collection_name' has multiple rows with Owner == 'Y'");
         }
         $multiple_owner_rows_reported[$key] = TRUE;
         continue;
       }
 
       $row = [
-        'collection' => $collection,
+        'collection' => $collection_name,
         'uid' => $uid,
         'state' => $data->is_active ? OgMembershipInterface::STATE_ACTIVE : OgMembershipInterface::STATE_PENDING,
         'created' => (int) $data->created,
@@ -99,10 +100,10 @@ class OgUserRole extends SourcePluginBase {
     $query = $db->select('d8_prepare', 'p')
       ->fields('p', ['collection', 'collection_owner'])
       ->isNotNull('p.collection_owner');
-    foreach ($query->execute()->fetchAllKeyed() as $collection => $collection_owner) {
+    foreach ($query->execute()->fetchAllKeyed() as $collection_name => $collection_owner) {
       $uids = array_map('intval', explode(',', $collection_owner));
-      foreach ($uids as $uid) {
-        $key = "$collection:$uid";
+      foreach ($uids as $delta => $uid) {
+        $key = "$collection_name:$uid";
         // Membership already added.
         if (isset($rows[$key])) {
           $rows[$key]['roles']['owner'] = 'rdf_entity-collection-administrator';
@@ -112,7 +113,7 @@ class OgUserRole extends SourcePluginBase {
         // New membership.
         else {
           $rows[$key] = [
-            'collection' => $collection,
+            'collection' => $collection_name,
             'uid' => $uid,
             'state' => OgMembershipInterface::STATE_ACTIVE,
             'roles' => [
@@ -124,8 +125,27 @@ class OgUserRole extends SourcePluginBase {
       }
     }
 
-    // Remove keys from roles.
-    array_walk($rows, function (array &$row) {
+    // Get all existing memberships using a SQL query to speed up the process.
+    $existing_memberships = [];
+    if (Database::getConnection()->schema()->tableExists('migrate_map_collection')) {
+      /** @var \Drupal\Core\Database\Query\SelectInterface $query */
+      $query = Database::getConnection()->select('og_membership', 'og')
+        ->condition('og.entity_type', 'rdf_entity');
+      $query->join('migrate_map_collection', 'c', "og.entity_id = c.destid1");
+      $query->addField('og', 'id');
+      $query->addExpression("CONCAT_WS(':', c.sourceid1, og.uid)");
+      $existing_memberships = $query->execute()->fetchAllKeyed(1, 0);
+    }
+
+    array_walk($rows, function (array &$row, $key) use ($existing_memberships) {
+      // It's possible that a membership has been already created for this user,
+      // as an effect of his collection authorship. In this case we add also the
+      // membership ID to the row, so the membership will be updated instead of
+      // being created.
+      if (isset($existing_memberships[$key])) {
+        $row['id'] = (int) $existing_memberships[$key];
+      }
+      // Remove keys from roles array.
       $row['roles'] = array_values($row['roles']);
     });
 
