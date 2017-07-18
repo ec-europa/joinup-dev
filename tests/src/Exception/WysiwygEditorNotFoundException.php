0477ae8b273993c8ad687b6c464a45c7b96880a6
Pieter Frenssen <pieter@frenssen.be>
2017-07-18 14:19:07 +0300

    Revert "ISAICP-3595: Add a test."
    This reverts commit 1a0f69a0826af990cb5b0604c5be4c7948bf21d1.

--- tests/src/Exception/WysiwygEditorNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\joinup\Exception;
-
-/**
- * Exception thrown when a WYSIWYG editor is not found in the page.
- */
-class WysiwygEditorNotFoundException extends \Exception {
-}
