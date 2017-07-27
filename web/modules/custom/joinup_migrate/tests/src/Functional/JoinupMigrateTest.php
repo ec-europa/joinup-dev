2bb651ca952e7dfc29efef22da468de8e897fd38
Claudiu Cristea <clau.cristea@gmail.com>
2017-07-27 20:35:41 +0300

    4th

--- web/modules/custom/joinup_migrate/tests/src/Functional/JoinupMigrateTest.php
+++ web/modules/custom/joinup_migrate/tests/src/Functional/JoinupMigrateTest.php
@@ -10,6 +10,7 @@ use Drupal\migrate\MigrateExecutable;
 use Drupal\migrate\MigrateMessageInterface;
 use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\redirect\Entity\Redirect;
+use Drupal\search_api\Entity\Index;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\Tests\rdf_entity\Traits\RdfDatabaseConnectionTrait;
 use Drupal\Tests\rdf_entity\Traits\EntityUtilityTrait;
@@ -196,6 +197,10 @@ class JoinupMigrateTest extends BrowserTestBase implements MigrateMessageInterfa
     chmod($settings_file, 0444);
 
     $this->manager = $this->container->get('plugin.manager.migration');
+
+    foreach (['published', 'unpublished'] as $index) {
+      Index::load($index)->setOption('index_directly', 0)->save();
+    }
   }
 
   /**
