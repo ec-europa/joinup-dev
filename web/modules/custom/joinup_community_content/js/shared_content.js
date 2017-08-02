3d5c24ec0a3156fed350b40be734078ec87d6e2d
Francesco Sardara <francesco.sardara@one-agency.be>
2017-08-02 17:29:32 +0200

    Issue #ISAICP-2219: some cleanup on the shared library javascript.

--- web/modules/custom/joinup_community_content/js/shared_content.js
+++ web/modules/custom/joinup_community_content/js/shared_content.js
@@ -17,7 +17,7 @@
   Drupal.theme.sharedContentCue = function (collection) {
     var label = Drupal.t('Shared from @collection', { '@collection': collection });
 
-    return '<div class="listing__stat" title="' + label + '"><div class="listing__icon icon icon--shared"></div></div>'
+    return '<div class="listing__stat" title="' + label + '"><div class="listing__icon icon icon--shared"></div></div>';
   };
 
   /**
@@ -47,7 +47,7 @@
           return;
         }
 
-        $(this).find('.listing__stats').append(Drupal.theme('sharedContentCue', $this.data('drupal-parent-label')));
+        $this.find('.listing__stats').append(Drupal.theme('sharedContentCue', $this.data('drupal-parent-label')));
       });
     }
   };
