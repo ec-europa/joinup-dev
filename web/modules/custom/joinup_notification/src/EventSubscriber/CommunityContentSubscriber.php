5b84cfe2f50a7c07557852f3aec1692671d3ba26
Ilias Dimopoulos <idimopoulos@hotmail.com>
2017-08-18 14:53:00 +0200

    ISAICP-3508: Send a deletion message when the entity is published as well. Send the appropriate message in each case.

--- web/modules/custom/joinup_notification/src/EventSubscriber/CommunityContentSubscriber.php
+++ web/modules/custom/joinup_notification/src/EventSubscriber/CommunityContentSubscriber.php
@@ -188,12 +188,15 @@ class CommunityContentSubscriber extends NotificationSubscriberBase implements E
       return;
     }
 
-    if (empty($this->config[$this->workflow->getId()][$this->entity->get($this->stateField)->first()->value])) {
+    $state = $this->entity->get($this->stateField)->first()->value;
+    if (empty($this->config[$this->workflow->getId()][$state])) {
       return;
     }
 
+    $transition_action = $state === 'deletion_request' ? t('approved your request of deletion for') : t('deleted') ;
     $user_data = $this->getUsersMessages($this->config[$this->workflow->getId()][$this->entity->get($this->stateField)->first()->value]);
-    $this->sendUserDataMessages($user_data);
+    $arguments = ['@transition:request_action:past' => $transition_action];
+    $this->sendUserDataMessages($user_data, $arguments);
   }
 
   /**
