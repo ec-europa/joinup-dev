51bb4b256bfee3ebdf6d03d16f824a34db386672
Ilias Dimopoulos <idimopoulos@hotmail.com>
2017-08-24 17:48:03 +0200

    ISAICP-3724: Add a javascript library for the destination parameter on the 'Sign in' menu link item.

---
+++ web/modules/custom/joinup_core/js/sign_in_destination.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * Attaches a destination parameter to the sign in link towards the current page.
+ */
+
+(function ($, Drupal, drupalSettings) {
+
+  /**
+   * Alters the sign in link to have a destination parameter to the source page.
+   *
+   * The home page 'Sign in' link and the 'Sign in' page 'Sign in' link are not altered.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches shared content behaviors.
+   */
+  Drupal.behaviors.signInDestination = {
+    attach: function (context) {
+      var path = drupalSettings.path;
+      if (path.isFront || path.currentPath === 'user/login') {
+          return;
+      }
+
+      $(context).find('[data-drupal-link-system-path="user/login"]').once('sign-in-redirect').each(function () {
+          var $this = $(this);
+          if($this.prop('href').indexOf('?destination=') === -1) {
+              var query_string = (drupalSettings.path.currentQuery) ? '?' + $.param(drupalSettings.path.currentQuery) : '';
+              var destination = encodeURIComponent(drupalSettings.path.currentPath + query_string)
+              $this.prop('href', $this.prop('href') + '?destination=/' + destination);
+          }
+      });
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
