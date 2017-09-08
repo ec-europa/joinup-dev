02c373e8a68a91216299d52e6be5bab44c7cd9ca
Claudiu Cristea <clau.cristea@gmail.com>
2017-09-08 20:09:01 +0300

    ISAICP-3678: Consolodate spatial coverage migration.

--- web/modules/custom/joinup_migrate/src/Plugin/migrate/source/CountryTrait.php
+++ web/modules/custom/joinup_migrate/src/Plugin/migrate/source/CountryTrait.php
@@ -8,21 +8,32 @@ namespace Drupal\joinup_migrate\Plugin\migrate\source;
 trait CountryTrait {
 
   /**
-   * Source country hierarchy.
+   * Country correction mapping.
    *
    * @var array
    */
-  protected static $countryHierarchy;
-
-  /**
-   * Country mapping.
-   *
-   * @var array
-   */
-  protected $countryCorrection = [
+  protected static $countryCorrection = [
+    'Africa' => NULL,
+    'Asia' => NULL,
+    'British Antarctic Territory' => 'Antarctica',
+    'Central and South America' => NULL,
+    "Cote d'Ivoire" => 'Côte d’Ivoire',
+    'EU Institutions' => 'European Union',
+    'Europe' => NULL,
     'Faroe Islands' => 'Faroes',
     'Fyrom' => 'Former Yugoslav Republic of Macedonia',
+    'Gambia' => 'The Gambia',
+    'Gilbert and Ellice Islands' => ['Kiribati', 'Tuvalu'],
+    'International Organizations' => NULL,
+    'Midway Islands' => 'United States Minor Outlying Islands',
+    'North America' => NULL,
+    'Other' => NULL,
+    'Pan European' => NULL,
+    'Queen Maud Land' => 'Antarctica',
+    'Sao Tome and Principe' => 'São Tomé and Príncipe',
     'South Korea/The Republic of Korea' => 'South Korea',
+    'US Miscellaneous Pacific Islands' => 'United States Minor Outlying Islands',
+    'Wake Islands' => 'United States Minor Outlying Islands',
   ];
 
   /**
@@ -30,68 +41,40 @@ trait CountryTrait {
    *
    * @param int[] $vids
    *   A list of node revision IDs.
-   * @param bool $include_continent_countries
-   *   (optional) If continents will be expanded to their countries. If the
-   *   value is FALSE, continent countries will be ignored. Defaults to TRUE.
    *
    * @return string[]
    *   A list of country names.
    */
-  protected function getCountries(array $vids, $include_continent_countries = TRUE) {
+  protected function getCountries(array $vids) {
     if (empty($vids)) {
       return [];
     }
 
     $query = $this->select('term_node', 'tn')
+      ->distinct()
       ->fields('td', ['name'])
       // The country vocabulary has vid equals 26.
       ->condition('td.vid', 26)
       ->condition('tn.vid', $vids, 'IN');
     $query->join('term_data', 'td', 'tn.tid = td.tid');
-
-    $terms = [];
-    foreach ($query->execute()->fetchCol() as $term) {
-      if ($include_continent_countries && $countries = $this->getCountriesByContinent($term)) {
-        // Replace continents with their component countries.
-        $terms = array_merge($terms, $countries);
-      }
-      elseif (!in_array($term, $terms)) {
-        $terms[] = $term;
-      }
-    }
+    $terms = $query->execute()->fetchCol();
 
     // Corrections.
-    return array_map(function ($term) {
-      return isset($this->countryCorrection[$term]) ? $this->countryCorrection[$term] : $term;
-    }, array_unique($terms));
-  }
-
-  /**
-   * Gets the country hierarchy.
-   *
-   * @param string $continent
-   *   The continent.
-   *
-   * @return array[]
-   *   Associative array keyed by continent and having a list of component
-   *   countries as values.
-   */
-  protected function getCountriesByContinent($continent) {
-    if (!isset(static::$countryHierarchy)) {
-      // Populate the source country hierarchy.
-      $query = $this->select('term_data', 'd')
-        ->fields('d', ['name'])
-        ->condition('d.vid', 26)
-        ->condition('h.parent', 0, '>');
-      $query->join('term_hierarchy', 'h', 'd.tid = h.tid');
-      $query->join('term_data', 'd1', 'h.parent = d1.tid');
-      $query->addExpression('d1.name', 'parent');
-      $query->orderBy('d1.name')->orderBy('d.name');
-      foreach ($query->execute()->fetchAll() as $country) {
-        static::$countryHierarchy[$country['parent']][] = $country['name'];
+    $countries = [];
+    foreach ($terms as $term) {
+      if (array_key_exists($term, static::$countryCorrection)) {
+        if (!$country = static::$countryCorrection[$term]) {
+          // This country should not be migrated.
+          continue;
+        }
+        $countries = array_merge($countries, (array) $country);
+      }
+      else {
+        $countries[] = $term;
       }
     }
-    return isset(static::$countryHierarchy[$continent]) ? static::$countryHierarchy[$continent] : NULL;
+
+    return $countries;
   }
 
 }
