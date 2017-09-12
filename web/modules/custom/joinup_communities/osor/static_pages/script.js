b89b6effc5c31e54231c560ff7bfce6bc741cf9e
Kalin Chernev <kalin.chernev@gmail.com>
2017-09-12 17:35:54 +0200

    ISAICP-3838: phpcs

--- web/modules/custom/joinup_communities/osor/static_pages/script.js
+++ web/modules/custom/joinup_communities/osor/static_pages/script.js
@@ -1,17 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
+/**
+ * @file
+ */
+
+document.addEventListener("DOMContentLoaded", function () {
   var actions = document.querySelectorAll("[id*='action-']");
-  actions.forEach(function(action) {
+  actions.forEach(function (action) {
     var actionId = action.getAttribute("id").split("-")[1];
     var reaction = "reaction-" + actionId;
     var reactionEl = document.getElementById(reaction);
     var substitude = "substitute-" + actionId;
     var substitudeEl = document.getElementById(substitude);
     if (reactionEl && substitudeEl) {
-      action.addEventListener("mouseover", function() {
+      action.addEventListener("mouseover", function () {
         reactionEl.style.display = "block";
         substitudeEl.style.display = "none";
       });
-      action.addEventListener("mouseleave", function() {
+      action.addEventListener("mouseleave", function () {
         reactionEl.style.display = "none";
         substitudeEl.style.display = "block";
       });
