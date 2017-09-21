dd06457ff42dab56689edd3aa97ae25f79a30244
Pieter Frenssen <pieter@frenssen.be>
2017-09-21 15:32:38 +0300

    ISAICP-3917: Add a script to update the production environment.

---
+++ scripts/update_production.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script will perform updates on the production environment.
+
+# Define paths.
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT=$(realpath ${SCRIPT_PATH}/..)
+
+# Perform the necessary steps for the update
+cd ${PROJECT_ROOT}
+./vendor/bin/drush updatedb --yes &&
+./vendor/bin/drush config-import --yes &&
+./vendor/bin/drush entity-updates --yes &&
+./vendor/bin/drush cache-rebuild --yes
+
+# Check if any of the steps returned an error.
+STATUS=$?
+if [ ${STATUS} -ne 0 ]; then
+  echo "An error occurred during the update."
+  exit ${STATUS}
+fi
+
+# Check if there are any errors or warnings reported.
+ERROR_COUNT=$(./vendor/bin/drush status-report --severity=1 --field=severity | wc -l)
+if [ ${ERROR_COUNT} -ne 0 ]; then
+  echo "Errors or warnings are reported after the update:"
+  ./vendor/bin/drush status-report --severity=1
+  exit 1
+fi
+
+echo "Update successfully completed. No errors or warnings reported."
+exit 0
