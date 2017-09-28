ee8d0ce2350ed08bcf643f17187e18f26489bf55
Kalin Chernev <kalin.chernev@gmail.com>
2017-09-28 16:06:31 +0200

    ISAICP-3932: add link

--- web/profiles/joinup/src/Plugin/Block/RecommendedContentBlock.php
+++ web/profiles/joinup/src/Plugin/Block/RecommendedContentBlock.php
@@ -97,7 +97,7 @@ class RecommendedContentBlock extends BlockBase implements ContainerFactoryPlugi
     $build = [
       'header' => [
         '#type' => 'inline_template',
-        '#template' => '<p>{% trans %}Joinup is a collaborative platform created by the European Commission and funded by the European Union via the Interoperability solutions for public administrations, businesses and citizens (ISA2) Programme. It offers several services that aim to help e-Government professionals share their experience with each other. We also hope to support them to find, choose, re-use, develop and implement interoperability solutions.{% endtrans %}</p>',
+        '#template' => '<p>{% trans %}Joinup is a collaborative platform created by the European Commission and funded by the European Union via the <a href="https://ec.europa.eu/isa2/">Interoperability solutions for public administrations, businesses and citizens</a> (ISA<sup>2</sup>) Programme. It offers several services that aim to help e-Government professionals share their experience with each other. We also hope to support them to find, choose, re-use, develop and implement interoperability solutions.{% endtrans %}</p>',
       ],
     ];
 
