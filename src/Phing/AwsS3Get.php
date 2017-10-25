5c719bfd67d65668d6554d877a5933aec2e21307
Pieter Frenssen <pieter@frenssen.be>
2017-10-25 15:55:16 +0200

    ISAICP-3994: Progressing on the Phing targets for retrieving database dumps.

--- src/Phing/AwsS3Get.php
+++ src/Phing/AwsS3Get.php
@@ -1,22 +1,63 @@
 <?php
 
-/**
- * @file
- * Contains \DrupalProject\build\Phing\AwsS3Get.
- */
+declare(strict_types = 1);
 
 namespace DrupalProject\Phing;
 
 use Aws\S3\S3Client;
 
 /**
- * Class AfterFixturesImportCleanup.
+ * Phing task to download a file from an AWS S3 bucket.
  */
 class AwsS3Get extends \Task {
-  protected $object;
-  protected $target;
+
+  /**
+   * The name of the S3 bucket.
+   *
+   * @var string
+   */
   protected $bucket;
+
+  /**
+   * The AWS region where the bucket is located.
+   *
+   * @var string
+   */
+  protected $region;
+
+  /**
+   * The path to the file in the S3 bucket to download.
+   *
+   * @var string
+   */
+  protected $object;
+
+  /**
+   * The local path to the file to save, including filename.
+   *
+   * @var string
+   */
+  protected $target;
+
+  /**
+   * The version of the file to download. Defaults to 'latest'.
+   *
+   * @var string
+   */
+  protected $version = 'latest';
+
+  /**
+   * The authentication key.
+   *
+   * @var string
+   */
   protected $key;
+
+  /**
+   * The authentication secret.
+   *
+   * @var string
+   */
   protected $secret;
 
   /**
@@ -24,10 +65,11 @@ class AwsS3Get extends \Task {
    */
   public function main() {
     $options = [
-      'version' => 'latest',
-      'region' => 'eu-west-1',
+      'version' => $this->version,
+      'region' => $this->region,
     ];
-    // If not set, credentials will be used from the environment.
+    // If not set, credentials will be retrieved from the environment.
+    // @see \Aws\Credentials\CredentialProvider
     if (!empty($this->key) && !empty($this->secret)) {
       $options['credentials'] = [
         'key' => $this->key,
@@ -44,40 +86,73 @@ class AwsS3Get extends \Task {
   }
 
   /**
-   * @param mixed $object
+   * Sets the name of the S3 bucket.
+   *
+   * @param string $bucket
+   *   The name of the S3 bucket.
    */
-  public function setObject($object) {
-    $this->object = $object;
-  }
-
-  /**
-   * @param mixed $target
-   */
-  public function setTarget($target) {
-    $this->target = $target;
-  }
-
-  /**
-   * @param mixed $bucket
-   */
-  public function setBucket($bucket) {
+  public function setBucket(string $bucket) {
     $this->bucket = $bucket;
   }
 
   /**
-   * @param mixed $key
+   * Sets the AWS region where the bucket is located.
+   *
+   * @param string $region
+   *   The AWS region where the bucket is located.
    */
-  public function setKey($key) {
+  public function setRegion(string $region) {
+    $this->region = $region;
+  }
+
+  /**
+   * Sets the path to the file in the S3 bucket to download.
+   *
+   * @param string $object
+   *   The path to the file in the S3 bucket to download.
+   */
+  public function setObject(string $object) {
+    $this->object = $object;
+  }
+
+  /**
+   * Sets the local path to the file to save, including filename.
+   *
+   * @param string $target
+   *   The local path to the file to save, including filename.
+   */
+  public function setTarget(string $target) {
+    $this->target = $target;
+  }
+
+  /**
+   * Sets the version of the file to download.
+   *
+   * @param string $version
+   *   The version of the file to download. Defaults to 'latest'.
+   */
+  public function setVersion(string $version = 'latest') {
+    $this->version = $version;
+  }
+
+  /**
+   * Sets the authentication key.
+   *
+   * @param string $key
+   *   The authentication key.
+   */
+  public function setKey(string $key = NULL) {
     $this->key = $key;
   }
 
   /**
-   * @param mixed $secret
+   * Sets the authentication secret.
+   *
+   * @param string $secret
+   *   The authentication secret.
    */
-  public function setSecret($secret) {
+  public function setSecret(string $secret = NULL) {
     $this->secret = $secret;
   }
-
-
 
 }
