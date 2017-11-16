1f40b65a1bf0bfc2dc1c1e5e942aa3e77988e514
Francesco Sardara <francesco.sardara@one-agency.be>
2017-11-16 17:07:43 +0100

    Issue #ISAICP-4054: rework the pin controller to support both solutions and community content.

--- web/profiles/joinup/src/Controller/PinEntityController.php
+++ web/profiles/joinup/src/Controller/PinEntityController.php
@@ -3,19 +3,29 @@
 namespace Drupal\joinup\Controller;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\joinup_community_content\CommunityContentHelper;
 use Drupal\joinup_core\JoinupRelationManager;
 use Drupal\node\NodeInterface;
 use Drupal\og\OgAccessInterface;
+use Drupal\rdf_entity\RdfInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Controller to pin/unpin community content.
+ * Controller to pin/unpin entities inside collections.
  */
 class PinEntityController extends ControllerBase {
+
+  /**
+   * The field that holds the collections where a solution is pinned in.
+   *
+   * @var string
+   */
+  const SOLUTION_PIN_FIELD = 'field_is_pinned_in';
 
   /**
    * The OG access service.
@@ -55,108 +65,259 @@ class PinEntityController extends ControllerBase {
   }
 
   /**
-   * Pins a node inside the collection it belongs.
+   * Pins a group content entity inside a collection.
    *
-   * @param \Drupal\node\NodeInterface $node
-   *   The node entity being pinned.
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The content entity being pinned.
    *
    * @return \Symfony\Component\HttpFoundation\RedirectResponse
    *   The redirect response.
    */
-  public function pin(NodeInterface $node) {
-    $node->setSticky(TRUE)->save();
+  public function pin(ContentEntityInterface $entity) {
+    $collections = $this->getCollections($entity);
+
+    if (count($collections) > 1) {
+      // @todo Show a form to choose where to pin.
+    }
+
+    $collection = reset($collections);
+    $this->setEntitySticky($entity, $collection, TRUE);
 
     drupal_set_message($this->t('@bundle %title has been pinned in the collection %collection.', [
-      '@bundle' => $node->get('type')->entity->label(),
-      '%title' => $node->label(),
-      '%collection' => $this->relationManager->getParent($node)->label(),
+      '@bundle' => $entity->get($entity->getEntityType()->getKey('bundle'))->entity->label(),
+      '%title' => $entity->label(),
+      '%collection' => $collection->label(),
     ]));
 
-    return $this->getRedirect($node);
+    return $this->getRedirect($collection);
   }
 
   /**
-   * Unpins a node inside the collection it belongs.
+   * Unpins a group content entity inside a collection.
    *
-   * @param \Drupal\node\NodeInterface $node
-   *   The node entity being unpinned.
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The content entity being unpinned.
    *
    * @return \Symfony\Component\HttpFoundation\RedirectResponse
    *   The redirect response.
    */
-  public function unpin(NodeInterface $node) {
-    $node->setSticky(FALSE)->save();
+  public function unpin(ContentEntityInterface $entity) {
+    $collections = $this->getCollections($entity);
+
+    if (count($collections) > 1) {
+      // @todo Show a form to choose where to unpin.
+    }
+
+    $collection = reset($collections);
+    $this->setEntitySticky($entity, $collection, FALSE);
 
     drupal_set_message($this->t('@bundle %title has been unpinned in the collection %collection.', [
-      '@bundle' => $node->get('type')->entity->label(),
-      '%title' => $node->label(),
-      '%collection' => $this->relationManager->getParent($node)->label(),
+      '@bundle' => $entity->get($entity->getEntityType()->getKey('bundle'))->entity->label(),
+      '%title' => $entity->label(),
+      '%collection' => $collection->label(),
     ]));
 
-    return $this->getRedirect($node);
+    return $this->getRedirect($collection);
   }
 
   /**
    * Access check for the pin route.
    *
-   * A node can be pinned only if it's not pinned, if its bundle is a community
-   * content one and if the user is a facilitator in the parent collection.
-   *
-   * @param \Drupal\node\NodeInterface $node
-   *   The node entity being pinned.
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The content entity being pinned.
    * @param \Drupal\Core\Session\AccountInterface $account
    *   The user account to check access for.
    *
    * @return \Drupal\Core\Access\AccessResult
    *   The access result.
    */
-  public function pinAccess(NodeInterface $node, AccountInterface $account) {
-    $collection = $this->relationManager->getParent($node);
+  public function pinAccess(ContentEntityInterface $entity, AccountInterface $account) {
+    if (!$this->isSolution($entity) && !$this->isCommunityContent($entity)) {
+      return AccessResult::forbidden();
+    }
 
-    return AccessResult::allowedIf(
-      !empty($collection) &&
-      $collection->bundle() === 'collection' &&
-      !$node->isSticky() &&
-      in_array($node->bundle(), CommunityContentHelper::getBundles())
-    )->andIf($this->ogAccess->userAccess($collection, 'pin community content', $account));
+    $collections = $this->getCollections($entity);
+    if (empty($collections)) {
+      return AccessResult::forbidden();
+    }
+
+    $cacheable_metadata = new CacheableMetadata();
+    $cacheable_metadata->addCacheableDependency($entity);
+
+    // Check if there is any collection where the entity can be pinned.
+    foreach ($collections as $collection) {
+      if (!$this->isEntitySticky($entity, $collection)) {
+        // @todo merge all the cache metadata from each access check.
+        $access = $this->ogAccess->userAccess($collection, 'pin group content', $account);
+        if ($access->isAllowed()) {
+          return $access->addCacheableDependency($cacheable_metadata);
+        }
+      }
+    }
+
+    return AccessResult::neutral()->addCacheableDependency($cacheable_metadata);
   }
 
   /**
    * Access check for the unpin route.
    *
-   * A node can be unpinned only if it's pinned, if its bundle is a community
-   * content one and if the user is a facilitator in the parent collection.
-   *
-   * @param \Drupal\node\NodeInterface $node
-   *   The node entity being unpinned.
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The content entity being unpinned.
    * @param \Drupal\Core\Session\AccountInterface $account
    *   The user account to check access for.
    *
    * @return \Drupal\Core\Access\AccessResult
    *   The access result.
    */
-  public function unpinAccess(NodeInterface $node, AccountInterface $account) {
-    $collection = $this->relationManager->getParent($node);
+  public function unpinAccess(ContentEntityInterface $entity, AccountInterface $account) {
+    if (!$this->isSolution($entity) && !$this->isCommunityContent($entity)) {
+      return AccessResult::forbidden();
+    }
 
-    return AccessResult::allowedIf(
-      !empty($collection) &&
-      $collection->bundle() === 'collection' &&
-      $node->isSticky() &&
-      in_array($node->bundle(), CommunityContentHelper::getBundles())
-    )->andIf($this->ogAccess->userAccess($collection, 'unpin community content', $account));
+    $collections = $this->getCollections($entity);
+    if (empty($collections)) {
+      return AccessResult::forbidden();
+    }
+
+    $cacheable_metadata = new CacheableMetadata();
+    $cacheable_metadata->addCacheableDependency($entity);
+
+    // Check if there is any collection where the entity can be unpinned.
+    foreach ($collections as $collection) {
+      if ($this->isEntitySticky($entity, $collection)) {
+        // @todo merge all the cache metadata from each access check.
+        $access = $this->ogAccess->userAccess($collection, 'unpin group content', $account);
+        if ($access->isAllowed()) {
+          return $access->addCacheableDependency($cacheable_metadata);
+        }
+      }
+    }
+
+    return AccessResult::neutral()->addCacheableDependency($cacheable_metadata);
   }
 
   /**
-   * Returns a response to redirect the user to the collection of the node.
+   * Gets the collections an entity is related to.
    *
-   * @param \Drupal\Core\Entity\EntityInterface $node
-   *   The node being handled.
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The content entity.
+   *
+   * @return \Drupal\rdf_entity\RdfInterface[]
+   *   A list of collections the entity is related.
+   */
+  protected function getCollections(ContentEntityInterface $entity) {
+    $collections = [];
+
+    if ($this->isSolution($entity)) {
+      $collections = $entity->get('collection')->referencedEntities();
+    }
+    elseif ($this->isCommunityContent($entity)) {
+      $collections = [$this->relationManager->getParent($entity)];
+    }
+
+    return $collections;
+  }
+
+  /**
+   * Returns whether the entity is an rdf solution.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The entity to check.
+   *
+   * @return bool
+   *   True if the entity is an rdf of bundle solution, false otherwise.
+   */
+  protected function isSolution(ContentEntityInterface $entity) {
+    return $entity instanceof RdfInterface && $entity->bundle() === 'solution';
+  }
+
+  /**
+   * Returns whether the entity is a community content node.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The entity to check.
+   *
+   * @return bool
+   *   True if the entity is a community content node, false otherwise.
+   */
+  protected function isCommunityContent(ContentEntityInterface $entity) {
+    return $entity instanceof NodeInterface && in_array($entity->bundle(), CommunityContentHelper::getBundles());
+  }
+
+  /**
+   * Checks if an entity is sticky inside a certain collection.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The entity to check.
+   * @param \Drupal\rdf_entity\RdfInterface $collection
+   *   The rdf collection.
+   *
+   * @return bool
+   *   True if the entity is sticky, false otherwise.
+   */
+  protected function isEntitySticky(ContentEntityInterface $entity, RdfInterface $collection) {
+    if ($this->isSolution($entity)) {
+      /** @var \Drupal\rdf_entity\RdfInterface $entity */
+      foreach ($entity->get(self::SOLUTION_PIN_FIELD)->referencedEntities() as $rdf) {
+        if ($rdf->id() === $collection->id()) {
+          return TRUE;
+        }
+      }
+    }
+    elseif ($this->isCommunityContent($entity)) {
+      // Nodes have only one possible parent, so the sticky boolean field
+      // reflects the sticky status.
+      /** @var \Drupal\node\NodeInterface $entity */
+      return $entity->isSticky();
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Sets the entity sticky status inside a certain collection.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The entity itself.
+   * @param \Drupal\rdf_entity\RdfInterface $collection
+   *   The rdf collection.
+   * @param bool $sticky
+   *   TRUE to set the entity as sticky, FALSE otherwise.
+   */
+  protected function setEntitySticky(ContentEntityInterface $entity, RdfInterface $collection, bool $sticky) {
+    if ($this->isSolution($entity)) {
+      $field = $entity->get(self::SOLUTION_PIN_FIELD);
+      if ($sticky) {
+        $field->appendItem($collection->id());
+      }
+      else {
+        $field->filter(function ($item) use ($collection) {
+          /** @var \Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem $item */
+          return $item->target_id !== $collection->id();
+        });
+      }
+    }
+    elseif ($this->isCommunityContent($entity)) {
+      // Nodes have only one possible parent, so the sticky boolean field
+      // reflects the sticky status.
+      /** @var \Drupal\node\NodeInterface $entity */
+      $entity->setSticky($sticky);
+    }
+
+    $entity->save();
+  }
+
+  /**
+   * Returns a response to redirect the user to an entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity.
    *
    * @return \Symfony\Component\HttpFoundation\RedirectResponse
-   *   The redirect response to the node collection.
+   *   The redirect response.
    */
-  protected function getRedirect(EntityInterface $node) {
-    $redirect = $this->relationManager->getParent($node)->toUrl();
+  protected function getRedirect(EntityInterface $entity) {
+    $redirect = $entity->toUrl();
 
     return $this->redirect($redirect->getRouteName(), $redirect->getRouteParameters());
   }
