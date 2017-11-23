23fcfdbb9567b214fcd085dc3deb4b63e942f51b
Sander Van Dooren <sander.vandooren@gmail.com>
2017-11-23 14:19:34 +0100

    Issue #ISAICP-4129: Kill running virtuoso before starting deployment.

--- resources/codedeploy/scripts/install.sh
+++ resources/codedeploy/scripts/install.sh
@@ -6,6 +6,9 @@ do
   sleep 2
 done
 
+# This can be removed once the ami is rebuild:
+nohup echo "shutdown();" | isql-vt &
+
 chown -R www-data:www-data /srv/project
 # Background and detach to work around time constrains of AWS CodeDeploy.
 nohup sudo -u www-data vendor/bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini setup-acceptance  >> /var/log/subsite-install.log 2>&1 &
