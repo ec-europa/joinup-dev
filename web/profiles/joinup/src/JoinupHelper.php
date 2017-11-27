ce5fa85064f8cdc06c7826caf72d38bfe85a8781
Francesco Sardara <francesco.sardara@one-agency.be>
2017-11-27 12:44:37 +0100

    Issue #ISAICP-4054: allow to pass a generic entity interface and add a method to check for solutions.

--- web/profiles/joinup/src/JoinupHelper.php
+++ web/profiles/joinup/src/JoinupHelper.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\joinup;
 
-use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\joinup_community_content\CommunityContentHelper;
 use Drupal\node\NodeInterface;
 use Drupal\rdf_entity\RdfInterface;
@@ -13,29 +13,57 @@ use Drupal\rdf_entity\RdfInterface;
 class JoinupHelper {
 
   /**
+   * Returns whether the entity is an rdf collection.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to check.
+   *
+   * @return bool
+   *   True if the entity is an rdf of bundle collection, false otherwise.
+   */
+  public static function isCollection(EntityInterface $entity) {
+    return self::isRdfEntityOfBundle($entity, 'collection');
+  }
+
+  /**
    * Returns whether the entity is an rdf solution.
    *
-   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   * @param \Drupal\Core\Entity\EntityInterface $entity
    *   The entity to check.
    *
    * @return bool
    *   True if the entity is an rdf of bundle solution, false otherwise.
    */
-  public static function isSolution(ContentEntityInterface $entity) {
-    return $entity instanceof RdfInterface && $entity->bundle() === 'solution';
+  public static function isSolution(EntityInterface $entity) {
+    return self::isRdfEntityOfBundle($entity, 'solution');
   }
 
   /**
    * Returns whether the entity is a community content node.
    *
-   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   * @param \Drupal\Core\Entity\EntityInterface $entity
    *   The entity to check.
    *
    * @return bool
    *   True if the entity is a community content node, false otherwise.
    */
-  public static function isCommunityContent(ContentEntityInterface $entity) {
+  public static function isCommunityContent(EntityInterface $entity) {
     return $entity instanceof NodeInterface && in_array($entity->bundle(), CommunityContentHelper::getBundles());
+  }
+
+  /**
+   * Returns whether the entity is an rdf entity of a specific bundle.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to check.
+   * @param string $bundle
+   *   The bundle the entity should be.
+   *
+   * @return bool
+   *   True if the entity is an rdf of bundle collection, false otherwise.
+   */
+  protected static function isRdfEntityOfBundle(EntityInterface $entity, $bundle) {
+    return $entity instanceof RdfInterface && $entity->bundle() === $bundle;
   }
 
 }
