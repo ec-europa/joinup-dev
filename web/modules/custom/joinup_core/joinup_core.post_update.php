bf70c57d8f5d7bd823ab3d39a1f795a1f85d0196
Claudiu Cristea <clau.cristea@gmail.com>
2017-12-20 13:08:45 +0200

    ISAICP-4229: Move existing files from public to private stream wrapper.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -25,3 +25,35 @@ function joinup_core_post_update_install_vbo() {
 function joinup_core_post_update_install_email_registration() {
   \Drupal::service('module_installer')->install(['email_registration']);
 }
+
+/**
+ * Move the contact form attachments under the private scheme.
+ */
+function joinup_core_post_update_move_contact_form_attachments() {
+  /** @var \Drupal\Core\File\FileSystemInterface $file_system */
+  $file_system = Drupal::service('file_system');
+
+  $message_storage = \Drupal::entityTypeManager()->getStorage('message');
+  $ids = $message_storage->getQuery()
+    ->condition('template', 'contact_form_submission')
+    ->exists('field_contact_attachment')
+    ->execute();
+
+  foreach ($message_storage->loadMultiple($ids) as $message) {
+    /** @var \Drupal\file\FileInterface $attachment */
+    if ($attachment = $message->field_contact_attachment->entity) {
+      $target = file_uri_target($attachment->getFileUri());
+      $uri = "private://$target";
+      $destination_dir = $file_system->dirname($uri);
+      if (!file_prepare_directory($destination_dir, FILE_CREATE_DIRECTORY)) {
+        throw new \RuntimeException("Cannot create directory '$destination_dir'.");
+      }
+      if (!file_move($attachment, $uri)) {
+        throw new \RuntimeException("Cannot move '{$attachment->getFileUri()}' to '$uri'.");
+      }
+    }
+  }
+
+  // Finally, remove the empty public://contact_form directory.
+  file_unmanaged_delete_recursive('public://contact_form');
+}
