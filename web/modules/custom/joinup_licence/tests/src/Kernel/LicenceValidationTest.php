60586a7014a3f5fb1ea64dd1680de8ffe557d5c5
Francesco Sardara <francesco.sardara@one-agency.be>
2018-01-29 14:14:34 +0100

    Issue #ISAICP-4215: add tests for contact information and licence.

---
+++ web/modules/custom/joinup_licence/tests/src/Kernel/LicenceValidationTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\joinup_licence\Kernel;
+
+use Drupal\Tests\joinup_core\Kernel\RdfEntityValidationTestBase;
+
+/**
+ * Tests the validation on the licence bundle entity.
+ *
+ * @group entity
+ */
+class LicenceValidationTest extends RdfEntityValidationTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'joinup_licence',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installConfig('joinup_licence');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getRequiredFields(): array {
+    return [
+      'label',
+      'field_licence_description',
+      'field_licence_type',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function bundle(): string {
+    return 'licence';
+  }
+
+}
