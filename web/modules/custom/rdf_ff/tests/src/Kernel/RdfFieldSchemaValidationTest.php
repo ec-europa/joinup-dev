cd20ee93df6d0ae36ad1aef56b7113a01db7ac56
Ilias Dimopoulos <idimopoulos@hotmail.com>
2018-02-14 15:42:04 +0200

    ISAICP-4300: Provide a test for the rdf_ff module.

---
+++ web/modules/custom/rdf_ff/tests/src/Kernel/RdfFieldSchemaValidationTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Drupal\Tests\rdf_ff\Kernel;
+
+use Drupal\rdf_entity\Entity\Query\Sparql\SparqlArg;
+use Drupal\Tests\joinup_core\Kernel\JoinupKernelTestBase;
+use EasyRdf\Graph;
+
+class RdfFieldSchemaValidationTest extends JoinupKernelTestBase {
+
+  /**
+   * The SPARQL connection.
+   *
+   * @var  \Drupal\rdf_entity\Database\Driver\sparql\Connection
+   */
+  protected $spaqlEndpoint;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManger;
+
+  /**
+   * The schema definition graph URI.
+   *
+   * @var string
+   */
+  protected $definitionUri = 'http://example.com/dummy/schema-definition';
+
+  /**
+   * The field schema validation service.
+   *
+   * @var \Drupal\rdf_ff\SchemaFieldValidatorInterface
+   */
+  protected $schemaFieldValidator;
+
+  /**
+   * Modules to enable for this test.
+   *
+   * @var string[]
+   */
+  public static $modules = [
+    'rdf_entity',
+    'rdf_entity_test',
+    'rdf_ff',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->spaqlEndpoint = $this->container->get('sparql_endpoint');
+    $this->entityTypeManger = $this->container->get('entity_type.manager');
+    $this->schemaFieldValidator = $this->container->get('rdf_ff.schema_field_validator');
+
+    /** @var \Drupal\rdf_entity\Entity\RdfEntityMapping $dummy_mapping */
+    $dummy_mapping = $this->entityTypeManger->getStorage('rdf_entity_mapping')->load("rdf_entity.dummy");
+    $dummy_mapping->setThirdPartySetting('rdf_ff', 'property_predicates', ['http://www.w3.org/2000/01/rdf-schema#domain']);
+    $dummy_mapping->setThirdPartySetting('rdf_ff', 'graph', $this->definitionUri);
+    $dummy_mapping->save();
+
+    $filename = __DIR__ . '/../../fixtures/dummy_definition.rdf';
+    if (!is_file($filename)) {
+      throw new \Exception("Definition file not found.");
+    }
+    $data = file_get_contents($filename);
+    $format = 'rdfxml';
+    $graph = new Graph($this->definitionUri, $data, $format);
+    $graph_uri = SparqlArg::uri($this->definitionUri);
+    $query = "INSERT DATA INTO $graph_uri {\n";
+    $query .= $graph->serialise('ntriples') . "\n";
+    $query .= '}';
+    $this->spaqlEndpoint->update($query);
+  }
+
+  /**
+   * Asserts that fields belong or don't belong to a defined schema.
+   */
+  public function testFieldSchema() {
+    $this->assertTrue($this->schemaFieldValidator->isDefinedInSchema('rdf_entity', 'dummy', 'field_text'));
+    $this->assertFalse($this->schemaFieldValidator->isDefinedInSchema('rdf_entity', 'dummy', 'field_text', 'format'));
+    $this->assertFalse($this->schemaFieldValidator->isDefinedInSchema('rdf_entity', 'dummy', 'label'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown() {
+    $query = <<<EndOfQuery
+DELETE {
+  GRAPH <{$this->definitionUri}> {
+    ?entity ?field ?value
+  }
+}
+WHERE {
+  GRAPH <{$this->definitionUri}> {
+    ?entity ?field ?value
+  }
+}
+EndOfQuery;
+    $this->sparql->query($query);
+    parent::tearDown();
+  }
+
+
+}
