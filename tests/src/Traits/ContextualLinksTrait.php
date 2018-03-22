708de1d615d81e28e77943eb4d9073b6e724b82f
Pieter Frenssen <pieter@frenssen.be>
2018-03-22 20:21:52 +0200

    ISAICP-3592: Refactor the contextual links trait.
    Factor out the business logic into a separate helper class:
     - The local scope of classes using this trait is no longer polluted with
       methods that deal with implementation details.
     - The dependencies on other traits is now clearly defined.
     - Methods were duplicated for dealing with regions and elements, but a
       region is also an element, so this distinction was not needed.
     - Consolidated code that was scattered over 2 different traits.

--- tests/src/Traits/ContextualLinksTrait.php
+++ tests/src/Traits/ContextualLinksTrait.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\joinup\Traits;
 
 use Behat\Mink\Element\NodeElement;
-use Symfony\Component\DomCrawler\Crawler;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\joinup\ContextualLinksHelper;
 
 /**
  * Helper methods to deal with contextual links.
@@ -11,141 +14,85 @@ use Symfony\Component\DomCrawler\Crawler;
 trait ContextualLinksTrait {
 
   /**
-   * Find all the contextual links in a region, without the need for javascript.
+   * The contextual links helper class.
    *
-   * @param string $region
-   *   The name of the region.
-   *
-   * @return array
-   *   An array of links found keyed by title.
-   *
-   * @throws \Exception
-   *   When the region is not found in the page.
+   * @var \Drupal\joinup\ContextualLinksHelper
    */
-  protected function findContextualLinksInRegion($region) {
-    return $this->findContextualLinksInElement($this->getRegion($region));
+  protected $contextualLinksHelper;
+
+  /**
+   * Returns the helper class to deal with contextual links.
+   *
+   * @return \Drupal\joinup\ContextualLinksHelper
+   *   The helper class.
+   */
+  protected function getContextualLinksHelper(): ContextualLinksHelper {
+    if (!$this instanceof RawDrupalContext) {
+      throw new \LogicException('This trait can only be used by contexts that extend RawDrupalContext.');
+    }
+
+    if (empty($this->contextualLinksHelper)) {
+      $this->contextualLinksHelper = new ContextualLinksHelper($this);
+    }
+    return $this->contextualLinksHelper;
   }
 
   /**
-   * Find all the contextual links in an element.
+   * Returns the paths for all the contextual links in the given element.
    *
    * @param \Behat\Mink\Element\NodeElement $element
-   *   The name of the element to check.
+   *   The element to check.
    *
    * @return array
-   *   An array of links found keyed by title.
+   *   An array of link paths found keyed by title.
    */
-  protected function findContextualLinksInElement(NodeElement $element) {
-    if (!$this->browserSupportsJavascript()) {
-      return $this->generateContextualLinks($element);
-    }
-
-    return $this->findContextualLinksInJsBrowsers($element);
+  protected function findContextualLinkPaths(NodeElement $element): array {
+    return $this->getContextualLinksHelper()->findContextualLinkPaths($element);
   }
 
   /**
-   * Generate contextual links for a specific element in non-JS browsers.
+   * Clicks (or follows) the contextual link in the given element.
    *
-   * Contextual links are retrieved on the browser side through the use
-   * of javascript, but that is not applicable for non-javascript browsers.
+   * Since the logic for contextual links is implemented in JavaScript this will
+   * not click the contextual link but redirect the browser to the corresponding
+   * page if the browser doesn't support JavaScript.
    *
    * @param \Behat\Mink\Element\NodeElement $element
-   *   The name of the element to check.
-   *
-   * @return array
-   *   An array of links found keyed by title.
+   *   The element that contains the contextual links.
+   * @param string $link
+   *   The link title.
    */
-  protected function generateContextualLinks(NodeElement $element) {
-    // We want to make an extra request to the website, using all the cookies
-    // from the current logged in user, but doing so will change the last page
-    // output, possibly breaking other steps. This can be prevented by cloning
-    // the client.
-    /** @var \Symfony\Component\BrowserKit\Client $client */
-    $client = clone $this->getSession()->getDriver()->getClient();
-
-    $contextual_ids = array_map(function ($element) {
-      /** @var \Behat\Mink\Element\NodeElement $element */
-      return $element->getAttribute('data-contextual-id');
-    }, $element->findAll('xpath', '//*[@data-contextual-id]'));
-
-    // @see Drupal.behaviors.contextual.attach(), contextual.js
-    $client->request('POST', '/contextual/render', [
-      'ids' => $contextual_ids,
-    ]);
-
-    $links = [];
-    $response = json_decode($client->getResponse()->getContent(), TRUE);
-    if ($response) {
-      foreach ($contextual_ids as $id) {
-        if (isset($response[$id])) {
-          $crawler = new Crawler();
-          $crawler->addHtmlContent($response[$id]);
-
-          foreach ($crawler->filterXPath('//a') as $node) {
-            /** @var \DOMElement $node */
-            $links[$node->nodeValue] = $node->getAttribute('href');
-          }
-        }
-      }
-    }
-
-    return $links;
+  protected function clickContextualLink(NodeElement $element, string $link): void {
+    $this->getContextualLinksHelper()->clickContextualLink($element, $link);
   }
 
   /**
-   * Find all the contextual links in an element on JS-enabled browsers.
+   * Returns the contextual links button that is present in the given element.
    *
    * @param \Behat\Mink\Element\NodeElement $element
-   *   The name of the element to check.
+   *   The element in which the contextual links button is expected to reside.
    *
-   * @return array
-   *   An array of links found keyed by title.
+   * @return \Behat\Mink\Element\NodeElement
+   *   The contextual links button.
    *
-   * @throws \Exception
-   *   Thrown when the contextual links are not shown within the timeframe.
+   * @throws \RuntimeException
+   *   Thrown when the region or the contextual links button was not found on
+   *   the page.
    */
-  protected function findContextualLinksInJsBrowsers(NodeElement $element) {
-    // If the contextual link placeholder element is not there already, it
-    // means that no links are available.
-    if (!$element->find('css', '[data-contextual-id]')) {
-      return [];
-    }
-
-    // Wait until the contextual module javascript is executed. Markup will be
-    // appended upon completion.
-    $contextual_button = $this->waitUntil(function () use ($element) {
-      return $element->find('css', '.contextual > button.trigger');
+  protected function findContextualLinkButton(NodeElement $element): NodeElement {
+    // Check if the wrapper for the contextual links is present on the page.
+    // Since the button is appended by the contextual.js script, we might need
+    // to wait a bit before the button itself is visible.
+    $button = $element->waitFor(5, function ($object) {
+      /** @var \Behat\Mink\Element\NodeElement $object */
+      return $object->find('xpath', '//div[contains(concat(" ", normalize-space(@class), " "), " contextual ")]/button');
     });
 
-    // If the contextual wrapper is not found, it means that no contextual
-    // links are available for this element.
-    if (!$contextual_button) {
-      return [];
+    if (!$button) {
+      throw new \RuntimeException('No contextual link button found.');
     }
 
-    // Open the contextual links dropdown if it is not yet open.
-    $link_list = $element->find('css', '.contextual ul.contextual-links');
-    if (!$link_list->isVisible()) {
-      /** @var \Behat\Mink\Element\NodeElement $contextual_button */
-      $contextual_button->focus();
-      $contextual_button->click();
-
-      $visible = $this->waitUntil(function () use ($link_list) {
-        return $link_list->isVisible();
-      });
-
-      if (!$visible) {
-        throw new \Exception('The contextual links did not open properly within the expected time frame.');
-      }
-    }
-
-    $links = [];
-    foreach ($link_list->findAll('xpath', '//a') as $link) {
-      /** @var \Behat\Mink\Element\NodeElement $link */
-      $links[$link->getText()] = $link->getAttribute('href');
-    }
-
-    return $links;
+    return $button;
   }
 
 }
