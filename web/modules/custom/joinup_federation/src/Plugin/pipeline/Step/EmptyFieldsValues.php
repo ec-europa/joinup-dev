41cdffe967d0b7ad8d41fad9ecbdbe889afbbf59
Claudiu Cristea <clau.cristea@gmail.com>
2018-06-20 16:46:31 +0300

    ISAICP-4572: Add a new step for assigning default values to empty fields.

---
+++ web/modules/custom/joinup_federation/src/Plugin/pipeline/Step/EmptyFieldsValues.php
@@ -0,0 +1,305 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_federation\Plugin\pipeline\Step;
+
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\joinup_federation\JoinupFederationStepPluginBase;
+use Drupal\rdf_entity\Database\Driver\sparql\Connection;
+use Drupal\rdf_entity\Entity\Query\Sparql\SparqlQueryInterface;
+use Drupal\rdf_entity\Entity\Rdf;
+use Drupal\rdf_entity\RdfEntitySparqlStorageInterface;
+use Drupal\rdf_entity\RdfInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Ensures values for empty fields from the staging graph.
+ *
+ * @PipelineStep(
+ *   id = "empty_fields_values",
+ *   label = @Translation("Empty fields values"),
+ * )
+ */
+class EmptyFieldsValues extends JoinupFederationStepPluginBase {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The entity field manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManagerInterface
+   */
+  protected $entityFieldManager;
+
+  /**
+   * The RDF entity SPARQL storage.
+   *
+   * @var \Drupal\rdf_entity\RdfEntitySparqlStorageInterface
+   */
+  protected $rdfStorage;
+
+  /**
+   * The cached SPARQL entity query.
+   *
+   * @var \Drupal\rdf_entity\Entity\Query\Sparql\SparqlQueryInterface
+   */
+  protected $sparqlQuery;
+
+  /**
+   * Creates a new pipeline step plugin instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\rdf_entity\Database\Driver\sparql\Connection $sparql
+   *   The SPARQL database connection.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   The entity field manager service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $sparql, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $sparql);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->entityFieldManager = $entity_field_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('sparql_endpoint'),
+      $container->get('entity_type.manager'),
+      $container->get('entity_field.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['collection' => NULL] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(array &$data) {
+    // Get the incoming entities.
+    $incoming_ids = $this->getSparqlQuery()
+      ->graphs(['staging'])
+      ->execute();
+    /** @var \Drupal\rdf_entity\RdfInterface[] $incoming_entities */
+    $incoming_entities = $incoming_ids ? $this->getRdfStorage()->loadMultiple($incoming_ids, ['staging']) : [];
+
+    // Get the incoming entities that are stored also locally.
+    $local_ids = $this->getSparqlQuery()
+      ->graphs(['default', 'draft'])
+      ->condition('id', array_values($incoming_ids), 'IN')
+      ->execute();
+    /** @var \Drupal\rdf_entity\RdfInterface[] $local_entities */
+    $local_entities = $local_ids ? Rdf::loadMultiple($local_ids) : [];
+
+    /** @var \Drupal\rdf_entity\RdfInterface $incoming_entity */
+    foreach ($incoming_entities as $id => $incoming_entity) {
+      $entity_exists = isset($local_entities[$id]);
+      // The entity already exists.
+      if ($entity_exists) {
+        // Check for bundle mismatch between the incoming and the local entity.
+        if ($incoming_entity->bundle() !== $local_entities[$id]->bundle()) {
+          $arguments = [
+            '%id' => $id,
+            '@incoming' => $incoming_entity->get('rid')->entity->getSingularLabel(),
+            '@local' => $local_entities[$id]->get('rid')->entity->getSingularLabel(),
+          ];
+          return [
+            '#markup' => $this->t("The imported @incoming with the ID '%id' tries to override a local @local with the same ID.", $arguments),
+          ];
+        }
+
+        $needs_save = $this->updateStagingFieldsFromLocalValues($incoming_entity, $local_entities[$id]);
+      }
+      // No local entity. Ensure defaults.
+      else {
+        $this->ensureFieldDefaults($incoming_entity);
+        $needs_save = TRUE;
+      }
+
+      if ($needs_save) {
+        $this->handleAffiliation($incoming_entity, $entity_exists);
+        $incoming_entity->skip_notification = TRUE;
+        $incoming_entity->save();
+      }
+    }
+  }
+
+  /**
+   * Updates the field values from the local to the incoming entity.
+   *
+   * @param \Drupal\rdf_entity\RdfInterface $incoming_entity
+   *   The imported entity.
+   * @param \Drupal\rdf_entity\RdfInterface $local_entity
+   *   The local entity.
+   *
+   * @return bool
+   *   If the incoming entity has been changed and needs to be saved.
+   */
+  protected function updateStagingFieldsFromLocalValues(RdfInterface $incoming_entity, RdfInterface $local_entity): bool {
+    $changed = FALSE;
+
+    foreach ($incoming_entity->getFieldDefinitions() as $field_name => $field_definition) {
+      // Bypass fields without mapping or fields we don't want to override.
+      if (in_array($field_name, ['id', 'rid', 'graph', 'uuid', 'uid'])) {
+        continue;
+      }
+      // Only stored fields are allowed.
+      if ($field_definition->isComputed()) {
+        continue;
+      }
+
+      $columns = $field_definition->getFieldStorageDefinition()->getColumns();
+      foreach ($columns as $column_name => $column_schema) {
+        // Check if the field is an ADMS-AP field.
+        $incoming_field = $incoming_entity->get($field_name);
+        $local_field = $local_entity->get($field_name);
+        // Assign only if the incoming field is empty.
+        if ($incoming_field->isEmpty()) {
+          $incoming_field->setValue($local_field->getValue());
+          $changed = TRUE;
+          // Don't check the rest of the columns because the whole field has
+          // been already assigned.
+          break;
+        }
+      }
+    }
+
+    return $changed;
+  }
+
+  /**
+   * Sets default values for fields.
+   *
+   * @param \Drupal\rdf_entity\RdfInterface $incoming_entity
+   *   The imported entity.
+   */
+  protected function ensureFieldDefaults(RdfInterface $incoming_entity): void {
+    // Determine the state field for this bundle, if any.
+    $state_field_name = NULL;
+    $state_field_map = $this->entityFieldManager->getFieldMapByFieldType('state');
+    if (!empty($state_field_map['rdf_entity'])) {
+      foreach ($state_field_map['rdf_entity'] as $field_name => $field_info) {
+        if (isset($field_info['bundles'][$incoming_entity->bundle()])) {
+          $state_field_name = $field_name;
+          break;
+        }
+      }
+    }
+
+    // Set he state of new entities to 'validated'.
+    if ($state_field_name) {
+      $incoming_entity->set($state_field_name, 'validated');
+    }
+
+    /** @var \Drupal\Core\Field\FieldItemListInterface $field */
+    foreach ($incoming_entity as $field_name => &$field) {
+      // Populate empty fields with their default value. This is a Drupal
+      // content entity that was not created via Drupal API. As an effect, empty
+      // fields didn't receive their default values. We have to explicitly do
+      // this before saving. The check if the field is a computed field is there
+      // in order to avoid the computation of the field. The method ::isEmpty()
+      // triggers the computation.
+      // @see \Drupal\Core\TypedData\ComputedItemListTrait::isEmpty()
+      if (!$field->getFieldDefinition()->isComputed() && $field->isEmpty()) {
+        $field->applyDefaultValue();
+      }
+    }
+  }
+
+  /**
+   * Handles the incoming solution affiliation.
+   *
+   * For existing solutions, we only check if the configured collection ID
+   * matches the solution affiliation. For new solutions, we affiliate the
+   * solution to the configured collection.
+   *
+   * @param \Drupal\rdf_entity\RdfInterface $incoming_solution
+   *   The local solution.
+   * @param bool $entity_exists
+   *   If the incoming entity already exits on the system.
+   *
+   * @throws \Exception
+   *   If the configured collection is different than the collection of the
+   *   local solution.
+   */
+  protected function handleAffiliation(RdfInterface $incoming_solution, bool $entity_exists): void {
+    // Check only solutions.
+    if ($incoming_solution->bundle() !== 'solution') {
+      return;
+    }
+
+    // If this plugin was not configured to assign a collection, exit early.
+    if (!$collection_id = $this->getConfiguration()['collection']) {
+      return;
+    }
+
+    if (!$entity_exists) {
+      $incoming_solution->set('collection', $collection_id);
+      return;
+    }
+
+    // Check for collection mismatch when federating an existing solution.
+    $match = FALSE;
+    foreach ($incoming_solution->get('collection') as $item) {
+      if ($item->target_id === $collection_id) {
+        $match = TRUE;
+        break;
+      }
+    }
+
+    if (!$match) {
+      throw new \Exception("Plugin 'empty_fields_values' is configured to assign the '$collection_id' collection but the existing solution '{$incoming_solution->id()}' has '{$incoming_solution->collection->target_id}' as collection.");
+    }
+    // For an existing solution we don't make any changes to its affiliation.
+  }
+
+  /**
+   * Returns the RDF storage.
+   *
+   * @return \Drupal\rdf_entity\RdfEntitySparqlStorageInterface
+   *   The RDF storage.
+   */
+  protected function getRdfStorage(): RdfEntitySparqlStorageInterface {
+    if (!isset($this->rdfStorage)) {
+      $this->rdfStorage = $this->entityTypeManager->getStorage('rdf_entity');
+    }
+    return $this->rdfStorage;
+  }
+
+  /**
+   * Returns the SPARQL entity query.
+   *
+   * @return \Drupal\rdf_entity\Entity\Query\Sparql\SparqlQueryInterface
+   *   The entity query.
+   */
+  protected function getSparqlQuery(): SparqlQueryInterface {
+    if (!isset($this->sparqlQuery)) {
+      $this->sparqlQuery = $this->getRdfStorage()->getQuery();
+    }
+    return $this->sparqlQuery;
+  }
+
+}
---
+++ web/modules/custom/joinup_federation/tests/src/Kernel/EmptyFieldsValuesStepTest.php
@@ -0,0 +1,215 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\joinup_federation\Kernel;
+
+use Drupal\Core\Serialization\Yaml;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\rdf_entity\Entity\Rdf;
+use Drupal\rdf_entity\Entity\RdfEntityGraph;
+use Drupal\rdf_entity\Entity\RdfEntityMapping;
+use Drupal\rdf_entity\Entity\RdfEntityType;
+use Drupal\taxonomy\Entity\Vocabulary;
+use EasyRdf\Graph;
+
+/**
+ * Tests the 'empty_fields_values' process step plugin.
+ *
+ * @group joinup_federation
+ */
+class EmptyFieldsValuesStepTest extends StepTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getUsedStepPlugins(): array {
+    return [
+      'remove_unsupported_data' => [],
+      'add_joinup_vocabularies' => [],
+      'empty_fields_values' => [
+        'collection' => 'http://catalog',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'field',
+    'rdf_schema_field_validation',
+    'rdf_taxonomy',
+    'taxonomy',
+    'text',
+    'user',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Create the 'default' and 'staging' graphs.
+    $graph = Yaml::decode(file_get_contents(__DIR__ . '/../../../../../../profiles/joinup/config/install/rdf_entity.graph.default.yml'));
+    RdfEntityGraph::create($graph)->save();
+    $graph = Yaml::decode(file_get_contents(__DIR__ . '/../../../config/install/rdf_entity.graph.staging.yml'));
+    RdfEntityGraph::create($graph)->save();
+
+    // Create the language vocabulary and mapping.
+    Vocabulary::create(['vid' => 'language', 'name' => 'Language'])->save();
+    $mapping = Yaml::decode(file_get_contents(__DIR__ . '/../../../../joinup_core/config/install/rdf_entity.mapping.taxonomy_term.language.yml'));
+    RdfEntityMapping::create($mapping)->save();
+
+    // Create the solution RDF type.
+    RdfEntityType::create(['rid' => 'solution', 'name' => 'Solution'])->save();
+    // Add some fields for the purpose of this test.
+    FieldStorageConfig::create([
+      'type' => 'text_long',
+      'entity_type' => 'rdf_entity',
+      'field_name' => 'field_is_description',
+    ])->setThirdPartySetting('rdf_entity', 'mapping', [
+      'value' => [
+        'predicate' => 'http://purl.org/dc/terms/description',
+        'format' => 't_literal',
+      ],
+      'format' => [
+        'predicate' => '',
+        'format' => '',
+      ],
+    ])->save();
+    FieldConfig::create([
+      'entity_type' => 'rdf_entity',
+      'bundle' => 'solution',
+      'field_name' => 'field_is_description',
+      'label' => 'Description',
+    ])->save();
+    FieldStorageConfig::create([
+      'type' => 'entity_reference',
+      'entity_type' => 'rdf_entity',
+      'field_name' => 'field_status',
+    ])->setThirdPartySetting('rdf_entity', 'mapping', [
+      'target_id' => [
+        'predicate' => 'http://www.w3.org/ns/adms#status',
+        'format' => 'resource',
+      ],
+    ])->save();
+    FieldConfig::create([
+      'entity_type' => 'rdf_entity',
+      'bundle' => 'solution',
+      'field_name' => 'field_status',
+      'label' => 'Status',
+      'default_value' => [
+        [
+          'target_id' => 'http://example.com/default-status',
+        ],
+      ],
+    ])->save();
+
+    $mapping = Yaml::decode(file_get_contents(__DIR__ . '/../../../../solution/config/install/rdf_entity.mapping.rdf_entity.solution.yml'));
+    RdfEntityMapping::create($mapping)->save();
+
+    // Create the collection bundle.
+    RdfEntityType::create(['rid' => 'collection', 'name' => 'Collection'])->save();
+    $mapping = Yaml::decode(file_get_contents(__DIR__ . '/../../../../collection/config/install/rdf_entity.mapping.rdf_entity.collection.yml'));
+    RdfEntityMapping::create($mapping)->save();
+    // And the affiliates field.
+    $field_storage_config = Yaml::decode(file_get_contents(__DIR__ . '/../../../../collection/config/install/field.storage.rdf_entity.field_ar_affiliates.yml'));
+    FieldStorageConfig::create($field_storage_config)->save();
+    $field_config = Yaml::decode(file_get_contents(__DIR__ . '/../../../../collection/config/install/field.field.rdf_entity.collection.field_ar_affiliates.yml'));
+    FieldConfig::create($field_config)->save();
+  }
+
+  /**
+   * Test values assignment with an existing solution.
+   */
+  public function testExistingSolution() {
+    // Create a local entity whose values will be overwritten.
+    Rdf::create([
+      'rid' => 'solution',
+      'id' => 'http://asset',
+      'label' => 'This will be overridden',
+      'field_is_description' => 'Also this...',
+      'field_status' => 'http://example.com/status',
+    ])->save();
+    Rdf::create([
+      'rid' => 'collection',
+      'id' => 'http://catalog',
+      'field_ar_affiliates' => 'http://asset',
+    ])->save();
+
+    $graph = new Graph(static::getTestingGraphs()['sink']);
+    $graph->parseFile(__DIR__ . '/../../fixtures/valid_adms.rdf');
+    $this->createGraphStore()->replace($graph);
+
+    $this->runPipelineStep('remove_unsupported_data');
+    $this->runPipelineStep('add_joinup_vocabularies');
+    // Cleanup the 'sink_plus_taxo' graph left after the last step. Normally
+    // this cleanup is accomplished by the 'adms_validation' step but we want to
+    // avoid running that step in this test.
+    $this->pipeline->clearGraph($this->pipeline->getGraphUri('sink_plus_taxo'));
+
+    $result = $this->runPipelineStep('empty_fields_values');
+
+    // Check that the step ran without any error.
+    $this->assertNull($result);
+
+    /** @var \Drupal\rdf_entity\RdfInterface $solution */
+    $solution = Rdf::load('http://asset', ['staging']);
+
+    // Check that incoming values are preseved over local ones.
+    $this->assertEquals('Asset', $solution->label());
+    $this->assertEquals('This is an Asset.', $solution->get('field_is_description')->value);
+    // Check that a missed incoming value is copied from the local entity.
+    $this->assertEquals('http://example.com/status', $solution->get('field_status')->target_id);
+  }
+
+  /**
+   * Test values assignment with a new solution.
+   */
+  public function testNewSolution() {
+    Rdf::create([
+      'rid' => 'collection',
+      'id' => 'http://catalog',
+    ])->save();
+
+    $graph = new Graph(static::getTestingGraphs()['sink']);
+    $graph->parseFile(__DIR__ . '/../../fixtures/valid_adms.rdf');
+    $this->createGraphStore()->replace($graph);
+
+    $this->runPipelineStep('remove_unsupported_data');
+    $this->runPipelineStep('add_joinup_vocabularies');
+    // Cleanup the 'sink_plus_taxo' graph left after the last step. Normally
+    // this cleanup is accomplished by the 'adms_validation' step but we want to
+    // avoid running that step in this test.
+    $this->pipeline->clearGraph($this->pipeline->getGraphUri('sink_plus_taxo'));
+
+    $result = $this->runPipelineStep('empty_fields_values');
+
+    // Check that the step ran without any error.
+    $this->assertNull($result);
+
+    // Check that the solution has been assigned to the configured collection.
+    $collection = Rdf::load('http://catalog');
+    $this->assertEquals('http://asset', $collection->field_ar_affiliates->target_id);
+
+    // Check that, for an empty field, the default value is assigned.
+    $solution = Rdf::load('http://asset', ['staging']);
+    $this->assertEquals('http://example.com/default-status', $solution->get('field_status')->target_id);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown() {
+    $storage = $this->container->get('entity_type.manager')->getStorage('rdf_entity');
+    $storage->delete($storage->loadMultiple([
+      'http://asset',
+      'http://catalog',
+    ]));
+    parent::tearDown();
+  }
+
+}
