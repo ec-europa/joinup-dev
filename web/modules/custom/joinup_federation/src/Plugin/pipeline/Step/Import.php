b7fc2c7d047785e1d67051b1dc48ec6e50b0b5e9
Claudiu Cristea <clau.cristea@gmail.com>
2018-07-04 17:45:09 +0300

    ISAICP-4518: Increase the batch size of 'import' plugin.

--- web/modules/custom/joinup_federation/src/Plugin/pipeline/Step/Import.php
+++ web/modules/custom/joinup_federation/src/Plugin/pipeline/Step/Import.php
@@ -33,7 +33,7 @@ class Import extends JoinupFederationStepPluginBase implements PipelineStepWithB
    *
    * @var int
    */
-  const BATCH_SIZE = 10;
+  const BATCH_SIZE = 30;
 
   /**
    * The entity field manager service.
