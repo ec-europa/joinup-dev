f55c2ddf1528f7066e56efdeeace4f0693af4dab
Claudiu Cristea <clau.cristea@gmail.com>
2018-07-17 10:28:31 +0300

    ISAICP-4607: Embed the dashboard block into the custom page content.

--- web/modules/custom/joinup_communities/tallinn/tallinn.post_update.php
+++ web/modules/custom/joinup_communities/tallinn/tallinn.post_update.php
@@ -5,9 +5,20 @@
  * Post update functions for Tallinn.
  */
 
+use Drupal\node\Entity\Node;
+
 /**
- * Install the 'embed_block' module.
+ * Install the 'embed_block' module and add the dashboard block in page content.
  */
-function tallinn_post_update_install_embed_block() {
+function tallinn_post_update_add_block() {
   \Drupal::service('module_installer')->install(['embed_block']);
+
+  // The 'Implementation monitoring' page node ID equals 701254.
+  /** @var \Drupal\node\NodeInterface $custom_page */
+  $custom_page = Node::load(701254);
+  $body_field = $custom_page->get('body');
+  $value = $body_field->getValue();
+  $value[0]['value'] .= '{block:tallinn_dashboard}';
+  $body_field->setValue($value);
+  $custom_page->save();
 }
