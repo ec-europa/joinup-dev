2baccc4af4ed1c061665eacd39e31ddf9dbae0b2
Claudiu Cristea <clau.cristea@gmail.com>
2018-07-20 16:54:16 +0300

    ISAICP-4607: Use uuid instead of nid.

--- web/modules/custom/joinup_communities/tallinn/tallinn.post_update.php
+++ web/modules/custom/joinup_communities/tallinn/tallinn.post_update.php
@@ -5,17 +5,16 @@
  * Post update functions for Tallinn.
  */
 
-use Drupal\node\Entity\Node;
-
 /**
  * Install the 'embed_block' module and add the dashboard block in page content.
  */
 function tallinn_post_update_add_block() {
   \Drupal::service('module_installer')->install(['embed_block']);
 
-  // The 'Implementation monitoring' page node ID equals 701254.
+  /** @var \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository */
+  $entity_repository = \Drupal::service('entity.repository');
   /** @var \Drupal\node\NodeInterface $custom_page */
-  $custom_page = Node::load(701254);
+  $custom_page = $entity_repository->loadEntityByUuid('node', '9d7b6405-061a-4064-ae7e-b34c67f3afad');
   $body_field = $custom_page->get('body');
   $value = $body_field->getValue();
   $value[0]['value'] .= '{block:tallinn_dashboard}';
