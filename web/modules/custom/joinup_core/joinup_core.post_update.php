8ef9ffdf375dd95d2aa5f16994f25e901443bb57
Ilias Dimopoulos <idimopoulos@hotmail.com>
2018-08-21 15:11:11 +0300

    ISAICP-4654: Generate a list of Ids to fix.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -90,3 +90,58 @@ function joinup_core_post_update_remove_action_transfer_solution_ownership() {
 function joinup_core_post_update_install_tallinn() {
   \Drupal::service('module_installer')->install(['tallinn']);
 }
+
+/**
+ * Retrieves a list of entities with their duplicated ids.
+ *
+ * @return array
+ *   An associative array of ids where each entry is an array of ids of
+ *   duplicates.
+ */
+function _joinup_core_get_duplicated_ids(): array {
+  return [
+    'http://administracionelectronica.gob.es/ctt/dscp' => [
+      'http://administracionelectronica.gob.es/ctt/dscp_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/pau' => [
+      'http://administracionelectronica.gob.es/ctt/pau_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/pfiaragon' => [
+      'http://administracionelectronica.gob.es/ctt/pfiaragon_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/dir3' => [
+      'http://administracionelectronica.gob.es/ctt/dir3_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/svd' => [
+      'http://administracionelectronica.gob.es/ctt/svd_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/scsp' => [
+      'http://administracionelectronica.gob.es/ctt/scsp_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/tsa' => [
+      'http://administracionelectronica.gob.es/ctt/tsa_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/afirma' => [
+      'http://administracionelectronica.gob.es/ctt/afirma_1',
+      'http://administracionelectronica.gob.es/ctt/afirma_2',
+    ],
+    'http://administracionelectronica.gob.es/ctt/codice' => [
+      'http://administracionelectronica.gob.es/ctt/codice_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/badaral' => [
+      'http://administracionelectronica.gob.es/ctt/badaral_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/sicres' => [
+      'http://administracionelectronica.gob.es/ctt/sicres',
+    ],
+    'http://administracionelectronica.gob.es/ctt/pruebalola' => [
+      'http://administracionelectronica.gob.es/ctt/pruebalola_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/alpadron' => [
+      'http://administracionelectronica.gob.es/ctt/alpadron_1',
+    ],
+    'http://administracionelectronica.gob.es/ctt/avanzalocalgis' => [
+      'http://administracionelectronica.gob.es/ctt/avanzalocalgis_1',
+    ],
+  ];
+}
