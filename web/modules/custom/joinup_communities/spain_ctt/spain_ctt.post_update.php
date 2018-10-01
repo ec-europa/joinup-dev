72ddd295b88794fbf9b2440d64afada38a2bf120
Ilias Dimopoulos <idimopoulos@hotmail.com>
2018-10-01 22:30:55 +0300

    ISAICP-4816: Update the alias for the entities that have an unnecessary suffix in their url.

--- web/modules/custom/joinup_communities/spain_ctt/spain_ctt.post_update.php
+++ web/modules/custom/joinup_communities/spain_ctt/spain_ctt.post_update.php
@@ -5,6 +5,8 @@
  * Post update functions for the Spain CTT module.
  */
 
+use Drupal\rdf_entity\Entity\Rdf;
+
 /**
  * Clean more solution duplicates in the ctt collection.
  */
@@ -13,6 +15,30 @@ function spain_ctt_post_update_clean_ctt_duplicates() {
   // the install function again.
   require __DIR__ . '/spain_ctt.install';
   spain_ctt_install();
+
+  // For the below entities, now that their duplicates are cleaned, rebuild the
+  // url alias to remove the unnecessary suffix.
+  $entity_ids = [
+    'http://administracionelectronica.gob.es/ctt/archive',
+    'http://administracionelectronica.gob.es/ctt/eemgde',
+    'http://administracionelectronica.gob.es/ctt/regfia',
+    'http://administracionelectronica.gob.es/ctt/dscp',
+    'http://administracionelectronica.gob.es/ctt/pau',
+    'http://administracionelectronica.gob.es/ctt/pfiaragon',
+    'http://administracionelectronica.gob.es/ctt/dir3',
+    'http://administracionelectronica.gob.es/ctt/svd',
+    'http://administracionelectronica.gob.es/ctt/scsp',
+    'http://administracionelectronica.gob.es/ctt/tsa',
+    'http://administracionelectronica.gob.es/ctt/afirma',
+    'http://administracionelectronica.gob.es/ctt/codice',
+    'http://administracionelectronica.gob.es/ctt/badaral',
+  ];
+
+  /** @var \Drupal\pathauto\PathautoGeneratorInterface $pathauto_generator */
+  $pathauto_generator = \Drupal::service('pathauto.generator');
+  foreach (Rdf::loadMultiple($entity_ids) as $entity) {
+    $pathauto_generator->updateEntityAlias($entity, 'update');
+  }
 }
 
 /**
