18bcbb4d00226b33bb8136b7df8d310f3b46b136
Claudiu Cristea <clau.cristea@gmail.com>
2018-10-05 19:12:41 +0300

    ISAICP-4497: Kernel test the JoinupSparqlDefaultGraphsSubscriber event subscriber.

---
+++ web/modules/custom/joinup_sparql/tests/src/Kernel/JoinupSparqlDefaultGraphsTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\joinup_sparql\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\rdf_entity\Entity\Rdf;
+use Drupal\rdf_entity\Entity\RdfEntityGraph;
+use Drupal\rdf_entity\Entity\RdfEntityMapping;
+use Drupal\Tests\rdf_entity\Traits\RdfDatabaseConnectionTrait;
+
+/**
+ * Tests the default graphs event subscriber.
+ *
+ * @group joinup_sparql
+ */
+class JoinupSparqlDefaultGraphsTest extends KernelTestBase {
+
+  use RdfDatabaseConnectionTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'joinup_sparql',
+    'rdf_draft',
+    'rdf_entity',
+    'rdf_entity_graph_test',
+    'user',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->setUpSparql();
+    $this->installConfig(['rdf_draft', 'rdf_entity', 'rdf_entity_graph_test']);
+
+    // Create an arbitrary graph.
+    RdfEntityGraph::create(['id' => 'arbitrary', 'label' => 'Arbitrary'])
+      // Is heavier than the 'draft' graph..
+      ->setWeight(20)
+      ->save();
+    RdfEntityMapping::loadByName('rdf_entity', 'fruit')
+      ->addGraphs(['arbitrary' => "http://example.com/fruit/graph/arbitrary"])
+      ->save();
+  }
+
+  /**
+   * Tests the default graphs event subscriber.
+   */
+  public function testDefaultGraphs(): void {
+    $id = 'http://example.com/apple';
+    Rdf::create([
+      'id' => $id,
+      'rid' => 'fruit',
+      'label' => 'Apple',
+      'graph' => 'arbitrary',
+    ])->save();
+
+    // Check that loading explicitly from a candidate list, that includes the
+    // 'arbitrary' graph, works.
+    $this->assertNotNull(Rdf::load($id, ['draft', 'arbitrary']));
+    // Check that loading the entity from the default graphs returns NULL.
+    $this->assertNull(Rdf::load($id));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown(): void {
+    Rdf::load('http://example.com/apple', ['arbitrary'])->delete();
+    parent::tearDown();
+  }
+
+}
