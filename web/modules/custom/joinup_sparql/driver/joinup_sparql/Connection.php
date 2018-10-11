1d14cbc0c2736bb0e5c15e6e6a38946c4a5f7172
Ilias Dimopoulos <idimopoulos@hotmail.com>
2018-10-11 15:29:46 +0300

    ISAICP-4206: Set the timeout for the sparql client to 30s.

--- web/modules/custom/joinup_sparql/driver/joinup_sparql/Connection.php
+++ web/modules/custom/joinup_sparql/driver/joinup_sparql/Connection.php
@@ -7,6 +7,9 @@ namespace Drupal\Driver\Database\joinup_sparql;
 use Drupal\Driver\Database\sparql\Connection as BaseConnection;
 use Drupal\rdf_entity\Database\Driver\sparql\ConnectionInterface;
 use Drupal\rdf_entity\Exception\SparqlQueryException;
+use EasyRdf\Http;
+use EasyRdf\Http\Client as HttpClient;
+use EasyRdf\Sparql\Client;
 use EasyRdf\Sparql\Result;
 
 /**
@@ -53,6 +56,15 @@ class Connection extends BaseConnection implements ConnectionInterface {
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function open(array &$connection_options = []): Client {
+    $client = new HttpClient();
+    $client->setConfig(['timeout' => '30']);
+    Http::setDefaultHttpClient($client);
+    return parent::open($connection_options);
+  }
 }
 
 /**
