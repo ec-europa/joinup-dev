2e026558c2374feb127824e739228fc2384a71f6
Sander Van Dooren <sander.vandooren@gmail.com>
2018-11-12 17:20:42 +0100

    Issue ISAICP-4824: config_readonly override.

--- resources/uat/settings.uat.php
+++ resources/uat/settings.uat.php
@@ -4,3 +4,7 @@
  * @file
  * Includes custom settings for the UAT environment.
  */
+
+$config['stage_file_proxy.settings']['origin'] = 'https://joinup.ec.europa.eu';
+$config['stage_file_proxy.settings']['hotlink'] = TRUE;
+$settings['config_readonly'] = !file_exists(getcwd() . '/../disable-config-readonly');
