db817af2fa45de38381bac2ce9bf0ed36704bac8
Sander Van Dooren <sander.vandooren@gmail.com>
2018-11-13 13:21:47 +0100

    Issue #ISAICP-4807: Use REDIS on UAT.

--- resources/uat/settings.uat.php
+++ resources/uat/settings.uat.php
@@ -4,3 +4,8 @@
  * @file
  * Includes custom settings for the UAT environment.
  */
+$settings['redis.connection']['interface'] = 'Predis';
+$settings['redis.connection']['host']      = 'joinup-uat-rds-01.y9zhag.0001.euw1.cache.amazonaws.com';
+$settings['cache']['default'] = 'cache.backend.redis';
+$settings['container_yamls'][] = 'sites/default/redis.services.yml';
+$settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
