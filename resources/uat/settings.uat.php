df2afee38b9956f1eb3503d51c5686a5ec78b28d
Sander Van Dooren <sander.vandooren@gmail.com>
2018-11-16 14:11:34 +0100

    Issue #ISAICP-4807: Include should be prefixed by Drupal root.

--- resources/uat/settings.uat.php
+++ resources/uat/settings.uat.php
@@ -7,5 +7,4 @@
 $settings['redis.connection']['interface'] = 'Predis';
 $settings['redis.connection']['host']      = 'joinup-uat-rds-01.y9zhag.0001.euw1.cache.amazonaws.com';
 $settings['cache']['default'] = 'cache.backend.redis';
-$settings['container_yamls'][] = 'sites/default/redis.services.yml';
-$settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+$settings['container_yamls'][] = DRUPAL_ROOT . 'modules/contrib/redis/example.services.yml';
