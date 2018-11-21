a7581d2b51d2292fe04b8063eb10cc63499dd2f4
Pieter Frenssen <pieter@frenssen.be>
2018-11-21 15:01:53 +0200

    ISAICP-4719: Make sure all newsletter related code is contained in the newsletter module.

--- web/profiles/joinup/joinup.post_update.php
+++ web/profiles/joinup/joinup.post_update.php
@@ -11,10 +11,3 @@
 function joinup_post_update_install_views_data_export() {
   \Drupal::service('module_installer')->install(['views_data_export']);
 }
-
-/**
- * Enable the "OpenEuropa Newsroom Newsletter" module.
- */
-function joinup_post_update_install_oe_newsroom_newsletter() {
-  \Drupal::service('module_installer')->install(['oe_newsroom_newsletter']);
-}
