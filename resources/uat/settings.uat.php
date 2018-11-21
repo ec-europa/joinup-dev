dad706e253412824620b72cf5e18618182439dad
Claudiu Cristea <clau.cristea@gmail.com>
2018-11-21 15:27:26 +0200

    ISAICP-4807: Remove consecutive whitespaces.

--- resources/uat/settings.uat.php
+++ resources/uat/settings.uat.php
@@ -5,6 +5,6 @@
  * Includes custom settings for the UAT environment.
  */
 $settings['redis.connection']['interface'] = 'Predis';
-$settings['redis.connection']['host']      = 'joinup-uat-rds-01.y9zhag.0001.euw1.cache.amazonaws.com';
+$settings['redis.connection']['host'] = 'joinup-uat-rds-01.y9zhag.0001.euw1.cache.amazonaws.com';
 $settings['cache']['default'] = 'cache.backend.redis';
 $settings['container_yamls'][] = DRUPAL_ROOT . 'modules/contrib/redis/example.services.yml';
