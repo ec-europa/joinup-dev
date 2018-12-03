63e43e2e0ca3bcbe23b705ea2f2afd4a47869734
Sander Van Dooren <sander.vandooren@gmail.com>
2018-12-03 11:53:19 +0100

    Issue #ISAICP-4770: Setup spooling in UAT.

--- resources/uat/settings.uat.php
+++ resources/uat/settings.uat.php
@@ -17,3 +17,7 @@ $settings['redis.connection']['interface'] = 'Predis';
 $settings['redis.connection']['host'] = 'joinup-uat-rds-01.y9zhag.0001.euw1.cache.amazonaws.com';
 $settings['cache']['default'] = 'cache.backend.redis';
 $settings['container_yamls'][] = DRUPAL_ROOT . 'modules/contrib/redis/example.services.yml';
+
+// Configure swiftmailer spooling.
+$config['swiftmailer.transport']['transport'] = 'spool';
+$config['swiftmailer.transport']['spool_directory'] = '/tmp/spool/';
