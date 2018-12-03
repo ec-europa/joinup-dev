b427439fb1cc3cdc03b35f11fb6a8c8f2dae1387
Sander Van Dooren <sander.vandooren@gmail.com>
2018-12-03 09:24:35 +0100

    Issue #ISACP-4486: Trim for for fun and for profit.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -112,4 +112,3 @@ function joinup_core_post_update_install_piwik2matomo() {
 function joinup_core_post_update_install_spain_ctt() {
   \Drupal::service('module_installer')->install(['spain_ctt']);
 }
-
