252366c014d6d92052a86c2829586f221484394b
Ilias Dimopoulos <idimopoulos@hotmail.com>
2018-12-17 19:08:43 +0200

    ISAICP-4751: Fix the query method name.

--- src/Phing/AfterFixturesImportCleanup.php
+++ src/Phing/AfterFixturesImportCleanup.php
@@ -27,32 +27,32 @@ class AfterFixturesImportCleanup extends VirtuosoTaskBase {
     // labeled 'Multilingual Code'.
     // @see http://publications.europa.eu/mdr/resource//documentation/schema/cat.html#element_languages
     // @see https://webgate.ec.europa.eu/CITnet/jira/browse/ISAICP-2764
-    $this->sparql('DELETE FROM <http://languages-skos> { ?entity ?field ?value. } WHERE { ?entity ?field ?value . FILTER(str(?value) = "Multilingual Code") };');
+    $this->query('DELETE FROM <http://languages-skos> { ?entity ?field ?value. } WHERE { ?entity ?field ?value . FILTER(str(?value) = "Multilingual Code") };');
 
     // @see ISAICP-3084
-    $this->sparql('INSERT INTO <http://adms-sw-v1.00> { <http://purl.org/adms/licencetype/ViralEffect-ShareAlike>  <http://www.w3.org/2004/02/skos/core#inScheme> <http://purl.org/adms/licencetype/1.1> };');
+    $this->query('INSERT INTO <http://adms-sw-v1.00> { <http://purl.org/adms/licencetype/ViralEffect-ShareAlike>  <http://www.w3.org/2004/02/skos/core#inScheme> <http://purl.org/adms/licencetype/1.1> };');
 
     // The licences are defined in both the adms-sw and the adms-skos files.
     // In adms-sw the version 1.1 is included while the adms-skos has the
     // version 1.0. As a bundle can have only one uri mapped, the 1.0 version
     // has to be removed.
     // @see https://webgate.ec.europa.eu/CITnet/jira/browse/ISAICP-2503
-    $this->sparql('DELETE FROM <http://adms_skos_v1.00> { ?entity ?field ?value. } WHERE { ?entity ?field ?value . ?entity <http://www.w3.org/2004/02/skos/core#inScheme> <http://purl.org/adms/licencetype/1.0>};');
+    $this->query('DELETE FROM <http://adms_skos_v1.00> { ?entity ?field ?value. } WHERE { ?entity ?field ?value . ?entity <http://www.w3.org/2004/02/skos/core#inScheme> <http://purl.org/adms/licencetype/1.0>};');
 
     // Remove any non english version of the taxonomy terms since we are only
     // supporting english version in the website. If more terms are needed, the
     // supported languages should be extended (the "en" below).
     // This needs to repeat multiple times as the terms might.
-    $this->sparql('DELETE { GRAPH ?g { ?entity ?field ?value } } WHERE { GRAPH ?g { ?entity ?field ?value . FILTER (LANG(?value) != "" && LANG(?value) != "en") } };');
+    $this->query('DELETE { GRAPH ?g { ?entity ?field ?value } } WHERE { GRAPH ?g { ?entity ?field ?value . FILTER (LANG(?value) != "" && LANG(?value) != "en") } };');
 
     // @see ISAICP-3216
-    $this->sparql('INSERT INTO <http://eira_skos> { ?subject a skos:Concept . ?subject skos:topConceptOf <http://data.europa.eu/eira> } WHERE { ?subject a skos:Collection . };');
-    $this->sparql('INSERT INTO <http://eira_skos> { ?subject skos:topConceptOf <http://data.europa.eu/eira> } WHERE { GRAPH <http://eira_skos> { ?subject a skos:Concept .} };');
-    $this->sparql('INSERT INTO <http://eira_skos> { ?member skos:broaderTransitive ?collection } WHERE { ?collection a skos:Collection . ?collection skos:member ?member };');
+    $this->query('INSERT INTO <http://eira_skos> { ?subject a skos:Concept . ?subject skos:topConceptOf <http://data.europa.eu/eira> } WHERE { ?subject a skos:Collection . };');
+    $this->query('INSERT INTO <http://eira_skos> { ?subject skos:topConceptOf <http://data.europa.eu/eira> } WHERE { GRAPH <http://eira_skos> { ?subject a skos:Concept .} };');
+    $this->query('INSERT INTO <http://eira_skos> { ?member skos:broaderTransitive ?collection } WHERE { ?collection a skos:Collection . ?collection skos:member ?member };');
 
     // Remove deprecated countries from the country list.
     // @See ISAICP-3442
-    $this->sparql('DELETE FROM <http://countries-skos> { ?entity ?field ?value. } WHERE { ?entity ?field ?value . ?entity <http://publications.europa.eu/ontology/authority/end.use> ?date . FILTER ( bound(?date) ) };');
+    $this->query('DELETE FROM <http://countries-skos> { ?entity ?field ?value. } WHERE { ?entity ?field ?value . ?entity <http://publications.europa.eu/ontology/authority/end.use> ?date . FILTER ( bound(?date) ) };');
   }
 
 }
