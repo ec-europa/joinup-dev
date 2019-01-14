fda892f522fcb7625845ae93910f57ba86024da2
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-01-14 16:11:22 +0200

    ISAICP-4510: Provide a pipeline for the joinup collection.

---
+++ web/modules/custom/joinup_communities/joinup_collection/src/Plugin/pipeline/Pipeline/JoinupCollectionPipeline.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\joinup_collection\Plugin\pipeline\Pipeline;
+
+use Drupal\joinup_federation\JoinupFederationPipelinePluginBase;
+
+/**
+ * The data pipeline for the Joinup collection.
+ *
+ * @PipelinePipeline(
+ *   id = "joinup_collection",
+ *   label = @Translation("Joinup collection - ADMSv1 > ADMSv2"),
+ *   steps = {
+ *     "manual_upload",
+ *     "remove_unsupported_data",
+ *     "add_joinup_vocabularies",
+ *     "adms_validation",
+ *     "user_selection_filter" = {
+ *       "collection" = "http://placeHolder/11c81d8f-1527-4044-a694-b847d66362e1",
+ *     },
+ *     "3_way_merge" = {
+ *       "collection" = "http://placeHolder/11c81d8f-1527-4044-a694-b847d66362e1",
+ *     },
+ *     "broken_references",
+ *     "joinup_validation",
+ *     "import",
+ *     "provenance_activity" = {
+ *       "collection" = "http://placeHolder/11c81d8f-1527-4044-a694-b847d66362e1",
+ *     },
+ *   },
+ * )
+ */
+class JoinupCollectionPipeline extends JoinupFederationPipelinePluginBase {}
