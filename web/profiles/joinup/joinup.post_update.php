0ed6e7a304269369b87959f02a635bb76007bf21
Francesco Sardara <francesco.sardara@ausy.be>
2019-01-24 15:03:19 +0100

    ISAICP-4624: add upgrade path.

--- web/profiles/joinup/joinup.post_update.php
+++ web/profiles/joinup/joinup.post_update.php
@@ -11,3 +11,10 @@
 function joinup_post_update_install_views_data_export() {
   \Drupal::service('module_installer')->install(['views_data_export']);
 }
+
+/**
+ * Enable the "Joinup RSS" module.
+ */
+function joinup_post_update_install_joinup_rss() {
+  \Drupal::service('module_installer')->install(['joinup_rss']);
+}
