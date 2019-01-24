89fc665aa6476969a6bff426f7b70ffba13ceec4
Francesco Sardara <francesco.sardara@ausy.be>
2019-01-24 09:18:10 +0100

    ISAICP-4624: use search api trait to fix data extraction from result items.

--- web/modules/custom/joinup_search/src/Plugin/views/field/SearchApiEntityLink.php
+++ web/modules/custom/joinup_search/src/Plugin/views/field/SearchApiEntityLink.php
@@ -4,9 +4,8 @@ declare(strict_types = 1);
 
 namespace Drupal\joinup_search\Plugin\views\field;
 
-use Drupal\Core\Entity\EntityInterface;
+use Drupal\search_api\Plugin\views\field\SearchApiFieldTrait;
 use Drupal\views\Plugin\views\field\EntityLink;
-use Drupal\views\ResultRow;
 
 /**
  * Field handler to present a link to an entity.
@@ -17,15 +16,6 @@ use Drupal\views\ResultRow;
  */
 class SearchApiEntityLink extends EntityLink {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getEntity(ResultRow $values) {
-    if (isset($values->_object) && $values->_object->getValue() instanceof EntityInterface) {
-      return $values->_object->getValue();
-    }
-
-    return NULL;
-  }
+  use SearchApiFieldTrait;
 
 }
