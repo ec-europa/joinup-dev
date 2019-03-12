5b177d61908d98bd035adf49b9cc779ac7d63cb5
balintjunkuncz <junkuncz@gmail.com>
2019-03-12 15:13:25 +0100

    ISAICP-5049: Added Constraint to validate two empty fields at the same time

---
+++ web/modules/custom/joinup_core/src/Plugin/Validation/Constraint/OptionalRequiredLocationFieldsValidator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\joinup_core\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Validates the Location fields.
+ */
+class OptionalRequiredLocationFieldsValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($entity, Constraint $constraint) {
+    /** @var \Drupal\Core\Entity\EntityInterface $entity */
+    if (!$entity instanceof \Drupal\node\NodeInterface) {
+      // We only want to work with nodes.
+      return;
+    }
+
+    if (!$entity->bundle() === 'event') {
+      // Don't do anything if it is not an event content.
+      return;
+    }
+
+    // Retrieve field values from the Constraint.
+    $field_name_1 = $constraint->field_name_1;
+    $field_name_2 = $constraint->field_name_2;
+
+    // Just to make sure the fields exist.
+    if ($entity->hasField($field_name_1) && $entity->hasField($field_name_2)) {
+      // Add Violation when both of them are empty.
+      if ($entity->get($field_name_1)->isEmpty() && $entity->get($field_name_2)->isEmpty()) {
+        $this->context->addViolation($constraint->message);
+      }
+    }
+  }
+
+}
