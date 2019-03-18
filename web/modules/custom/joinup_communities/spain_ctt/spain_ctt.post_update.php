ae6a15ac51bdc8c9f5d15598a5f3888a3930b0b4
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-03-18 12:50:14 +0200

    ISAICP-5156: Delete unwanted entities from CTT.

--- web/modules/custom/joinup_communities/spain_ctt/spain_ctt.post_update.php
+++ web/modules/custom/joinup_communities/spain_ctt/spain_ctt.post_update.php
@@ -86,3 +86,67 @@ QUERY;
   $connection = \Drupal::service('sparql_endpoint');
   $connection->query($query);
 }
+
+/**
+ * Delete unwanted ctt entities.
+ */
+function spain_ctt_post_update_delete_ctt_entities() {
+  $solution_ids = [
+    'http://administracionelectronica.gob.es/ctt/dir3caib_1',
+    'http://administracionelectronica.gob.es/ctt/lucia_1',
+    'http://administracionelectronica.gob.es/ctt/censoelectoral',
+    'http://administracionelectronica.gob.es/ctt/pasareladepagos',
+    'http://administracionelectronica.gob.es/ctt/arcas',
+    'http://administracionelectronica.gob.es/ctt/autentica_1',
+    'http://administracionelectronica.gob.es/ctt/compartircursosonline',
+    'http://administracionelectronica.gob.es/ctt/adise',
+    'http://administracionelectronica.gob.es/ctt/cursosonline',
+    'http://administracionelectronica.gob.es/ctt/registra',
+    'http://administracionelectronica.gob.es/ctt/plantillaspge',
+    'http://administracionelectronica.gob.es/ctt/sipas',
+    'http://administracionelectronica.gob.es/ctt/publiges',
+    'http://administracionelectronica.gob.es/ctt/tienda',
+    'http://administracionelectronica.gob.es/ctt/gesval',
+    'http://administracionelectronica.gob.es/ctt/gama',
+    'http://administracionelectronica.gob.es/ctt/gades',
+    'http://administracionelectronica.gob.es/ctt/gpf',
+    'http://administracionelectronica.gob.es/ctt/allopd',
+    'http://administracionelectronica.gob.es/ctt/aplica_1',
+    'http://administracionelectronica.gob.es/ctt/aplica',
+    'http://administracionelectronica.gob.es/ctt/estadisticatrama',
+    'http://administracionelectronica.gob.es/ctt/grial',
+    'http://administracionelectronica.gob.es/ctt/encuestaenlinea',
+    'http://administracionelectronica.gob.es/ctt/alefacil',
+    'http://administracionelectronica.gob.es/ctt/alweb',
+    'http://administracionelectronica.gob.es/ctt/alpadron',
+    'http://administracionelectronica.gob.es/ctt/ces',
+    'http://administracionelectronica.gob.es/ctt/buzon060',
+    'http://administracionelectronica.gob.es/ctt/extra',
+    'http://administracionelectronica.gob.es/ctt/ticketing',
+    'http://administracionelectronica.gob.es/ctt/sorolla',
+    'http://administracionelectronica.gob.es/ctt/road',
+    'http://administracionelectronica.gob.es/ctt/red060',
+    'http://administracionelectronica.gob.es/ctt/note',
+    'http://administracionelectronica.gob.es/ctt/pac',
+    'http://administracionelectronica.gob.es/ctt/proa',
+    'http://administracionelectronica.gob.es/ctt/gesfoge',
+    'http://administracionelectronica.gob.es/ctt/gesnote',
+    'http://administracionelectronica.gob.es/ctt/foge',
+    'http://administracionelectronica.gob.es/ctt/docuconta',
+    'http://administracionelectronica.gob.es/ctt/canoa',
+    'http://administracionelectronica.gob.es/ctt/tramitador',
+  ];
+  $storage = \Drupal::entityTypeManager()->getStorage('rdf_entity');
+
+  foreach ($solution_ids as $solution_id) {
+    $solution = $storage->load($solution_id);
+    // Solutions in CTT have maximum one direct reference to distributions
+    // and no reference to a release.
+    // These solutions also do not have any community content.
+    if ($distribution_id = $solution->field_is_distribution->target_id) {
+      $distribution = $storage->load($distribution_id);
+      $distribution->delete();
+    }
+    $solution->delete();
+  }
+}
