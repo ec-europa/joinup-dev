4f3f08d893491d60df5a07b5ae090797e63db04b
Claudiu Cristea <clau.cristea@gmail.com>
2019-03-27 16:50:50 +0200

    ISAICP-4758: Make Solr server configurable.

--- scripts/solr/restore_solr.sh
+++ scripts/solr/restore_solr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cr=$'\r'
-PORT=8983
+SOLR_SERVER_URL="http://localhost:8983/solr"
 TIMEOUT=300
 re='^[0-9]+$'
 
@@ -12,8 +12,9 @@ function show_help {
  echo -e "Restores a Solr core from a named snapshot located in a directory"
  echo -e "-h    --help          Show this help text"
  echo -e "-c    --core          Solr core to be restored"
- echo -e "-s    --snapshot-dir  The directory containing the snapshot"
+ echo -e "-d    --snapshot-dir  The directory containing the snapshot"
  echo -e "-n    --snapshot-name The snapshot name"
+ echo -e "-u    --solr-url      Solr server URL. Defaults to http://localhost:8983/solr"
  echo -e "-t    --timeout       Restore time timeout"
  echo -e "-v    --verbose       If set, verbose logging is on"
 }
@@ -26,8 +27,9 @@ while [[ "$1" == -* ]]; do
    --) shift; break;;
    -v|--verbose) shift; VERBOSE_LOG="yes";;
    -t|--timeout) shift; TIMEOUT=$1; shift;;
-   -s|--snapshot-dir) shift; SNAPSHOT_DIR=$1; shift;;
-   -s|--snapshot-name) shift; SNAPSHOT_NAME=$1; shift;;
+   -d|--snapshot-dir) shift; SNAPSHOT_DIR=$1; shift;;
+   -n|--snapshot-name) shift; SNAPSHOT_NAME=$1; shift;;
+   -u|--solr-url) shift; SOLR_SERVER_URL=$1; shift;;
    -*) echo "invalid option: $1" 1>&2; show_help; exit 1;;
  esac
 done
@@ -42,7 +44,7 @@ if [ "${CORE}" == '' ] || [ "${SNAPSHOT_DIR}" == '' ] || [ "${SNAPSHOT_NAME}" ==
   exit 1;
 fi
 
-CORE_EXISTS=`curl -sS "http://localhost:${PORT}/solr/admin/cores?action=STATUS&core=${CORE}" |grep -o '<long name="uptime">'`
+CORE_EXISTS=`curl -sS "${SOLR_SERVER_URL}/admin/cores?action=STATUS&core=${CORE}" |grep -o '<long name="uptime">'`
 
 if [ "${CORE_EXISTS}" == '' ]; then
   echo "$(tput setaf 1)CORE does not exists on this solr server ! please take an other one$(tput sgr0)";
@@ -63,7 +65,7 @@ if [ ! -d "${SNAPSHOT_DIR}/snapshot.${SNAPSHOT_NAME}" ]; then
 fi
 
 # Restore de index.
-line="http://localhost:8983/solr/${CORE}/replication?command=restore&name=${SNAPSHOT_NAME}&location=${SNAPSHOT_DIR}"
+line="${SOLR_SERVER_URL}/${CORE}/replication?command=restore&name=${SNAPSHOT_NAME}&location=${SNAPSHOT_DIR}"
 RESTORE_START=`/usr/bin/curl -sS ${line}`
 
 if [ "${VERBOSE_LOG}" == "yes"  ]; then
@@ -87,7 +89,7 @@ while [[ "${SUCCESS}" != '<str name="status">success</str>' ]] && [[ ${COUNTER} 
 do
    sleep 1
 
-   CHECK_SUCCESS=`/usr/bin/curl -sS "http://localhost:8983/solr/${CORE}/replication?command=restorestatus&name=${SNAPSHOT_NAME}&location=${SNAPSHOT_DIR}"`
+   CHECK_SUCCESS=`/usr/bin/curl -sS "${SOLR_SERVER_URL}/${CORE}/replication?command=restorestatus&name=${SNAPSHOT_NAME}&location=${SNAPSHOT_DIR}"`
    SUCCESS=`echo ${CHECK_SUCCESS} | grep -o '<str name="status">success</str>'`
 
    COUNTER=$((COUNTER+1))
