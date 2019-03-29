3dd92992c9e7eb3b0a030623897328c5fe7bc58d
Tomasz Rutkowski <tom@waat.eu>
2019-03-29 12:02:31 +0100

    Styling for chipfield component in searchbar

--- web/themes/joinup/js/global-search.js
+++ web/themes/joinup/js/global-search.js
@@ -5,6 +5,15 @@
     attach: function (context, settings) {
       // Attach the JS behaviour from mdl-chip-input to the search bar.
       new window['MaterialChipInput'](jQuery('.search-bar.mdl-chipfield')[0]);
+
+      // Position input cursor if chip is available.
+      $('.search-bar--header').each(function () {
+        var chipWidth = $(this).find('.mdl-chip').width();
+        if (chipWidth !== 0) {
+          chipWidth = chipWidth + 30;
+          $(this).find('.search-bar__input').css('padding-left', chipWidth + 'px');
+        }
+      });
     }
   };
 
