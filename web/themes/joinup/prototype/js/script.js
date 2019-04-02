f1ac9b390b419d5e8745a8d962f10b1ded6d4bba
Tomasz Rutkowski <tom@waat.eu>
2019-04-02 16:18:36 +0200

    ISAICP-5149: Add javascript search input

--- web/themes/joinup/prototype/js/script.js
+++ web/themes/joinup/prototype/js/script.js
@@ -75,52 +75,77 @@ var loadMore = loadMore || {};
 })(jQuery);
 
 (function ($) {
-  $('.license-filter__item a').each(function () {
-    $(this).on('click', function (event) {
-      event.preventDefault();
+  "use strict";
+  function checkLicenseCategories() {
+    var $licenseTile = $('.license-tile');
 
-      var currentClass = $(this).attr('class');
-      if (typeof currentClass !== 'undefined') {
-        var classes = currentClass.split(' ');
-        $('.license-tile').each(function () {
-          if ($(this).find('.' + classes[0]).length === 0) {
-            if (classes.length > 1) {
-              $(this).removeClass('is-hidden');
-            }
-            else if (!$(this).hasClass('is-hidden')) {
+    // Remove hidden class before further processing.
+    $licenseTile.each(function () {
+      $(this).removeClass('is-hidden');
+    });
+
+    // Check every active filter item
+    // and hide tiles which don't contain proper data-license-category
+    $('.license-filter__item a.is-active').each(function () {
+      var currentlicenseCategory = $(this).attr('data-license-category');
+      if (typeof currentlicenseCategory !== 'undefined') {
+        $licenseTile.each(function () {
+          var licenseCategory = $(this).attr('data-license-category');
+          if (typeof licenseCategory !== 'undefined') {
+            var licenseCategoryArray = licenseCategory.split(' ');
+            if ($.inArray(currentlicenseCategory, licenseCategoryArray) < 0 && !$(this).hasClass('is-hidden')) {
               $(this).addClass('is-hidden');
             }
           }
         });
       }
+    });
+
+    // Check license search field value
+    // and hide tiles which don't contain proper data-spdx
+    var licenseTiles = 0;
+    var currentSpdxId = $('#license-search').val();
+    $licenseTile.each(function () {
+      if (currentSpdxId.length > 0) {
+        var spdxId = $(this).attr('data-spdx');
+        if (!spdxId.includes(currentSpdxId) && !$(this).hasClass('is-hidden')) {
+          $(this).addClass('is-hidden');
+        }
+      }
+
+      // Count not hidden tiles.
+      if (!$(this).hasClass('is-hidden')) {
+        licenseTiles++;
+      }
+    });
+
+    // Show calculated number of tiles
+    $('.license-counter__number').text(licenseTiles);
+  }
+
+  // Trigger if license filter is clicked.
+  $('.license-filter__item a').each(function () {
+    $(this).on('click', function (event) {
+      event.preventDefault();
 
       $(this).toggleClass('is-active');
 
-      $('.license-filter__item a.is-active').each(function () {
-        var currentClass = $(this).attr('class');
-        if (typeof currentClass !== 'undefined') {
-          var classes = currentClass.split(' ');
-
-          $('.license-tile').each(function () {
-            if ($(this).find('.' + classes[0]).length === 0) {
-              if (classes.length > 1 && !$(this).hasClass('is-hidden')) {
-                $(this).addClass('is-hidden');
-              }
-            }
-          });
-        }
-      });
-
-      var licenseTiles = 0;
-      $('.license-tile').each(function () {
-        if (!$(this).hasClass('is-hidden')) {
-          licenseTiles++;
-        }
-      });
-
-      $('.license-counter__number').text(licenseTiles);
+      checkLicenseCategories();
 
     });
+  });
+
+  // Trigger if enter key is pressed in license search
+  $('#license-search').on('keypress', function (event) {
+    if (event.which === 13) {
+      checkLicenseCategories();
+    }
+  });
+
+  // Filter on window load
+  // Needed for license search filter.
+  $(window).load(function() {
+    checkLicenseCategories();
   });
 
 })(jQuery);
