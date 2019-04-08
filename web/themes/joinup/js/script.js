2abf7bce41013e3e580af8251303c39170ff429c
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-04-08 08:32:15 +0300

    ISAICP-5141: Update the theme's script.js according to the prototype.

--- web/themes/joinup/js/script.js
+++ web/themes/joinup/js/script.js
@@ -31,3 +31,79 @@
     }
   });
 })(jQuery);
+
+(function ($) {
+  "use strict";
+  function checkLicenseCategories() {
+    var $licenseTile = $('.license-tile');
+
+    // Remove hidden class before further processing.
+    $licenseTile.each(function () {
+      $(this).removeClass('is-hidden');
+    });
+
+    // Check every active filter item
+    // and hide tiles which don't contain proper data-license-category
+    $('.license-filter__item a.is-active').each(function () {
+      var currentlicenseCategory = $(this).attr('data-license-category');
+      if (typeof currentlicenseCategory !== 'undefined') {
+        $licenseTile.each(function () {
+          var licenseCategory = $(this).attr('data-license-category');
+          if (typeof licenseCategory !== 'undefined') {
+            var licenseCategoryArray = licenseCategory.split(' ');
+            if ($.inArray(currentlicenseCategory, licenseCategoryArray) < 0 && !$(this).hasClass('is-hidden')) {
+              $(this).addClass('is-hidden');
+            }
+          }
+        });
+      }
+    });
+
+    // Check license search field value
+    // and hide tiles which don't contain proper data-spdx
+    var licenseTiles = 0;
+    var currentSpdxId = $('#license-search').val();
+    $licenseTile.each(function () {
+      if (currentSpdxId.length > 0) {
+        var spdxId = $(this).attr('data-spdx');
+        if (!spdxId.includes(currentSpdxId) && !$(this).hasClass('is-hidden')) {
+          $(this).addClass('is-hidden');
+        }
+      }
+
+      // Count not hidden tiles.
+      if (!$(this).hasClass('is-hidden')) {
+        licenseTiles++;
+      }
+    });
+
+    // Show calculated number of tiles
+    $('.license-counter__number').text(licenseTiles);
+  }
+
+  // Trigger if license filter is clicked.
+  $('.license-filter__item a').each(function () {
+    $(this).on('click', function (event) {
+      event.preventDefault();
+
+      $(this).toggleClass('is-active');
+
+      checkLicenseCategories();
+
+    });
+  });
+
+  // Trigger if enter key is pressed in license search
+  $('#license-search').on('keypress', function (event) {
+    if (event.which === 13) {
+      checkLicenseCategories();
+    }
+  });
+
+  // Filter on window load
+  // Needed for license search filter.
+  $(window).load(function() {
+    checkLicenseCategories();
+  });
+
+})(jQuery);
