ee1b7e828c622f7d65fa76c9ed39a8bbef91f183
Pieter Frenssen <pieter@frenssen.be>
2019-05-22 21:22:46 +0300

    ISAICP-5254: Remove cruft.

--- web/modules/custom/joinup_subscription/src/Form/UnsubscribeFromAllCollectionsForm.php
+++ web/modules/custom/joinup_subscription/src/Form/UnsubscribeFromAllCollectionsForm.php
@@ -122,21 +122,6 @@ class UnsubscribeFromAllCollectionsForm extends ConfirmFormBase {
   }
 
   /**
-   * Submit callback for the return button.
-   *
-   * Returns the user to the same destination as the cancel URL.
-   *
-   * @param array $form
-   *   The form array.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The form state object.
-   */
-  public function returnSubmit(array &$form, FormStateInterface $form_state): void {
-    $return_url = $this->getCancelUrl();
-    $form_state->setRedirect($return_url->getRouteName(), $return_url->getRouteParameters());
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
