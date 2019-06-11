1a4812f40fba7898a4d4f9ff9f15c3e21e9780cb
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-06-11 10:59:43 +0300

    ISAICP-4643: Fix naming in update method.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -664,7 +664,7 @@ function joinup_core_post_update_set_news_default_version() {
 /**
  * Enforce the privacy mode for providers that support it.
  */
-function joinup_core_post_install_set_video_privacy_mode() {
+function joinup_core_post_update_set_video_privacy_mode() {
   \Drupal::service('config.factory')
     ->getEditable('video_embed_field.settings')
     ->set('privacy_mode', 'enabled')
