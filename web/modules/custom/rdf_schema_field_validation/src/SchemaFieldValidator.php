946f41cecf90cd074f602d023dfcc82d35dd96e3
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-06-27 15:37:49 +0300

    ISAICP-5330: Provide a correct validation message as the previous one was missleading.

--- web/modules/custom/rdf_schema_field_validation/src/SchemaFieldValidator.php
+++ web/modules/custom/rdf_schema_field_validation/src/SchemaFieldValidator.php
@@ -59,7 +59,7 @@ class SchemaFieldValidator implements SchemaFieldValidatorInterface {
   public function isDefinedInSchema(string $entity_type_id, string $bundle, string $field_name, string $column_name = ''): bool {
     $mapping = $this->getEntityMapping($entity_type_id, $bundle);
     if (empty($mapping) || empty($properties = $mapping->getThirdPartySettings('rdf_schema_field_validation'))) {
-      throw new \Exception("The entity does not appear to have mapped properties.");
+      throw new \Exception('The entity bundle does not appear to have a schema associated with it so it cannot validate the mapping of its fields.');
     }
 
     try {
