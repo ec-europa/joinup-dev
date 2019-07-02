dbc12f53014129470c483dca06c0066865d489cf
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-07-02 11:56:59 +0300

    ISAICP-5057: Install the publication_date module.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -667,3 +667,10 @@ function joinup_core_post_update_set_news_default_version() {
 function joinup_core_post_update_enable_nio() {
   \Drupal::service('module_installer')->install(['nio']);
 }
+
+/**
+ * Enable the Publication Date module.
+ */
+function joinup_core_post_update_install_publication_date() {
+  \Drupal::service('module_installer')->install(['publication_date']);
+}
