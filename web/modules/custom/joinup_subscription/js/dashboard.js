13b2eca4cbdce90eac09d1f0fac4e8bc49be48e2
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-07-24 03:09:37 +0300

    ISAICP-5089: Add JS to mimic the checkboxes implementation for the select box.

--- web/modules/custom/joinup_subscription/js/dashboard.js
+++ web/modules/custom/joinup_subscription/js/dashboard.js
@@ -3,16 +3,27 @@
     attach: function (context, settings) {
       // Whenever any of the checkboxes for a collection is clicked, enable the
       // submit button.
+      $(context).find('.form-select').each(function () {
+        $(this)
+          .on('change', function () {
+            $(this)
+              .closest('.field--name-field-user-frequency')
+              .find('input.button')
+              .prop('disabled', false)
+              .prop('value', 'Save changes')
+              .removeClass('is-disabled');
+          });
+      });
       $(context).find('input.form-checkbox').once('dashboard').each(function () {
         $(this)
-          .on('click', function () {
-          $(this)
-            .closest('.form__subscribe-types-inner')
-            .find('input.button')
-            .prop('disabled', false)
-            .prop('value', 'Save changes')
-            .removeClass('is-disabled');
-        });
+            .on('click', function () {
+              $(this)
+                  .closest('.form__subscribe-types-inner')
+                  .find('input.button')
+                  .prop('disabled', false)
+                  .prop('value', 'Save changes')
+                  .removeClass('is-disabled');
+            });
       });
     }
   };
