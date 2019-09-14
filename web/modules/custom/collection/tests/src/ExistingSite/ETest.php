fd96812d8e8b16dff663a32cfbfe04476132c0aa
Claudiu Cristea <clau.cristea@gmail.com>
2019-09-14 10:47:09 +0300

    dtt

--- web/modules/custom/collection/tests/src/ExistingSite/ETest.php
+++ web/modules/custom/collection/tests/src/ExistingSite/ETest.php
@@ -6,21 +6,24 @@ use weitzman\DrupalTestTraits\Entity\NodeCreationTrait;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
 /**
+ * Test.
+ *
  * @group collection
  */
 class ETest extends ExistingSiteBase {
 
   use NodeCreationTrait;
 
+  /**
+   * Test.
+   */
   public function test() {
     $n = $this->createNode([
       'title' => 'Page',
-      'type' => 'custom_page'
+      'type' => 'custom_page',
     ]);
-
 
     $this->assertSame('Page', $n->label());
   }
 
 }
-
