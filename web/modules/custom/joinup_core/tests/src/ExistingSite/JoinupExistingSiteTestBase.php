d7b93ba0dc2e8b34683d158a93b1688e8ee9b501
Claudiu Cristea <clau.cristea@gmail.com>
2019-09-14 16:59:11 +0300

    m

--- web/modules/custom/joinup_core/tests/src/ExistingSite/JoinupExistingSiteTestBase.php
+++ web/modules/custom/joinup_core/tests/src/ExistingSite/JoinupExistingSiteTestBase.php
@@ -16,9 +16,18 @@ class JoinupExistingSiteTestBase extends ExistingSiteBase {
    */
   public function tearDown(): void {
     // Make sure we don't send any notifications during test entities cleanup.
-    foreach ($this->cleanupEntities as $delta => $entity) {
-      $this->cleanupEntities[$delta]->skip_notification = TRUE;
+    foreach ($this->cleanupEntities as $entity) {
+      $entity->skip_notification = TRUE;
     }
+
+    /** @var \Drupal\Component\Plugin\PluginManagerInterface $delete_orphans_manager */
+    $delete_orphans_manager = \Drupal::service('plugin.manager.og.delete_orphans');
+    /** @var \Drupal\og\OgDeleteOrphansInterface $delete_orphans_plugin */
+    $delete_orphans_plugin = $delete_orphans_manager->createInstance('simple');
+    // Delete the OG group content orphans now because parent::tearDown() is
+    // destroying the container and the registered shutdown callback will fail.
+    $delete_orphans_plugin->process();
+
     parent::tearDown();
   }
 
