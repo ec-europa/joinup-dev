6de3dba3a04c5d0e99b3c970acc03a2488db6282
Claudiu Cristea <clau.cristea@gmail.com>
2019-09-28 00:32:02 +0300

    ISAICP-5495: Temporary remove Antibot protection during ExistingSiteBase tests.

--- web/modules/custom/joinup_core/tests/src/ExistingSite/JoinupExistingSiteTestBase.php
+++ web/modules/custom/joinup_core/tests/src/ExistingSite/JoinupExistingSiteTestBase.php
@@ -4,12 +4,40 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\joinup_core\ExistingSite;
 
+use Drupal\joinup\Traits\ConfigReadOnlyTrait;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
 /**
  * Base class for Joinup  ExistingSite tests.
  */
 class JoinupExistingSiteTestBase extends ExistingSiteBase {
+
+  use ConfigReadOnlyTrait;
+
+  /**
+   * The current list of form IDs protected by Antibot.
+   *
+   * @var string[]
+   */
+  protected $currentAntibotFormIds;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $antibot_settings = \Drupal::configFactory()->getEditable('antibot.settings');
+    // Save the current form IDs protected by Antibot.
+    $this->currentAntibotFormIds = $antibot_settings->get('form_ids');
+    // Unprotect all forms for the scope of this test. Antibot is blocking all
+    // form submissions when javascript is disabled. As ExistingSiteBase tests
+    // are running without javascript, we disable Antibot for all forms during
+    // the tests.
+    $this->bypassReadOnlyConfig();
+    $antibot_settings->set('form_ids', [])->save();
+    $this->restoreReadOnlyConfig();
+  }
 
   /**
    * {@inheritdoc}
@@ -27,6 +55,14 @@ class JoinupExistingSiteTestBase extends ExistingSiteBase {
     // Delete the OG group content orphans now because parent::tearDown() is
     // destroying the container and the registered shutdown callback will fail.
     $delete_orphans_plugin->process();
+
+    // Restore the list of form IDs to be protected by Antibot.
+    $this->bypassReadOnlyConfig();
+    \Drupal::configFactory()
+      ->getEditable('antibot.settings')
+      ->set('form_ids', $this->currentAntibotFormIds)
+      ->save();
+    $this->restoreReadOnlyConfig();
 
     parent::tearDown();
   }
