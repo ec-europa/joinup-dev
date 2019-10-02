5fd14ac3e67d4afe7701e14a14d3c54517e86db7
Pieter Frenssen <pieter@frenssen.be>
2019-10-02 11:14:41 +0300

    ISAICP-4910: Implement same state update permission for the Owner entity.

---
+++ web/modules/custom/owner/src/Plugin/WorkflowStatePermission/OwnerWorkflowStatePermission.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\owner\Plugin\WorkflowStatePermission;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Plugin\PluginBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\joinup_core\WorkflowStatePermissionPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Checks whether changing workflow states is permitted for a given user.
+ *
+ * Depending on the user role some workflow states are not available. For
+ * example an author can request deletion of an owner entity, but only a
+ * moderator can approve the deletion.
+ *
+ * @WorkflowStatePermission(
+ *   id = "owner",
+ * )
+ *
+ * @see owner.settings.yml
+ */
+class OwnerWorkflowStatePermission extends PluginBase implements WorkflowStatePermissionPluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a SolutionWorkflowStatePermissions object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $configFactory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(EntityInterface $entity): bool {
+    return $entity->getEntityTypeId() === 'rdf_entity' && $entity->bundle() === 'owner';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isStateUpdatePermitted(AccountInterface $account, EntityInterface $entity, string $from_state, string $to_state): bool {
+    $allowed_conditions = $this->configFactory->get('owner.settings')->get('transitions');
+
+    if ($account->hasPermission('administer rdf entity')) {
+      return TRUE;
+    }
+
+    // Check if the user has one of the allowed system roles.
+    $authorized_roles = isset($allowed_conditions[$to_state][$from_state]) ? $allowed_conditions[$to_state][$from_state] : [];
+    return (bool) array_intersect($authorized_roles, $account->getRoles());
+  }
+
+}
