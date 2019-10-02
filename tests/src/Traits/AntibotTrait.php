a1f66d1b19913c4d04930ee08553639118f13c13
Claudiu Cristea <clau.cristea@gmail.com>
2019-10-02 10:05:02 +0300

    ISAICP-5495: Workaround Behat tests static cache issue.

--- tests/src/Traits/AntibotTrait.php
+++ tests/src/Traits/AntibotTrait.php
@@ -23,8 +23,21 @@ trait AntibotTrait {
    */
   protected static function disableAntibot(): void {
     $antibot_settings = \Drupal::configFactory()->getEditable('antibot.settings');
+
     // Save the current form IDs protected by Antibot.
     static::$currentAntibotFormIds = $antibot_settings->get('form_ids');
+
+    // Workaround Behat tests static cache persistence issue. Even all forms are
+    // tagged with 'config:antibot.settings' cache tag and saving Antibot
+    // settings will call the cache tags invalidator, as Behat tests are running
+    // in a single, long requests, the cache tags invalidator checksum service
+    // is statically caching the tags. We have to reset the service internal
+    // cache prior saving the settings, so that the subsequent cache
+    // invalidation to be in effect. Note that this is not needed, in a real
+    // scenario, where the cache tags invalidator checksum service internal
+    // cache is cleared at the end of each request.
+    \Drupal::service('cache_tags.invalidator.checksum')->reset();
+
     // Unprotect all forms for the scope of this test. Antibot is blocking all
     // form submissions when javascript is disabled. As most of the tests are
     // running without javascript, we disable Antibot for all forms during the
