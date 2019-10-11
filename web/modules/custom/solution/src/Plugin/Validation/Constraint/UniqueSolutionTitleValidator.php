540cc4e1fdd20ecf67b7e8a9b6fef3e02bf760c7
Claudiu Cristea <clau.cristea@gmail.com>
2019-10-11 16:28:57 +0300

    ISAICP-5574: Replace the usage of procedural code in UniqueSolutionTitleValidator.

--- web/modules/custom/solution/src/Plugin/Validation/Constraint/UniqueSolutionTitleValidator.php
+++ web/modules/custom/solution/src/Plugin/Validation/Constraint/UniqueSolutionTitleValidator.php
@@ -4,6 +4,9 @@ declare(strict_types = 1);
 
 namespace Drupal\solution\Plugin\Validation\Constraint;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\solution\SolutionTitleDuplicateHelperInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
@@ -20,6 +23,32 @@ use Symfony\Component\Validator\ConstraintValidator;
 class UniqueSolutionTitleValidator extends ConstraintValidator implements ContainerInjectionInterface {
 
   /**
+   * The solution title duplicate helper service.
+   *
+   * @var \Drupal\solution\SolutionTitleDuplicateHelperInterface
+   */
+  protected $solutionTitleHelper;
+
+  /**
+   * Constructs a new validator instance.
+   *
+   * @param \Drupal\solution\SolutionTitleDuplicateHelperInterface $solution_title_helper
+   *   The solution title duplicate helper service.
+   */
+  public function __construct(SolutionTitleDuplicateHelperInterface $solution_title_helper) {
+    $this->solutionTitleHelper = $solution_title_helper;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): self {
+    return new static(
+      $container->get('solution.title_duplicate_helper')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function validate($items, Constraint $constraint): void {
@@ -34,7 +63,12 @@ class UniqueSolutionTitleValidator extends ConstraintValidator implements Contai
       throw new \InvalidArgumentException("This validator is designated only for solutions.");
     }
 
-    if (!solution_title_is_unique($entity)) {
+    // Either the solution's title is not unique within affiliation, or the
+    // solution title has duplicates with other solutions, all without
+    // affiliation. In the first case ::titleIsUniqueWithinAffiliation() returns
+    // FALSE, in the latter NULL.
+    // @see \Drupal\solution\SolutionTitleDuplicateHelperInterface::titleIsUniqueWithinAffiliation()
+    if (!$this->solutionTitleHelper->titleIsUniqueWithinAffiliation($solution)) {
       $this->context->addViolation($constraint->message, [
         '%value' => $item->value,
         '@entity_type' => $solution->getEntityType()->getLowercaseLabel(),
