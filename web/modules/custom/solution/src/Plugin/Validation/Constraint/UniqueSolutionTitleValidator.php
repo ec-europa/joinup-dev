f35571b0c0dd3da9a6d681378f7719840e195d7f
Claudiu Cristea <clau.cristea@gmail.com>
2019-10-11 16:18:26 +0300

    ISAICP-5574: Modernize the UniqueSolutionTitle constraint & validator.

--- web/modules/custom/solution/src/Plugin/Validation/Constraint/UniqueSolutionTitleValidator.php
+++ web/modules/custom/solution/src/Plugin/Validation/Constraint/UniqueSolutionTitleValidator.php
@@ -1,39 +1,44 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\solution\Plugin\Validation\Constraint;
 
-use Drupal\Component\Utility\Unicode;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 /**
  * Validates that a field is unique for the given entity type within a bundle.
  *
- * This is the validator for the UniqueSolutionTitleConstraint.
+ * This is the validator for the UniqueSolutionTitle.
  *
  * The following checks make sure that a solution must have a unique title among
  * within their collections.
  *
- * @see \Drupal\solution\Plugin\Validation\Constraint\UniqueSolutionTitleConstraint
+ * @see \Drupal\solution\Plugin\Validation\Constraint\UniqueSolutionTitle
  */
-class UniqueSolutionTitleValidator extends ConstraintValidator {
+class UniqueSolutionTitleValidator extends ConstraintValidator implements ContainerInjectionInterface {
 
   /**
    * {@inheritdoc}
    */
-  public function validate($items, Constraint $constraint) {
+  public function validate($items, Constraint $constraint): void {
     if (!$item = $items->first()) {
       return;
     }
 
-    /** @var \Drupal\rdf_entity\RdfInterface $entity */
-    $entity = $items->getEntity();
+    /** @var \Drupal\rdf_entity\RdfInterface $solution */
+    $solution = $items->getEntity();
+
+    if ($solution->bundle() !== 'solution') {
+      throw new \InvalidArgumentException("This validator is designated only for solutions.");
+    }
 
     if (!solution_title_is_unique($entity)) {
       $this->context->addViolation($constraint->message, [
         '%value' => $item->value,
-        '@entity_type' => $entity->getEntityType()->getLowercaseLabel(),
-        '@field_name' => Unicode::strtolower($items->getFieldDefinition()->getLabel()),
+        '@entity_type' => $solution->getEntityType()->getLowercaseLabel(),
+        '@field_name' => $items->getFieldDefinition()->getLabel(),
       ]);
     }
   }
