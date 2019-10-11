3e240a8c97f5b5889a5e2a904ad12490ef7d0393
Tomasz Rutkowski <tom@waat.eu>
2019-10-11 17:50:31 +0200

    ISAICP-5436: Disable the submit button if checkbox is toggled on and back off

--- web/modules/custom/joinup_subscription/js/my_subscriptions.js
+++ web/modules/custom/joinup_subscription/js/my_subscriptions.js
@@ -16,14 +16,33 @@
       });
       $(context).find('input.form-checkbox').once('dashboard').each(function () {
         $(this)
-            .on('click', function () {
-              $(this)
-                  .closest('.form__subscribe-types-inner')
-                  .find('input.button')
-                  .prop('disabled', false)
-                  .prop('value', 'Save changes')
-                  .removeClass('is-disabled');
+          .on('click', function () {
+            var subscription_status = [];
+
+            // Get current status of checkboxes.
+            $(this).closest('.form__subscribe-types-inner').find('input.form-checkbox').each(function() {
+              subscription_status.push($(this).prop('checked'));
             });
+
+            // Get subscription button.
+            var $button = $(this).closest('.form__subscribe-types-inner').find('input.button');
+
+            // Get initial status of checkboxes stored inside button attribute.
+            var subscription_status_initial = $button.attr('data-drupal-subscriptions');
+
+            // Compare current and initial status of checkboxes.
+            if (JSON.stringify(subscription_status) === subscription_status_initial) {
+              $button
+                .prop('disabled', true)
+                .addClass('is-disabled');
+            }
+            else {
+              $button
+                .prop('disabled', false)
+                .prop('value', 'Save changes')
+                .removeClass('is-disabled');
+            }
+          });
       });
     }
   };
