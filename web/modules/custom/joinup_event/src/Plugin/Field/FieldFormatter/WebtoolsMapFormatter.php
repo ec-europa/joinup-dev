47f60565c313ae7c48a95eb989a0487ca26bbc8a
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-10-19 00:39:43 +0300

    ISAICP-5564: Override the webtools map formatter to add the render: true property.

---
+++ web/modules/custom/joinup_event/src/Plugin/Field/FieldFormatter/WebtoolsMapFormatter.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_event\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\oe_webtools_maps\Component\Render\JsonEncoded;
+use Drupal\oe_webtools_maps\Plugin\Field\FieldFormatter\WebtoolsMapFormatter as OriginalWebtoolsMapFormatter;
+
+/**
+ * Displays a Geofield as a map using the Webtools Maps service.
+ */
+class WebtoolsMapFormatter extends OriginalWebtoolsMapFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = parent::viewElements($items, $langcode);
+    foreach ($items as $delta => $item) {
+      $element[$delta]['#value'] = new JsonEncoded([
+        'service' => 'map',
+        'version' => '2.0',
+        'render' => TRUE,
+        'map' => [
+          'zoom' => $this->getSetting('zoom_level'),
+          'center' => [$item->get('lat')->getValue(), $item->get('lon')->getValue()],
+        ],
+      ]);
+    }
+    return $element;
+  }
+
+}
