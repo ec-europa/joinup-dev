beb52cb5c197e81025c5146d28554395cb920e1c
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-10-21 11:41:47 +0300

    ISAICP-5564: Adapt to the latest patch and comment that it is working.

--- web/modules/custom/joinup_event/src/Plugin/Field/FieldFormatter/WebtoolsMapFormatter.php
+++ web/modules/custom/joinup_event/src/Plugin/Field/FieldFormatter/WebtoolsMapFormatter.php
@@ -18,6 +18,8 @@ class WebtoolsMapFormatter extends OriginalWebtoolsMapFormatter {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = parent::viewElements($items, $langcode);
+    // The only difference with the parent method is that we add the `render`
+    // property to force the map to render.
     foreach ($items as $delta => $item) {
       $element[$delta]['#value'] = new JsonEncoded([
         'service' => 'map',
