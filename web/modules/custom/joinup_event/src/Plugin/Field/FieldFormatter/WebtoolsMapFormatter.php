453f9283ec1c56a5a7bf565e2041d494478021fc
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-11-05 15:47:32 +0200

    ISAICP-4998: Refactor our formatter to properly use the classes and the API.

--- web/modules/custom/joinup_event/src/Plugin/Field/FieldFormatter/WebtoolsMapFormatter.php
+++ web/modules/custom/joinup_event/src/Plugin/Field/FieldFormatter/WebtoolsMapFormatter.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace Drupal\joinup_event\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\oe_webtools_maps\Component\Render\JsonEncoded;
 use Drupal\oe_webtools_maps\Plugin\Field\FieldFormatter\WebtoolsMapFormatter as OriginalWebtoolsMapFormatter;
 
 /**
@@ -19,64 +18,12 @@ class WebtoolsMapFormatter extends OriginalWebtoolsMapFormatter {
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = parent::viewElements($items, $langcode);
 
-    // Set the render property to true and add a marker to the map.
-    foreach ($items as $delta => $item) {
-      $data_array = [
-        'service' => 'map',
-        'version' => '2.0',
-        'render' => TRUE,
-        'map' => [
-          'zoom' => $this->getSetting('zoom_level'),
-          'center' => [$item->get('lat')->getValue(), $item->get('lon')->getValue()],
-        ],
-      ];
+    /** @var \Drupal\oe_webtools_maps\Component\Render\JsonEncoded $json */
+    $json = $element[0]['#value'];
+    // The render property forces the map to appear on page load and not on
+    // scroll.
+    $json->setJson($json->getJson() + ['render' => TRUE]);
 
-      if (!empty($item->get('lat')->getValue()) && !empty($item->get('lon')->getValue())) {
-        $entity = $item->getEntity();
-        // Normally, this should always has a value since the coordinated derive
-        // from the field_location. However, to protect from a site break on
-        // possible future updates, we perform a check.
-        $description = $entity->hasField('field_location') && !empty($entity->field_location->value) ? $entity->field_location->value : '';
-
-        $data_array['layers'] = [
-          [
-            'markers' => [
-              'type' => 'FeatureCollection',
-              'features' => [
-                [
-                  'type' => 'Feature',
-                  'properties' => [
-                    'name' => $entity->label(),
-                    'description' => $description,
-                  ],
-                  'geometry' => [
-                    'type' => 'Point',
-                    'coordinates' => [
-                      // According to OE team, while every instance of longitude
-                      // (lon) and latitude (lat) values are lat first and lon
-                      // second, for the marker coordinates we need to set them
-                      // in a reverse way as the normal way is the US standard
-                      // and in our API it translates the values wrongly making
-                      // the marker appear in the middle of the full map
-                      // (apparently the default values apply).
-                      $item->get('lon')->getValue(),
-                      $item->get('lat')->getValue(),
-                    ],
-                  ],
-                ],
-              ],
-            ],
-          ],
-        ];
-      }
-
-      $element[$delta] = [
-        '#type' => 'html_tag',
-        '#tag' => 'script',
-        '#value' => new JsonEncoded($data_array),
-        '#attributes' => ['type' => 'application/json'],
-      ];
-    }
     return $element;
   }
 
