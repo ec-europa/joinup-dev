11c905ca945f43d250a5182370d7321686de20a3
Pieter Frenssen <pieter@frenssen.be>
2019-12-17 19:04:00 +0200

    ISAICP-5622: Adhere to coding standards.

--- web/themes/joinup/js/mdl-chips-input.js
+++ web/themes/joinup/js/mdl-chips-input.js
@@ -1,11 +1,15 @@
-(function() {
-    'use strict';
+/**
+ * @file
+ * Support for chips in input fields.
+ *
+ * Based on `dkarv/mdl-chip-input`.
+ *
+ * @see https://github.com/dkarv/mdl-chip-input
+ */
 
-    /**
-     * Support for chips in input fields.
-     *
-     * Based on https://github.com/dkarv/mdl-chip-input
-     */
+(function () {
+  'use strict';
+
     var MaterialChipInput = function MaterialChipInput(element) {
         this.element_ = element;
 
@@ -13,10 +17,10 @@
     };
     window['MaterialChipInput'] = MaterialChipInput;
 
-    MaterialChipInput.prototype.addChip_ = function(id, text) {
+    MaterialChipInput.prototype.addChip_ = function (id, text) {
         var currentChipIds = this.getChipIds();
         if (currentChipIds.indexOf(id) > -1) {
-            // ignore duplicates
+            // Ignore duplicates.
             return;
         }
         var chip = document.createElement('span');
@@ -26,7 +30,7 @@
             '<button type="button" class="mdl-chip__action">' +
             '<i class="material-icons">close</i></button>';
         var update = this.updateTargets_.bind(this);
-        chip.getElementsByClassName('mdl-chip__action')[0].onclick = function() {
+        chip.getElementsByClassName('mdl-chip__action')[0].onclick = function () {
             chip.remove();
             update();
         };
@@ -36,12 +40,12 @@
     /**
      * Returns the IDs of the currently active chips.
      */
-    MaterialChipInput.prototype.getChipIds = function() {
+    MaterialChipInput.prototype.getChipIds = function () {
         var currentChipIds = [];
         var children = this.element_.children;
-        for(var i = children.length; i--;) {
-            if(children[i].classList.contains('mdl-chip')) {
-                currentChipIds.unshift(children[i].children[0].getAttribute('data-id'))
+        for (var i = children.length; i--;) {
+            if (children[i].classList.contains('mdl-chip')) {
+                currentChipIds.unshift(children[i].children[0].getAttribute('data-id'));
             }
         }
         return currentChipIds;
@@ -53,7 +57,7 @@
      * This currently only supports deleting previously existing values if a
      * chip is deleted by the user.
      */
-    MaterialChipInput.prototype.updateTargets_ = function() {
+    MaterialChipInput.prototype.updateTargets_ = function () {
         var currentChipIds = this.getChipIds();
         var numTargets = this.targets_.length;
         var target;
@@ -62,53 +66,54 @@
             if (!(currentChipIds.indexOf(target.value) > -1)) {
                 target.remove();
             }
-        };
+        }
         if (currentChipIds.length >= this.options_.maximum) {
             this.input_.style.display = 'none';
-        } else {
+        }
+        else {
             this.input_.style.display = 'block';
         }
     };
 
-    MaterialChipInput.prototype.mouseDown_ = function(event) {
+    MaterialChipInput.prototype.mouseDown_ = function (event) {
         this.input_.focus();
     };
 
-    MaterialChipInput.prototype.keyDown_ = function(event) {
+    MaterialChipInput.prototype.keyDown_ = function (event) {
         var code = event.which || event.keyCode;
-        if(code === 8 && !this.input_.value) {
-            // remove last tag if input is empty
-            if(this.element_.children.length > 1) {
+        if (code === 8 && !this.input_.value) {
+            // Remove last tag if input is empty.
+            if (this.element_.children.length > 1) {
                 this.element_.children[this.element_.children.length - 2].remove();
                 this.updateTargets_();
             }
         }
     };
 
-    MaterialChipInput.prototype.init = function() {
-        if(this.element_) {
+    MaterialChipInput.prototype.init = function () {
+        if (this.element_) {
             this.inputs_ = this.element_.getElementsByClassName('inputs')[0];
 
             this.input_ = this.element_.getElementsByClassName('mdl-textfield__input')[0];
             this.input_.addEventListener('keydown', this.keyDown_.bind(this));
             this.targets_ = this.element_.getElementsByClassName('mdl-chipfield__input');
 
-            // let the whole element look like a input field
+            // Let the whole element look like a input field.
             this.element_.addEventListener('click', this.mouseDown_.bind(this));
 
-            // set the default options
+            // Set the default options.
             this.options_ = {
                 maximum: Number.MAX_VALUE,
                 separator: ','
             };
 
-            // initialize the chips
+            // Initialize the chips.
             var length = this.targets_.length;
             var target;
             for (var i = 0; i < length; i++) {
                 target = this.targets_[i];
                 this.addChip_(target.value, target.getAttribute('data-description'));
-            };
+            }
         }
-    }
+    };
 })();
