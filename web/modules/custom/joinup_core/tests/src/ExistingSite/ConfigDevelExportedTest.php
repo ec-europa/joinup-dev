57e2ece862be1342c47530e027fe24b610c18a39
Pieter Frenssen <pieter@frenssen.be>
2019-12-18 09:11:50 +0200

    ISAICP-5533: Make sure all exported config is correct and tested.

--- web/modules/custom/joinup_core/tests/src/ExistingSite/ConfigDevelExportedTest.php
+++ web/modules/custom/joinup_core/tests/src/ExistingSite/ConfigDevelExportedTest.php
@@ -4,10 +4,11 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\joinup_core\ExistingSite;
 
+use Drupal\Core\Extension\Extension;
 use Drush\TestTraits\DrushTestTrait;
 
 /**
- * Tests that the config of all enabled modules is listed in their info files.
+ * Tests that the config of our custom modules is listed in their info files.
  *
  * @group joinup_core
  */
@@ -32,10 +33,30 @@ class ConfigDevelExportedTest extends JoinupExistingSiteTestBase {
   }
 
   /**
-   * Tests drush commands.
+   * Tests that the list of exported config is correct.
    */
-  public function testCommands() {
-    $this->drush('config:devel-update-info', ['extension' => 'joinup_core'], ['check' => NULL]);
+  public function testConfigDevelExportedConfig(): void {
+    // Get a list of all enabled custom modules.
+    /** @var \Drupal\Core\Extension\Extension[] $modules */
+    $modules = array_filter($this->moduleHandler->getModuleList(), function (Extension $extension) {
+      return substr($extension->getPath(), 0, 14) === 'modules/custom';
+    });
+
+    $extension_names = array_map(function (Extension $extension) {
+      return $extension->getName();
+    }, $modules);
+
+    // Also check the profile and the theme.
+    $extension_names += ['joinup', 'joinup_theme'];
+
+    foreach ($extension_names as $extension_name) {
+      $this->drush('config:devel-update-info', ['extension' => $extension_name], ['check' => NULL]);
+    }
+
+    // The test will fail if the above Drush command throws an exception when a
+    // module has invalid exported config. We need to do one assertion though or
+    // PHPUnit will mark this test as risky.
+    $this->assertTrue(TRUE);
   }
 
 }
