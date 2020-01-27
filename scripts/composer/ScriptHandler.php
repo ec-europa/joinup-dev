7057e63e2ded2605140be5b5bd8043b2ad0c9eee
Pieter Frenssen <pieter@frenssen.be>
2020-01-27 17:35:38 +0200

    ISAICP-5722: We still need to create the required directories and settings file with the new Composer integration.

---
+++ scripts/composer/ScriptHandler.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * Contains \DrupalProject\composer\ScriptHandler.
+ */
+
+declare(strict_types = 1);
+
+namespace DrupalProject\composer;
+
+use Composer\Script\Event;
+use Drupal\Core\Site\Settings;
+use DrupalFinder\DrupalFinder;
+use Symfony\Component\Filesystem\Filesystem;
+use Webmozart\PathUtil\Path;
+
+class ScriptHandler {
+
+  public static function createRequiredFiles(Event $event) {
+    $fs = new Filesystem();
+    $drupalFinder = new DrupalFinder();
+    $drupalFinder->locateRoot(getcwd());
+    $drupalRoot = $drupalFinder->getDrupalRoot();
+
+    $dirs = [
+      'modules',
+      'profiles',
+      'themes',
+    ];
+
+    // Required for unit testing
+    foreach ($dirs as $dir) {
+      if (!$fs->exists($drupalRoot . '/'. $dir)) {
+        $fs->mkdir($drupalRoot . '/'. $dir);
+        $fs->touch($drupalRoot . '/'. $dir . '/.gitkeep');
+      }
+    }
+
+    // Prepare the settings file for installation
+    if (!$fs->exists($drupalRoot . '/sites/default/settings.php') and $fs->exists($drupalRoot . '/sites/default/default.settings.php')) {
+      $fs->copy($drupalRoot . '/sites/default/default.settings.php', $drupalRoot . '/sites/default/settings.php');
+      require_once $drupalRoot . '/core/includes/bootstrap.inc';
+      require_once $drupalRoot . '/core/includes/install.inc';
+
+      $config_sync_directory = Settings::get('config_sync_directory');
+      $settings['config_directories'] = [
+        $config_sync_directory => (object) [
+          'value' => Path::makeRelative($drupalFinder->getComposerRoot() . '/config/sync', $drupalRoot),
+          'required' => TRUE,
+        ],
+      ];
+      drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
+      $fs->chmod($drupalRoot . '/sites/default/settings.php', 0640);
+      $event->getIO()->write("Created a sites/default/settings.php file with chmod 0640");
+    }
+
+    // Create the files directory with chmod 0755
+    if (!$fs->exists($drupalRoot . '/sites/default/files')) {
+      $oldmask = umask(0);
+      $fs->mkdir($drupalRoot . '/sites/default/files', 0755);
+      umask($oldmask);
+      $event->getIO()->write("Created a sites/default/files directory with chmod 0755");
+    }
+  }
+
+}
