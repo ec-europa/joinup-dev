9e45cbcae2517ca5e1296fda17c30104e1a1cedc
Claudiu Cristea <clau.cristea@gmail.com>
2020-02-11 18:33:39 +0200

    ISAICP-5791: JoinupExistingSiteTestBase to enable test mail collector.

--- web/modules/custom/joinup_core/tests/src/ExistingSite/JoinupExistingSiteTestBase.php
+++ web/modules/custom/joinup_core/tests/src/ExistingSite/JoinupExistingSiteTestBase.php
@@ -5,33 +5,59 @@ declare(strict_types = 1);
 namespace Drupal\Tests\joinup_core\ExistingSite;
 
 use Drupal\joinup\Traits\AntibotTrait;
+use Drupal\joinup\Traits\MailConfigTrait;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
+use weitzman\DrupalTestTraits\Mail\MailCollectionTrait;
 
 /**
  * Base class for Joinup ExistingSite tests.
  */
-class JoinupExistingSiteTestBase extends ExistingSiteBase {
+abstract class JoinupExistingSiteTestBase extends ExistingSiteBase {
 
   use AntibotTrait;
+  use MailCollectionTrait {
+    startMailCollection as traitStartMailCollection;
+    restoreMailSettings as traitRestoreMailSettings;
+  }
+  use MailConfigTrait;
+
+  /**
+   * The list of Honeypot forms.
+   *
+   * @var bool[]
+   */
+  protected static $honeypotForms;
 
   /**
    * {@inheritdoc}
    */
   protected function setUp(): void {
     parent::setUp();
+
+    // Use the testing mail collector during tests.
+    $this->startMailCollection();
+
     // As ExistingSiteBase tests are running without javascript, we disable
-    // Antibot during the tests run.
-    static::disableAntibot();
+    // Antibot and Honeypot functionality during the tests run.
+    $this->disableAntibot();
+    $this->disableHoneypot();
   }
 
   /**
    * {@inheritdoc}
    */
   public function tearDown(): void {
+    // Restore Antibot and Honeypot functionality after test run.
+    $this->restoreAntibot();
+    $this->restoreHoneypot();
+
     // Make sure we don't send any notifications during test entities cleanup.
     foreach ($this->cleanupEntities as $entity) {
       $entity->skip_notification = TRUE;
     }
+
+    // Restore the mail settings.
+    $this->restoreMailSettings();
 
     parent::tearDown();
 
@@ -42,9 +68,60 @@ class JoinupExistingSiteTestBase extends ExistingSiteBase {
     // Delete the OG group content orphans now because parent::tearDown() is
     // destroying the container and the registered shutdown callback will fail.
     $delete_orphans_plugin->process();
+  }
 
-    // Restores the Antibot functionality.
-    static::restoreAntibot();
+  /**
+   * Overrides the trait method by bypassing config read-only.
+   *
+   * @throws \Exception
+   *   If mail config is overwritten in settings.php or settings.local.php.
+   */
+  protected function startMailCollection(): void {
+    // Check if the mail system configuration has been overridden in
+    // settings.php or settings.local.php.
+    $this->checkMailConfigOverride();
+
+    static::bypassReadOnlyConfig();
+    $this->traitStartMailCollection();
+    static::restoreReadOnlyConfig();
+
+    $this->resetTestMailCollector();
+  }
+
+  /**
+   * Overrides the trait method by bypassing config read-only.
+   */
+  protected function restoreMailSettings(): void {
+    static::bypassReadOnlyConfig();
+    $this->traitRestoreMailSettings();
+    static::restoreReadOnlyConfig();
+  }
+
+  /**
+   * Disables Honeypot settings during the test run.
+   */
+  protected function disableHoneypot(): void {
+    static::bypassReadOnlyConfig();
+    $config_factory = \Drupal::configFactory();
+    $config = $config_factory->getEditable('honeypot.settings');
+    if (!isset($this->honeypotForms)) {
+      static::$honeypotForms = $config->get('form_settings');
+    }
+    $config
+      ->set('form_settings', array_fill_keys(array_keys(static::$honeypotForms), FALSE))
+      ->save();
+    static::restoreReadOnlyConfig();
+  }
+
+  /**
+   * Restores Honeypot settings after test run.
+   */
+  protected function restoreHoneypot(): void {
+    static::bypassReadOnlyConfig();
+    \Drupal::configFactory()->getEditable('honeypot.settings')
+      ->set('form_settings', static::$honeypotForms)
+      ->save();
+    static::restoreReadOnlyConfig();
   }
 
 }
