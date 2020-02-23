a836408ab09e327a4526ac7855b4bcffe08bf7d4
Claudiu Cristea <clau.cristea@gmail.com>
2020-02-23 13:56:55 +0200

    ISAICP-5791: Use the active store mail configurations during config diffs test.

--- web/modules/custom/joinup_core/tests/src/ExistingSite/ConfigTest.php
+++ web/modules/custom/joinup_core/tests/src/ExistingSite/ConfigTest.php
@@ -21,6 +21,20 @@ class ConfigTest extends JoinupExistingSiteTestBase {
   protected $disableSpamProtection = FALSE;
 
   /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Mail configuration has been changed during tests run in order to prevent
+    // sending emails outside and catch messages for test assertion purpose. As
+    // this setup creates a difference between the active and the sync store, we
+    // restore the active store mail configuration for the purpose of this test.
+    // @see \Drupal\Tests\joinup_core\ExistingSite\JoinupExistingSiteTestBase::setUp()
+    $this->restoreMailSettings();
+  }
+
+  /**
    * Tests that the active and sync stores are the same.
    */
   public function testConfig(): void {
@@ -33,6 +47,15 @@ class ConfigTest extends JoinupExistingSiteTestBase {
 
     // Check that there are no differences between database and config sync.
     $this->assertEmpty($diff, 'Differences between active and sync stores for: ' . implode(', ', $diff));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown(): void {
+    // Re-enable mail collection.
+    $this->startMailCollection();
+    parent::tearDown();
   }
 
   /**
