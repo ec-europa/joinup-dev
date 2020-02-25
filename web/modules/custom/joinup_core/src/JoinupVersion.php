4b14868a901d6fb61f4ca25441ed26295f54a287
Pieter Frenssen <pieter@frenssen.be>
2020-02-25 18:01:07 +0200

    ISAICP-5684: Started on the JoinupVersion service.

---
+++ web/modules/custom/joinup_core/src/JoinupVersion.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_core;
+
+/**
+ * Retrieves the current Joinup version.
+ *
+ * The version is saved during composer install in a `VERSION` file in the root
+ * folder of the project.
+ */
+class JoinupVersion {
+
+}
