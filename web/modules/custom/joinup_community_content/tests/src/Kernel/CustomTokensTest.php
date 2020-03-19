2d93b8d37e16c5567e6dd8793fd8484207a93342
Pieter Frenssen <pieter@frenssen.be>
2020-03-19 18:03:42 +0200

    ISAICP-5692: Update tests that fail due to services shifting around.

--- web/modules/custom/joinup_community_content/tests/src/Kernel/CustomTokensTest.php
+++ web/modules/custom/joinup_community_content/tests/src/Kernel/CustomTokensTest.php
@@ -24,6 +24,7 @@ class CustomTokensTest extends TokenReplaceKernelTestBase {
     'comment',
     'diff',
     'joinup_community_content',
+    'joinup_group',
     'message_notify',
     'node',
     'og',
