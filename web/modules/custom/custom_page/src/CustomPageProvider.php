c1c9e1cfe6cf1e01b4f264a579244b6e66ce4012
Pieter Frenssen <pieter@frenssen.be>
2020-03-27 14:36:46 +0200

    ISAICP-5708: Try to retrieve the custom pages from the menu links.

--- web/modules/custom/custom_page/src/CustomPageProvider.php
+++ web/modules/custom/custom_page/src/CustomPageProvider.php
@@ -4,10 +4,19 @@ declare(strict_types = 1);
 
 namespace Drupal\custom_page;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
 /**
  * Provides custom pages to whoever desires them.
  */
 class CustomPageProvider implements CustomPageProviderInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
 
   /**
    * The custom page menu links manager.
@@ -19,10 +28,13 @@ class CustomPageProvider implements CustomPageProviderInterface {
   /**
    * Builds a new custom page OG menu links updater service.
    *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
    * @param \Drupal\custom_page\CustomPageOgMenuLinksManagerInterface $menuLinkManager
    *   The custom page menu link manager.
    */
-  public function __construct(CustomPageOgMenuLinksManagerInterface $menuLinkManager) {
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, CustomPageOgMenuLinksManagerInterface $menuLinkManager) {
+    $this->entityTypeManager = $entityTypeManager;
     $this->menuLinkManager = $menuLinkManager;
   }
 
@@ -40,7 +52,18 @@ class CustomPageProvider implements CustomPageProviderInterface {
    */
   public function getCustomPagesByGroupId(string $group_id, bool $include_disabled = FALSE): array {
     $menu_instance = $this->menuLinkManager->getOgMenuInstanceByGroupId($group_id);
-    // @todo Implement.
+    if (!empty($menu_instance)) {
+      $properties = [
+        'bundle' => 'menu_link_content',
+        'menu_name' => "ogmenu-{$menu_instance->id()}",
+      ];
+      if (!$include_disabled) {
+        $properties['enabled'] = 1;
+      }
+
+      $storage = $this->entityTypeManager->getStorage('menu_link_content');
+      $entities = $storage->loadByProperties($properties);
+    }
     return [];
   }
 
