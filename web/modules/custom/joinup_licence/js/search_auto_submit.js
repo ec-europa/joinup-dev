932b449333fa78d44d60a97858a6523e7ceb7132
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-04-08 19:10:31 +0300

    ISACIP-5586: Do not run uneeded filtering tests.

--- web/modules/custom/joinup_licence/js/search_auto_submit.js
+++ web/modules/custom/joinup_licence/js/search_auto_submit.js
@@ -17,15 +17,7 @@
     attach: function (context) {
       $('.auto_submit', context).on('change', function (event) {
         var $value = $(event.target).val();
-
-        // In order to ensure that the original values are maintained or at
-        // least, that no malformed string is passed, check if the value
-        // selected matches the same pattern as the requirements of the route.
-        // Note: The + symbol is maintained here compared to the route
-        // requirements where it is assumed that it has been converted to a
-        // space character.
-        var pattern = /^[a-zA-Z0-9][a-zA-Z0-9.\+-]+$/;
-        if (pattern.test($value)) {
+        if ($value !== '') {
           var new_pathname = document.location.pathname;
           new_pathname += ';' + $value;
           document.location.href = document.location.href.replace(document.location.pathname, new_pathname);
