466e6bf21e0a5ed5471dfec917c29057bfc44dae
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-04-11 19:07:18 +0300

    ISAICP-5690: Provide the constraint settings for the short id field.

---
+++ web/modules/custom/joinup_group/src/Plugin/Validation/Constraint/UniqueShortIdInsensitiveConstraint.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_group\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Checks if an entity field has a unique value within its bundle.
+ *
+ * The check is case insensitive. Works only for RDF entities.
+ *
+ * @Constraint(
+ *   id = "UniqueShortIdInsensitive",
+ *   label = @Translation("Unique case insensitive field within a bundle constraint.", context = "Validation"),
+ * )
+ */
+class UniqueShortIdInsensitiveConstraint extends Constraint {
+
+  /**
+   * The message to show when validation fails.
+   *
+   * @var string
+   */
+  public $message = 'Content with @field_name %value already exists. Please choose a different @field_name.';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validatedBy() {
+    return '\Drupal\joinup_group\Plugin\Validation\Constraint\UniqueShortIdInsensitiveValidator';
+  }
+
+}
---
+++ web/modules/custom/joinup_group/src/Plugin/Validation/Constraint/UniqueShortIdInsensitiveValidator.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_group\Plugin\Validation\Constraint;
+
+use Drupal\Core\Database\Database;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Validates that a field is unique for the given entity type within a bundle.
+ */
+class UniqueShortIdInsensitiveValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($items, Constraint $constraint) {
+    /** @var \Drupal\rdf_entity\RdfInterface $entity */
+    $entity = $items->getEntity();
+    if ($entity->getEntityTypeId() !== 'rdf_entity') {
+      throw new \RuntimeException('UniqueShortidInsensitiveConstraint can only be applied to collections and solutions because the SQL like condition is not case insensitive.');
+    }
+
+    // Short ID has cardinality 1. Does not need to iterate over other deltas.
+    if (!$item = $items->first()) {
+      return;
+    }
+
+    $bundle = $entity->bundle();
+    $data = [
+      'collection' => [
+        'from' => 'FROM <http://joinup.eu/collection/published> FROM <http://joinup.eu/collection/draft>',
+        'type' => '<http://www.w3.org/ns/dcat#Catalog>',
+      ],
+      'solution' => [
+        'from' => 'FROM <http://joinup.eu/solution/published> FROM <http://joinup.eu/solution/draft>',
+        'type' => '<http://www.w3.org/ns/dcat#Dataset>',
+      ],
+    ];
+
+    $item_value = Database::getConnection()->escapeLike($item->value);
+    $query = <<<QUERY
+SELECT DISTINCT(?entity_id)
+{$data[$bundle]['from']}
+WHERE {
+  ?entity_id a {$data[$bundle]['type']} .
+  ?entity_id <http://purl.org/dc/terms/alternative> ?value .
+  FILTER (lcase(?value) = lcase("{$item_value}")) .
+}
+QUERY;
+    $count = \Drupal::getContainer()->get('sparql.endpoint')->query($query)->count();
+
+    if ($count) {
+      $this->context->addViolation($constraint->message, [
+        '%value' => $item->value,
+        '@entity_type' => $entity->getEntityType()->getSingularLabel(),
+        '@field_name' => mb_strtolower($items->getFieldDefinition()->getLabel()),
+      ]);
+    }
+  }
+
+}
