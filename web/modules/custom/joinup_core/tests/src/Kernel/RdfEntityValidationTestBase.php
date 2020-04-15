26c45c02fc866ee363438dcb8be37d14dbc86d65
Pieter Frenssen <pieter@frenssen.be>
2020-04-15 09:49:02 +0300

    ISAICP-5694: Adhere to coding standards.

--- web/modules/custom/joinup_core/tests/src/Kernel/RdfEntityValidationTestBase.php
+++ web/modules/custom/joinup_core/tests/src/Kernel/RdfEntityValidationTestBase.php
@@ -4,8 +4,8 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\joinup_core\Kernel;
 
-use Drupal\rdf_entity\Entity\Rdf;
 use Drupal\Tests\joinup_test\Kernel\JoinupKernelTestBase;
+use Drupal\rdf_entity\Entity\Rdf;
 
 /**
  * Base class for tests that verify the validation of rdf entities.
