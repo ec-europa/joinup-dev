bc164d5319926f9db99409c78b143f6133ed3d45
Pieter Frenssen <pieter@frenssen.be>
2020-04-16 12:52:20 +0300

    ISAICP-5694: Unbreak SyncFieldsFromParentSolutionTest.

--- web/modules/custom/asset_release/tests/src/Kernel/SyncFieldsFromParentSolutionTest.php
+++ web/modules/custom/asset_release/tests/src/Kernel/SyncFieldsFromParentSolutionTest.php
@@ -42,6 +42,7 @@ class SyncFieldsFromParentSolutionTest extends KernelTestBase {
     'image',
     'inline_entity_form',
     'joinup_core',
+    'joinup_rdf',
     'joinup_workflow',
     'link',
     'matomo_reporting_api',
@@ -62,6 +63,7 @@ class SyncFieldsFromParentSolutionTest extends KernelTestBase {
     'state_machine',
     'system',
     'taxonomy',
+    'template_suggestion',
     'text',
     'tour',
     'user',
@@ -82,6 +84,7 @@ class SyncFieldsFromParentSolutionTest extends KernelTestBase {
     $this->installEntitySchema('rdf_entity');
     $this->installConfig([
       'joinup_core',
+      'joinup_rdf',
       'rdf_draft',
       'rdf_entity',
       'solution',
