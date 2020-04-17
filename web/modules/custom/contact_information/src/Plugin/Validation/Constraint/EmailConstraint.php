1d73b1a41e6c185d85ad6876833105d7186f131d
Pieter Frenssen <pieter@frenssen.be>
2020-04-17 11:31:40 +0300

    ISAICP-5694: Research history of mysterious custom email validator and move it to the right module.

--- web/modules/custom/contact_information/src/Plugin/Validation/Constraint/EmailConstraint.php
+++ web/modules/custom/contact_information/src/Plugin/Validation/Constraint/EmailConstraint.php
@@ -2,12 +2,17 @@
 
 declare(strict_types = 1);
 
-namespace Drupal\joinup_core\Plugin\Validation\Constraint;
+namespace Drupal\contact_information\Plugin\Validation\Constraint;
 
 use Symfony\Component\Validator\Constraint;
 
 /**
  * Checks if an email is valid according Joinup specifications.
+ *
+ * Do not use this for anything other than contact information emails. For all
+ * other emails use the standard EmailValidator.
+ *
+ * @see \Drupal\Component\Utility\EmailValidator
  *
  * @Constraint(
  *   id = "JoinupEmail",
--- web/modules/custom/contact_information/src/Plugin/Validation/Constraint/EmailConstraintValidator.php
+++ web/modules/custom/contact_information/src/Plugin/Validation/Constraint/EmailConstraintValidator.php
@@ -2,13 +2,18 @@
 
 declare(strict_types = 1);
 
-namespace Drupal\joinup_core\Plugin\Validation\Constraint;
+namespace Drupal\contact_information\Plugin\Validation\Constraint;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 /**
- * Validates an e-mail according Joinup specifications.
+ * Validates email using a Joinup business analyst approved regular expression.
+ *
+ * Do not use this for anything other than contact information emails. For all
+ * other emails use the standard EmailValidator.
+ *
+ * @see \Drupal\Component\Utility\EmailValidator
  */
 class EmailConstraintValidator extends ConstraintValidator {
 
@@ -20,6 +25,8 @@ class EmailConstraintValidator extends ConstraintValidator {
       return;
     }
 
+    // The regular expression seems to originate from this online resource.
+    // @see https://www.regular-expressions.info/email.html
     if (!preg_match('/^[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i', $item->value)) {
       $this->context->addViolation($constraint->message, ['%email' => $item->value]);
     }
