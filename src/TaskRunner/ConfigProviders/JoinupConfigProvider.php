4bfff11f0f4d4b4e31d14107a8326aa3fc2e11d2
Claudiu Cristea <clau.cristea@gmail.com>
2020-04-25 14:32:00 +0300

    ISAICP-5825: Provide Joinup specific configuration.

---
+++ src/TaskRunner/ConfigProviders/JoinupConfigProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Joinup\TaskRunner\ConfigProviders;
+
+use DrupalFinder\DrupalFinder;
+use OpenEuropa\TaskRunner\Contract\ConfigProviderInterface;
+use OpenEuropa\TaskRunner\Traits\ConfigFromFilesTrait;
+use Robo\Config\Config;
+use Robo\Exception\AbortTasksException;
+
+/**
+ * Runs configs from the ./runner dir between runner.yml.dist and runner.yml.
+ */
+class JoinupConfigProvider implements ConfigProviderInterface {
+
+  use ConfigFromFilesTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function provide(Config $config): void {
+    // Get the Joinup project full-qualified directory.
+    $drupalFinder = new DrupalFinder();
+    if (!$drupalFinder->locateRoot(getcwd())) {
+      throw new AbortTasksException('Cannot locate Drupal path.');
+    }
+    $config->set('joinup.dir', $drupalFinder->getComposerRoot());
+
+    // Import configurations from ./runner/config/.
+    static::importFromFiles($config, glob('runner/*.yml'));
+  }
+
+}
