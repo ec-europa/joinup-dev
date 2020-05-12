0805b4c2096d48e2ac4c7034ac5d75ef924196d5
Claudiu Cristea <clau.cristea@gmail.com>
2020-05-12 17:51:54 +0300

    ISAICP-5896: Mention also 'custom pages'.

--- web/modules/custom/collection/src/Entity/CollectionContentInterface.php
+++ web/modules/custom/collection/src/Entity/CollectionContentInterface.php
@@ -7,7 +7,7 @@ namespace Drupal\collection\Entity;
 /**
  * Interface for entities that are collection content.
  *
- * This comprises community content entities and solutions.
+ * This comprises community content, custom pages, and solutions.
  */
 interface CollectionContentInterface {
 
