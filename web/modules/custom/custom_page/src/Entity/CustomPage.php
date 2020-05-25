7804fd831ef6740c7956758913203519aff78df0
Claudiu Cristea <clau.cristea@gmail.com>
2020-05-25 20:21:29 +0300

    ISAICP-5979: Create a bundle class for custom pages.

---
+++ web/modules/custom/custom_page/src/Entity/CustomPage.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\custom_page\Entity;
+
+use Drupal\collection\Entity\NodeCollectionContentTrait;
+use Drupal\joinup_group\Entity\NodeGroupContentTrait;
+use Drupal\node\Entity\Node;
+
+/**
+ * Entity class for custom page node entities.
+ */
+class CustomPage extends Node implements CustomPageInterface {
+
+  use NodeCollectionContentTrait;
+  use NodeGroupContentTrait;
+
+}
