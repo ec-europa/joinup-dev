c3f60ec9881e35dccbee6d484ee531b34d2312e8
Pieter Frenssen <pieter@frenssen.be>
2020-05-27 19:44:25 +0300

    ISAICP-5997: Adhere to coding standards.

--- src/Composer/load.environment.php
+++ src/Composer/load.environment.php
@@ -1,17 +1,19 @@
 <?php
 
 /**
- * This file is included very early. See autoload.files in composer.json and
- * https://getcomposer.org/doc/04-schema.md#files
+ * @file
+ * Loads environment specific configuration.
+ *
+ * This file is included very early. See autoload.files in composer.json.
+ * @see https://getcomposer.org/doc/04-schema.md#files
  */
 
+declare(strict_types = 1);
+
 use Dotenv\Dotenv;
-use Dotenv\Exception\InvalidPathException;
 
 // Load the .env.dist file in the project root, and overridden settings in .env
 // if it exists.
 $path = dirname(__DIR__, 2);
-$dotenv = Dotenv::createImmutable($path, ['.env.dist', '.env'], false);
+$dotenv = Dotenv::createImmutable($path, ['.env.dist', '.env'], FALSE);
 $dotenv->safeLoad();
-
-var_dump(getenv());
