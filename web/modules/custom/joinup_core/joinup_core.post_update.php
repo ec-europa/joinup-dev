229555521c1735eb9f19f4802eb8b880485da2c8
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-06-09 10:47:42 +0300

    ISAICP-6009: Provide an update to install the vocabulary in an existing site.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -7,7 +7,10 @@
 
 declare(strict_types = 1);
 
+use Drupal\Core\Database\Database;
 use Drupal\redirect\Entity\Redirect;
+use EasyRdf\Graph;
+use EasyRdf\GraphStore;
 
 /**
  * Update solution, release, distribution and community content aliases.
@@ -84,4 +87,19 @@ QUERY;
   $current_result = $results->current();
   $current_result = reset($current_result);
   return $current_result->getValue();
+}
+
+/**
+ * Insert the new EIF vocabulary into the database.
+ */
+function joinup_core_post_update_0106102(): void {
+  $sparql_connection = Database::getConnection('default', 'sparql_default');
+  $connection_options = $sparql_connection->getConnectionOptions();
+  $connect_string = "http://{$connection_options['host']}:{$connection_options['port']}/sparql-graph-crud";
+  $graph_store = new GraphStore($connect_string);
+
+  $filepath = __DIR__ . '/../../../../resources/fixtures/eif_voc.rdf';
+  $graph = new Graph('http://eif_voc');
+  $graph->parse(file_get_contents($filepath));
+  $graph_store->insert($graph);
 }
