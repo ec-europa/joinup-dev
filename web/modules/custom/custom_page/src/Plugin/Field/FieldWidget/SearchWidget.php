a004de4dbf9f3fb4f4dcb7af8640c5d993c63d42
Pieter Frenssen <pieter@frenssen.be>
2020-06-23 19:27:02 +0300

    ISAICP-5878: Fix 'Undefined index: global_search' notice.

--- web/modules/custom/custom_page/src/Plugin/Field/FieldWidget/SearchWidget.php
+++ web/modules/custom/custom_page/src/Plugin/Field/FieldWidget/SearchWidget.php
@@ -112,7 +112,7 @@ class SearchWidget extends DefaultSearchWidget {
       '#type' => 'checkbox',
       '#title' => $this->t('Global search'),
       '#description' => $this->t('If checked, the search will not be limited into the group content.'),
-      '#default_value' => $default_values['global_search'],
+      '#default_value' => $default_values['global_search'] ?? FALSE,
       '#weight' => -11,
     ];
 
