028f0092038ae9decf841886175a07c679692b27
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-06-25 10:43:55 +0300

    ISAICP-6012: Provide a post update to create a menu item in the IEF toolbox solution.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -8,6 +8,8 @@
 declare(strict_types = 1);
 
 use Drupal\Core\Database\Database;
+use Drupal\Core\Url;
+use Drupal\menu_link_content\Entity\MenuLinkContent;
 use EasyRdf\Graph;
 use EasyRdf\GraphStore;
 
@@ -24,4 +26,19 @@ function joinup_core_post_update_0106200(): void {
   $graph = new Graph('http://eif_voc');
   $graph->parse(file_get_contents($filepath));
   $graph_store->insert($graph);
+}
+
+/**
+ * Create the references page in the EIF Toolbox menu.
+ */
+function joinup_core_post_update_0106201(&$sandbox) {
+  $menu_name = 'ogmenu-3444';
+  $internal_path = Url::fromRoute('eif.recommendations')->toUriString();
+  $link = MenuLinkContent::create([
+    'title' => t('Recommendations'),
+    'menu_name' => $menu_name,
+    'link' => ['uri' => $internal_path],
+    'weight' => 4,
+  ]);
+  $link->save();
 }
