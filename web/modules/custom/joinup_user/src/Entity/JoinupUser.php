2e1044a1cc0988fc70a686651f8db4dfaeaf1894
Pieter Frenssen <pieter@frenssen.be>
2020-07-01 21:55:10 +0300

    ISAICP-5798: Ensure that the user ID is returned with the correct type.

--- web/modules/custom/joinup_user/src/Entity/JoinupUser.php
+++ web/modules/custom/joinup_user/src/Entity/JoinupUser.php
@@ -51,4 +51,13 @@ class JoinupUser extends User implements JoinupUserInterface {
     return $this->get('status')->value == -1;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function id(): int {
+    // According to `AccountInterface::id()` this should return an integer, but
+    // we get a string back from the database. Enforce the right type.
+    return (int) parent::id();
+  }
+
 }
