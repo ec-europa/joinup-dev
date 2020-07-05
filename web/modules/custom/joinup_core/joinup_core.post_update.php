d02268f0c5029584adb14f2f246f454d05d57d1d
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-07-05 12:57:54 +0300

    ISAICP-5745: Provide an update that will fix the changed time of all node revisions.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -35,3 +35,25 @@ function joinup_core_post_update_0106200(&$sandbox) {
   $sparql_connection->query('WITH <http://eira_skos> INSERT INTO <http://eira_skos> { ?subject skos:topConceptOf <http://data.europa.eu/dr8> } WHERE { ?subject a skos:Concept .};');
   $sparql_connection->query('WITH <http://eira_skos> INSERT { ?member skos:broaderTransitive ?collection } WHERE { ?collection a skos:Collection . ?collection skos:member ?member };');
 }
+
+/**
+ * Fix the last updated time of node entities.
+ */
+function joinup_core_post_update_0106201(&$sandbox) {
+  // In Joinup, all node updates through the UI always create a new revision.
+  // Only updates through the API can update an entity without creating a new
+  // revision. However, after moving the visit_count outside the storage, there
+  // is no other functionality that can perform such a task.
+  // Thus, it is safe to assume, that the "changed" property of each revision is
+  // the same as the "revision_timestamp". The following query will fix all
+  // cases where the entity was updated by an automatic procedure that wasn't
+  // actually touching any values.
+  $query = <<<QUERY
+UPDATE {node_field_revision} nfr
+INNER JOIN {node_revision} nr ON nfr.vid = nr.vid
+SET nfr.changed = nr.revision_timestamp
+WHERE nfr.changed != nr.revision_timestamp
+QUERY;
+
+  \Drupal::database()->query($query);
+}
