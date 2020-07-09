216985196ba2627dc64aff55479d083062a61ece
Pieter Frenssen <pieter@frenssen.be>
2020-07-09 12:04:20 +0300

    ISAICP-6106: Mark methods which should now be handled by bundle classes as deprecated.

--- web/modules/custom/joinup_group/src/JoinupGroupHelper.php
+++ web/modules/custom/joinup_group/src/JoinupGroupHelper.php
@@ -16,6 +16,10 @@ class JoinupGroupHelper {
 
   /**
    * Group bundles.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Use `$entity instanceof
+   *   {SolutionInterface|CollectionInterface}` instead.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   const GROUP_BUNDLES = [
     'collection' => 'collection',
@@ -24,6 +28,10 @@ class JoinupGroupHelper {
 
   /**
    * Content creation field machine names per group bundle.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Instead we should provide a
+   *   method such as `GroupInterface::getGroupContentCreationSetting()`.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   const GROUP_CONTENT_CREATION = [
     'collection' => 'field_ar_content_creation',
@@ -32,6 +40,10 @@ class JoinupGroupHelper {
 
   /**
    * Content moderation field machine names per group bundle.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Instead we should provide a
+   *   method such as `GroupInterface::getGroupModerationSetting()`.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   const GROUP_MODERATION_FIELDS = [
     'collection' => 'field_ar_moderation',
@@ -55,6 +67,10 @@ class JoinupGroupHelper {
    * @return bool
    *   True if the entity is an rdf of bundle collection or solution, false
    *   otherwise.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Use `$entity instanceof
+   *   GroupInterface` instead.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   public static function isGroup(EntityInterface $entity): bool {
     return $entity instanceof RdfInterface && isset(self::GROUP_BUNDLES[$entity->bundle()]);
@@ -68,6 +84,10 @@ class JoinupGroupHelper {
    *
    * @return bool
    *   True if the entity is an rdf of bundle collection, false otherwise.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Use `$entity instanceof
+   *   CollectionInterface` instead.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   public static function isCollection(EntityInterface $entity): bool {
     return self::isRdfEntityOfBundle($entity, 'collection');
@@ -81,6 +101,10 @@ class JoinupGroupHelper {
    *
    * @return bool
    *   True if the entity is an rdf of bundle solution, false otherwise.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Use `$entity instanceof
+   *   SolutionInterface` instead.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   public static function isSolution(EntityInterface $entity): bool {
     return self::isRdfEntityOfBundle($entity, 'solution');
@@ -98,6 +122,10 @@ class JoinupGroupHelper {
    *
    * @return \Drupal\Core\Entity\EntityInterface|null
    *   The group entity, or NULL if the entity doesn't have a group.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Use
+   *   `GroupContentInterface::getGroup()` instead.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   public static function getGroup(EntityInterface $entity): ?EntityInterface {
     if ($entity instanceof CommentInterface) {
@@ -115,6 +143,13 @@ class JoinupGroupHelper {
    *
    * @return string
    *   The field name.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. This method should not be used
+   *   since calling code should not be concerned with implementation details
+   *   such as field names. we should instead have methods on a GroupInterface
+   *   class that provide the actually needed functionality while abstracting
+   *   away the need for retrieving field names.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   public static function getGroupField(EntityInterface $entity): string {
     // Asset releases use the ADMS-AP dictated name for the group field, while
@@ -132,6 +167,10 @@ class JoinupGroupHelper {
    *   The content moderation status. Can be one of the following values:
    *   - CommunityContentWorkflowAccessControlHandler::PRE_MODERATION
    *   - CommunityContentWorkflowAccessControlHandler::POST_MODERATION
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Instead we should provide a
+   *   method such as `GroupInterface::getGroupModerationSetting()`.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   public static function getModeration(EntityInterface $entity): int {
     return (int) $entity->{self::GROUP_MODERATION_FIELDS[$entity->bundle()]}->first()->value;
@@ -148,6 +187,10 @@ class JoinupGroupHelper {
    *   - \Drupal\joinup_group\ContentCreationOptions::FACILITATORS
    *   - \Drupal\joinup_group\ContentCreationOptions::MEMBERS
    *   - \Drupal\joinup_group\ContentCreationOptions::REGISTERED_USERS
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Instead we should provide a
+   *   method such as `GroupInterface::getContentCreationSetting()`.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   public static function getContentCreation(EntityInterface $entity): string {
     return $entity->{self::GROUP_CONTENT_CREATION[$entity->bundle()]}->first()->value;
@@ -161,6 +204,10 @@ class JoinupGroupHelper {
    *
    * @return string
    *   The workflow state.
+   *
+   * @deprecated in 1.62 and is removed from 2.0. Use
+   *   `EntityWorkflowStateInterface::getWorkflowState()` instead.
+   * @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6105
    */
   public static function getState(EntityInterface $entity): string {
     return $entity->{self::GROUP_STATE_FIELDS[$entity->bundle()]}->first()->value;
