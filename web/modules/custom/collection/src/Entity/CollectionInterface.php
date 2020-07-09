6dea37152d74b5bdf5f2323fcbe5245cb460fe2d
Pieter Frenssen <pieter@frenssen.be>
2020-07-09 12:57:38 +0300

    ISAICP-6107: Harden GroupContentInterface::getGroup() against the case where a deleted group causes a NULL value to be returned.

--- web/modules/custom/collection/src/Entity/CollectionInterface.php
+++ web/modules/custom/collection/src/Entity/CollectionInterface.php
@@ -5,13 +5,14 @@ declare(strict_types = 1);
 namespace Drupal\collection\Entity;
 
 use Drupal\joinup_bundle_class\ShortIdInterface;
+use Drupal\joinup_group\Entity\GroupInterface;
 use Drupal\joinup_workflow\EntityWorkflowStateInterface;
 use Drupal\rdf_entity\RdfInterface;
 
 /**
  * Interface for collection entities in Joinup.
  */
-interface CollectionInterface extends RdfInterface, ShortIdInterface, EntityWorkflowStateInterface {
+interface CollectionInterface extends RdfInterface, GroupInterface, EntityWorkflowStateInterface, ShortIdInterface {
 
   /**
    * Returns the solutions that are affiliated with this collection.
---
+++ web/modules/custom/joinup_group/src/Entity/GroupInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_group\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Interface for entities that are groups.
+ *
+ * This comprises collections and solutions.
+ */
+interface GroupInterface extends ContentEntityInterface {
+
+}
