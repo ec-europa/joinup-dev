a0648a6ee441b90288b04a97981da7cdd234b1da
Claudiu Cristea <clau.cristea@gmail.com>
2020-08-10 18:09:14 +0200

    ISAICP-6123: Create a Solution link in the EIF Toolbox left menu.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -6,3 +6,24 @@
  */
 
 declare(strict_types = 1);
+
+use Drupal\Core\Url;
+use Drupal\eif\Eif;
+use Drupal\menu_link_content\Entity\MenuLinkContent;
+use Drupal\sparql_entity_storage\UriEncoder;
+
+/**
+ * Create the solutions page menu link in EIF Toolbox.
+ */
+function joinup_core_post_update_0106400(array &$sandbox): void {
+  MenuLinkContent::create([
+    'title' => t('Solutions'),
+    'menu_name' => 'ogmenu-3444',
+    'link' => [
+      'uri' => Url::fromRoute('view.eif_solutions.page', [
+        'rdf_entity' => UriEncoder::encodeUrl(Eif::EIF_ID),
+      ])->toUriString(),
+    ],
+    'weight' => 5,
+  ])->save();
+}
