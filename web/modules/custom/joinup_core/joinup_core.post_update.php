2f527094cfd4ba40b72c15b2446c2acad02131dd
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-08-11 20:34:11 +0300

    ISAICP-6121: Re-index the vocabilaries.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -6,3 +6,29 @@
  */
 
 declare(strict_types = 1);
+
+use Drupal\search_api\Plugin\search_api\datasource\ContentEntity;
+
+/**
+ * Re-index the new EIF vocabularies to apply new changes.
+ */
+function joinup_core_post_update_0106400(): void {
+  $vids = [
+    'eif_conceptual_model',
+    'eif_interoperability_layer',
+    'eif_principle',
+    'eif_recommendation',
+  ];
+
+  $entity_type_manager = \Drupal::entityTypeManager();
+  $storage = $entity_type_manager->getStorage('taxonomy_term');
+  $tids = $storage->getQuery()->condition('vid', $vids, 'IN')->execute();
+  /** @var \Drupal\taxonomy\TermInterface $term */
+  foreach ($storage->loadMultiple($tids) as $term) {
+    ContentEntity::indexEntity($term);
+  }
+  /** @var \Drupal\search_api\IndexInterface $index */
+  $index = $entity_type_manager->getStorage('search_api_index')->load('published');
+  $index->reindex();
+  $index->indexItems(-1, 'entity:taxonomy_term');
+}
