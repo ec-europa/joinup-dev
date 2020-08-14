ee75d1acfe91b62a54b429557ad81d958035fa64
Claudiu Cristea <clau.cristea@gmail.com>
2020-08-14 13:59:36 +0200

    ISAICP-6123: The custom page already adds its menu link.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -6,24 +6,3 @@
  */
 
 declare(strict_types = 1);
-
-use Drupal\Core\Url;
-use Drupal\eif\EifInterface;
-use Drupal\menu_link_content\Entity\MenuLinkContent;
-use Drupal\sparql_entity_storage\UriEncoder;
-
-/**
- * Create the solutions page menu link in EIF Toolbox.
- */
-function joinup_core_post_update_0106400(array &$sandbox): void {
-  MenuLinkContent::create([
-    'title' => t('Solutions'),
-    'menu_name' => 'ogmenu-3444',
-    'link' => [
-      'uri' => Url::fromRoute('view.eif_solutions.page', [
-        'rdf_entity' => UriEncoder::encodeUrl(EifInterface::EIF_ID),
-      ])->toUriString(),
-    ],
-    'weight' => 5,
-  ])->save();
-}
