8b0cb6f59f2166c581d276920c34921c203f1e4a
Pieter Frenssen <pieter@frenssen.be>
2020-08-15 15:50:24 +0300

    ISAICP-5798: Remove micro-optimization in favour of using the OG API.
    It is the responsibility of OG to return the memberships in a performant way and
    maintain a static cache for it.
    
    Ref. https://github.com/Gizra/og/issues/696

--- web/modules/custom/joinup_group/src/Entity/GroupTrait.php
+++ web/modules/custom/joinup_group/src/Entity/GroupTrait.php
@@ -13,39 +13,14 @@ use Drupal\og\OgMembershipInterface;
 trait GroupTrait {
 
   /**
-   * Static cache of memberships for this group, keyed by user ID.
-   *
-   * @var \Drupal\og\OgMembershipInterface[]|NULL[]
-   */
-  protected $memberships;
-
-  /**
    * {@inheritdoc}
    */
   public function getMembership(int $uid): ?OgMembershipInterface {
     assert(is_subclass_of($this, ContentEntityBase::class), __TRAIT__ . ' is intended to be used in bundle classes for content entities.');
-    if (!isset($this->memberships[$uid])) {
-      $og_membership_storage = $this->entityTypeManager()->getStorage('og_membership');
 
-      $query = $og_membership_storage
-        ->getQuery()
-        ->condition('uid', $uid)
-        ->condition('entity_type', $this->getEntityTypeId())
-        ->condition('entity_id', $this->id());
-
-      $membership_ids = $query->execute();
-      if (empty($membership_ids)) {
-        return NULL;
-      }
-      $membership_id = reset($membership_ids);
-
-      /** @var \Drupal\og\OgMembershipInterface|NULL $membership */
-      $membership = $og_membership_storage->load($membership_id);
-
-      $this->memberships[$uid] = $membership;
-    }
-
-    return $this->memberships[$uid];
+    /** @var \Drupal\og\MembershipManagerInterface $membership_manager */
+    $membership_manager = \Drupal::service('og.membership_manager');
+    return $membership_manager->getMembership($this, $uid, [OgMembershipInterface::STATE_ACTIVE]);
   }
 
 }
