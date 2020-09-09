96d1bc5d21dd225e4c0b5260e7769067d25ca503
Claudiu Cristea <clau.cristea@gmail.com>
2020-09-09 10:52:08 +0300

    ISAICP-6152: Create a bundle class for 'rdf_grap' RDF entities.

---
+++ web/modules/custom/joinup_rdf_graph/src/Entity/RdfGraph.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_rdf_graph\Entity;
+
+use Drupal\rdf_entity\Entity\Rdf;
+
+/**
+ * Entity class for 'rdf_graph' RDF entities.
+ */
+class RdfGraph extends Rdf implements RdfGraphInterface {
+
+}
