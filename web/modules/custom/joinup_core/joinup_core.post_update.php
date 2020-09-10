9375979f2e395e75c3042873efb6eb64a5d0bcfa
Pieter Frenssen <pieter@frenssen.be>
2020-09-10 19:17:11 +0300

    ISAICP-6098: Provide an upgrade path.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -8,6 +8,9 @@
 declare(strict_types = 1);
 
 use Drupal\Core\Database\Database;
+use Drupal\joinup_community_content\CommunityContentHelper;
+use Drupal\joinup_community_content\Entity\CommunityContentInterface;
+use Drupal\joinup_group\Entity\PinnableGroupContentInterface;
 use Drupal\search_api\Plugin\search_api\datasource\ContentEntity;
 use EasyRdf\Graph;
 use EasyRdf\GraphStore;
@@ -47,4 +50,48 @@ function joinup_core_post_update_0106400(): void {
   $index = $entity_type_manager->getStorage('search_api_index')->load('published');
   $index->reindex();
   $index->indexItems(-1, 'entity:taxonomy_term');
+}
+/**
+ * Migrate data about pinned entities into meta entities.
+ */
+function joinup_core_post_update_0106401(): void {
+  $entity_type_manager = \Drupal::entityTypeManager();
+
+  $get_storage = function (string $entity_type_id) use ($entity_type_manager) {
+    return $entity_type_manager->getStorage($entity_type_id);
+  };
+
+  $entity_ids = [];
+
+  // Retrieve community content to migrate.
+  $entity_ids['node'] = $get_storage('node')
+    ->getQuery()
+    ->condition('type', CommunityContentHelper::BUNDLES, 'IN')
+    ->condition('sticky', TRUE)
+    ->execute();
+
+  // Retrieve solutions to migrate.
+  $entity_ids['rdf_entity'] = $get_storage('rdf_entity')
+    ->getQuery()
+    ->condition('rid', 'solution')
+    ->exists('field_is_pinned_in')
+    ->execute();
+
+  foreach ($entity_ids as $entity_type_id => $ids) {
+    $storage = $get_storage($entity_type_id);
+    foreach ($storage->loadMultiple($ids) as $entity) {
+      if ($entity instanceof PinnableGroupContentInterface) {
+        if ($entity instanceof CommunityContentInterface) {
+          $entity->pin();
+        }
+        else {
+          /** @var \Drupal\joinup_group\Entity\GroupInterface[] $pinned_groups */
+          $pinned_groups = $entity->get('field_is_pinned_in')->referencedEntities();
+          foreach ($pinned_groups as $pinned_group) {
+            $entity->pin($pinned_group);
+          }
+        }
+      }
+    }
+  }
 }
