a348b23ad07ea3156f15a840d7d7a1daa7f7aa58
Pieter Frenssen <pieter@frenssen.be>
2020-09-16 17:32:24 +0300

    ISAICP-6143: Provide a method to return the parent category of a legal type.

--- web/modules/custom/joinup_licence/src/Entity/LicenceLegalTypeInterface.php
+++ web/modules/custom/joinup_licence/src/Entity/LicenceLegalTypeInterface.php
@@ -11,4 +11,15 @@ use Drupal\taxonomy\TermInterface;
  */
 interface LicenceLegalTypeInterface extends TermInterface {
 
+  /**
+   * Returns the category to which the legal type belongs.
+   *
+   * These are the root level terms such as 'Can', 'Must', 'Cannot' etc.
+   *
+   * @return \Drupal\joinup_licence\Entity\LicenceLegalTypeInterface|null
+   *   The root level category, or NULL if the term doesn't have a root level
+   *   category (e.g. because it is a root level category itself).
+   */
+  public function getCategory(): ?LicenceLegalTypeInterface;
+
 }
