a5674b80e78b3c89e52c52b886f2660e57f34fb6
Claudiu Cristea <clau.cristea@gmail.com>
2020-09-19 18:22:08 +0300

    ISAICP-6197: Redirects are automatically created on alias change.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -7,7 +7,6 @@
 
 declare(strict_types = 1);
 
-use Drupal\redirect\Entity\Redirect;
 /**
  * Remove path alias duplicates.
  */
@@ -55,53 +54,27 @@ function joinup_core_post_update_0106401(?array &$sandbox = NULL): string {
 /**
  * Update aliases for entities with the old alias.
  */
-function joinup_core_post_update_0106401(&$sandbox): string {
+function joinup_core_post_update_0106402(?array &$sandbox = NULL): string {
   $rdf_storage = \Drupal::entityTypeManager()->getStorage('rdf_entity');
   if (empty($sandbox['entity_ids'])) {
     $sandbox['entity_ids'] = $rdf_storage->getQuery()->execute();
     $sandbox['count'] = 0;
-    $sandbox['updated'] = 0;
-    $sandbox['redirects'] = 0;
     $sandbox['max'] = count($sandbox['entity_ids']);
   }
 
-  $entity_ids = array_splice($sandbox['entity_ids'], 0, 50);
+  $entity_ids = array_splice($sandbox['entity_ids'], 0, 100);
 
-  $alias_manager = \Drupal::getContainer()->get('path_alias.manager');
   $alias_generator = \Drupal::getContainer()->get('pathauto.generator');
-  $redirect_storage = \Drupal::entityTypeManager()->getStorage('redirect');
   foreach ($rdf_storage->loadMultiple($entity_ids) as $entity) {
     // Update aliases for the entity's default language and its translations.
     foreach ($entity->getTranslationLanguages() as $langcode => $language) {
-      $old_alias = $alias_manager->getAliasByPath($entity->toUrl()->toString());
       /** @var \Drupal\Core\Entity\TranslatableInterface $translated_entity */
       $translated_entity = $entity->getTranslation($langcode);
-      $result = $alias_generator->createEntityAlias($translated_entity, 'bulkupdate');
-      if ($result) {
-        $sandbox['updated']++;
-        if ($old_alias === $result['alias']) {
-          continue;
-        }
-
-        $redirects = $redirect_storage->loadByProperties([
-          'redirect_source__path' => $old_alias,
-          'redirect_redirect__uri' => 'internal:' . $result['alias'],
-          'language' => $translated_entity->language(),
-        ]);
-        if (empty($redirects)) {
-          Redirect::create([
-            'redirect_source' => $old_alias,
-            'redirect_redirect' => 'internal:' . $result['alias'],
-            'language' => $translated_entity->language(),
-            'status_code' => '301',
-          ])->save();
-          $sandbox['redirects']++;
-        }
-      }
+      $alias_generator->createEntityAlias($translated_entity, 'bulkupdate');
     }
   }
 
   $sandbox['count'] += count($entity_ids);
   $sandbox['#finished'] = (int) empty($sandbox['entity_ids']);
-  return "Processed {$sandbox['count']}/{$sandbox['max']}. {$sandbox['updated']} were updated. {$sandbox['redirects']} redirects were created.";
+  return "Processed {$sandbox['count']}/{$sandbox['max']}";
 }
