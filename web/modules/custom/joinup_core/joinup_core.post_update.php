6cfb9f27997d1b6b78659bf22e517f024ae62adb
Claudiu Cristea <clau.cristea@gmail.com>
2020-09-19 18:21:17 +0300

    ISAICP-6197: Remove path alias duplicates.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -8,6 +8,49 @@
 declare(strict_types = 1);
 
 use Drupal\redirect\Entity\Redirect;
+/**
+ * Remove path alias duplicates.
+ */
+function joinup_core_post_update_0106401(?array &$sandbox = NULL): string {
+  $db = \Drupal::database();
+  if (!isset($sandbox['duplicate_pids'])) {
+    // Get all duplicate path alias IDs.
+    $sandbox['duplicate_pids'] = $db->query("SELECT p.id
+    FROM {path_alias} p
+    LEFT JOIN (
+      -- This sub-query returns all alias duplicates of English aliases.
+      SELECT
+        MAX(id) AS valid_id,
+        COUNT(*) AS duplicates_count,
+        path
+      FROM {path_alias}
+      WHERE langcode = 'en'
+      GROUP BY path
+      HAVING duplicates_count > 1
+    ) valid_aliases ON p.path = valid_aliases.path
+    WHERE valid_aliases.valid_id IS NOT NULL
+    AND p.id <> valid_aliases.valid_id
+    -- Only select English aliases.
+    AND p.langcode = 'en'")->fetchCol();
+    $sandbox['progress'] = 0;
+    $sandbox['total'] = count($sandbox['duplicate_pids']);
+  }
+
+  $to_delete = array_splice($sandbox['duplicate_pids'], 0, 1000);
+  $db->delete('path_alias_revision')
+    ->condition('id', $to_delete, 'IN')
+    ->execute();
+  $db->delete('path_alias')
+    ->condition('id', $to_delete, 'IN')
+    ->execute();
+  $sandbox['progress'] += count($to_delete);
+
+  if ($sandbox['#finished'] = (int) empty($sandbox['duplicate_pids'])) {
+    \Drupal::entityTypeManager()->getStorage('path_alias')->resetCache();
+  }
+
+  return "Removed {$sandbox['progress']}/{$sandbox['total']}";
+}
 
 /**
  * Update aliases for entities with the old alias.
