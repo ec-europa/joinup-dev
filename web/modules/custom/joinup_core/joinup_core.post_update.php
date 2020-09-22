2d85e2557e851429e66f6da141f3a387798c897a
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-09-22 22:11:30 +0300

    ISAICP-6197: Include nodes. Update parents first. Exclude problematic entities. Update, don't create.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -66,26 +66,49 @@ function joinup_core_post_update_0106401(?array &$sandbox = NULL): string {
  */
 function joinup_core_post_update_0106402(?array &$sandbox = NULL): string {
   $rdf_storage = \Drupal::entityTypeManager()->getStorage('rdf_entity');
+  $node_storage = \Drupal::entityTypeManager()->getStorage('node');
   if (empty($sandbox['entity_ids'])) {
-    $sandbox['entity_ids'] = $rdf_storage->getQuery()->execute();
+    $sandbox['entity_ids']['rdf_entity'] = $rdf_storage->getQuery()->condition('rid', 'collection')->execute();
+    $sandbox['entity_ids']['rdf_entity'] += $rdf_storage->getQuery()->condition('rid', 'solution')->execute();
+    $sandbox['entity_ids']['rdf_entity'] += $rdf_storage->getQuery()->condition('rid', 'asset_release')->execute();
+    $sandbox['entity_ids']['rdf_entity'] += $rdf_storage->getQuery()->condition('rid', [
+      'collection',
+      'solution',
+      'asset_release',
+    ], 'NOT IN')->execute();
+    $sandbox['entity_ids']['node'] = [];
     $sandbox['count'] = 0;
-    $sandbox['max'] = count($sandbox['entity_ids']);
+    $sandbox['max'] = count($sandbox['entity_ids']['rdf_entity']) + count($sandbox['entity_ids']['node']);
   }
 
-  $entity_ids = array_splice($sandbox['entity_ids'], 0, 100);
+  if (empty($sandbox['entity_ids']['rdf_entity'])) {
+    $storage = $node_storage;
+    $entity_ids = array_splice($sandbox['entity_ids']['node'], 0, 100);
+  }
+  else {
+    $storage = $rdf_storage;
+    $entity_ids = array_splice($sandbox['entity_ids']['rdf_entity'], 0, 100);
+  }
 
   $alias_generator = \Drupal::getContainer()->get('pathauto.generator');
-  foreach ($rdf_storage->loadMultiple($entity_ids) as $entity) {
+  foreach ($storage->loadMultiple($entity_ids) as $entity) {
+    if ($entity->bundle() === 'asset_release' && $entity->field_isr_release_number->isEmpty()) {
+      // There are a number of releases that do not have a release number, even
+      // though it is a mandatory field. Aliases fail to be created for these
+      // entities. These will be handled in ISAICP-6217.
+      // @see https://citnet.tech.ec.europa.eu/CITnet/jira/browse/ISAICP-6217
+      continue;
+    }
     // Update aliases for the entity's default language and its translations.
     foreach ($entity->getTranslationLanguages() as $langcode => $language) {
       /** @var \Drupal\Core\Entity\TranslatableInterface $translated_entity */
       $translated_entity = $entity->getTranslation($langcode);
-      $alias_generator->createEntityAlias($translated_entity, 'bulkupdate');
+      $alias_generator->updateEntityAlias($translated_entity, 'bulkupdate');
     }
   }
 
   $sandbox['count'] += count($entity_ids);
-  $sandbox['#finished'] = (int) empty($sandbox['entity_ids']);
+  $sandbox['#finished'] = (int) (empty($sandbox['entity_ids']['rdf_entity']) && empty($sandbox['entity_ids']['node']));
   return "Processed {$sandbox['count']}/{$sandbox['max']}";
 }
 
