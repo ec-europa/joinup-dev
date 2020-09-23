9da6fbbccf168b0a5e4400869bf7399c469e878d
Claudiu Cristea <clau.cristea@gmail.com>
2020-09-23 11:01:26 +0300

    ISAICP-6197: Don't re-number already merged post updates.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -13,9 +13,46 @@ use EasyRdf\Graph;
 use EasyRdf\GraphStore;
 
 /**
+ * Re-insert the new EIF vocabularies to apply new changes.
+ */
+function joinup_core_post_update_0106401(): void {
+  $vids = [
+    'eif_conceptual_model',
+    'eif_interoperability_layer',
+    'eif_principle',
+    'eif_recommendation',
+  ];
+
+  $sparql_connection = Database::getConnection('default', 'sparql_default');
+  $connection_options = $sparql_connection->getConnectionOptions();
+  $connect_string = "http://{$connection_options['host']}:{$connection_options['port']}/sparql-graph-crud";
+  $graph_store = new GraphStore($connect_string);
+  foreach ($vids as $vocabulary) {
+    $filepath = __DIR__ . "/../../../../resources/fixtures/{$vocabulary}.rdf";
+    $graph_uri = "http://{$vocabulary}";
+    $graph = new Graph($graph_uri);
+    $sparql_connection->update("CLEAR GRAPH <{$graph_uri}>");
+    $graph->parse(file_get_contents($filepath));
+    $graph_store->insert($graph);
+  }
+
+  $entity_type_manager = \Drupal::entityTypeManager();
+  $storage = $entity_type_manager->getStorage('taxonomy_term');
+  $tids = $storage->getQuery()->condition('vid', $vids, 'IN')->execute();
+  /** @var \Drupal\taxonomy\TermInterface $term */
+  foreach ($storage->loadMultiple($tids) as $term) {
+    ContentEntity::indexEntity($term);
+  }
+  /** @var \Drupal\search_api\IndexInterface $index */
+  $index = $entity_type_manager->getStorage('search_api_index')->load('published');
+  $index->reindex();
+  $index->indexItems(-1, 'entity:taxonomy_term');
+}
+
+/**
  * Remove path alias duplicates.
  */
-function joinup_core_post_update_0106401(?array &$sandbox = NULL): string {
+function joinup_core_post_update_0106402(?array &$sandbox = NULL): string {
   $db = \Drupal::database();
   if (!isset($sandbox['duplicate_pids'])) {
     // Get all duplicate path alias IDs.
@@ -64,7 +101,7 @@ function joinup_core_post_update_0106401(?array &$sandbox = NULL): string {
 /**
  * Update aliases for entities with the old alias.
  */
-function joinup_core_post_update_0106402(?array &$sandbox = NULL): string {
+function joinup_core_post_update_0106403(?array &$sandbox = NULL): string {
   $rdf_storage = \Drupal::entityTypeManager()->getStorage('rdf_entity');
   $node_storage = \Drupal::entityTypeManager()->getStorage('node');
   if (empty($sandbox['entity_ids'])) {
@@ -110,41 +147,4 @@ function joinup_core_post_update_0106402(?array &$sandbox = NULL): string {
   $sandbox['count'] += count($entity_ids);
   $sandbox['#finished'] = (int) (empty($sandbox['entity_ids']['rdf_entity']) && empty($sandbox['entity_ids']['node']));
   return "Processed {$sandbox['count']}/{$sandbox['max']}";
-}
-
-/**
- * Re-insert the new EIF vocabularies to apply new changes.
- */
-function joinup_core_post_update_0106403(): void {
-  $vids = [
-    'eif_conceptual_model',
-    'eif_interoperability_layer',
-    'eif_principle',
-    'eif_recommendation',
-  ];
-
-  $sparql_connection = Database::getConnection('default', 'sparql_default');
-  $connection_options = $sparql_connection->getConnectionOptions();
-  $connect_string = "http://{$connection_options['host']}:{$connection_options['port']}/sparql-graph-crud";
-  $graph_store = new GraphStore($connect_string);
-  foreach ($vids as $vocabulary) {
-    $filepath = __DIR__ . "/../../../../resources/fixtures/{$vocabulary}.rdf";
-    $graph_uri = "http://{$vocabulary}";
-    $graph = new Graph($graph_uri);
-    $sparql_connection->update("CLEAR GRAPH <{$graph_uri}>");
-    $graph->parse(file_get_contents($filepath));
-    $graph_store->insert($graph);
-  }
-
-  $entity_type_manager = \Drupal::entityTypeManager();
-  $storage = $entity_type_manager->getStorage('taxonomy_term');
-  $tids = $storage->getQuery()->condition('vid', $vids, 'IN')->execute();
-  /** @var \Drupal\taxonomy\TermInterface $term */
-  foreach ($storage->loadMultiple($tids) as $term) {
-    ContentEntity::indexEntity($term);
-  }
-  /** @var \Drupal\search_api\IndexInterface $index */
-  $index = $entity_type_manager->getStorage('search_api_index')->load('published');
-  $index->reindex();
-  $index->indexItems(-1, 'entity:taxonomy_term');
 }
