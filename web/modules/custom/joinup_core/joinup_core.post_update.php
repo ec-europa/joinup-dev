ac75948e5cb2241cdccae04cbd010cfb9b10e89b
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-10-09 00:14:35 +0300

    ISAICP-6208: Clear leftover graphs and update entities without creation date.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -7,44 +7,65 @@
 
 declare(strict_types = 1);
 
-use Drupal\Core\Database\Database;
-use Drupal\search_api\Plugin\search_api\datasource\ContentEntity;
-use EasyRdf\Graph;
-use EasyRdf\GraphStore;
+/**
+ * Clean up the validation graphs.
+ */
+function joinup_core_post_update_0106500(array &$sandbox): void {
+  $query = <<<QUERY
+SELECT DISTINCT ?g 
+   WHERE { GRAPH ?g {?s ?p ?o} } 
+ORDER BY ?g
+QUERY;
+
+  $connection = \Drupal::getContainer()->get('sparql.endpoint');
+  $graphs = $connection->query($query);
+  foreach ($graphs as $graph) {
+    $uri = $graph->g->getUri();
+    if (strpos($uri, 'http://adms-validator/') === 0) {
+      $connection->query("CLEAR GRAPH <$uri>");
+    }
+  }
+}
 
 /**
- * Re-insert the new EIF vocabularies to apply new changes.
+ * Add creation time to entities solutions that lack it.
  */
-function joinup_core_post_update_0106401(): void {
-  $vids = [
-    'eif_conceptual_model',
-    'eif_interoperability_layer',
-    'eif_principle',
-    'eif_recommendation',
-  ];
+function joinup_core_post_update_0106501(array &$sandbox): void {
+  // Query the solutions without created date and their provenance records
+  // corresponding created date.
+  $query = <<<QUERY
+SELECT DISTINCT ?graph ?id ?created
+WHERE {
+  GRAPH ?graph {
+    ?id ?p ?o .
+    ?id a <http://www.w3.org/ns/dcat#Dataset>
+    FILTER NOT EXISTS {?id <http://purl.org/dc/terms/issued> ?created__value} .
+    FILTER NOT EXISTS {?id <http://purl.org/dc/terms/isVersionOf> ?field_isr_is_version_of__target_id}
+  }
+  ?provenance_id a <http://www.w3.org/ns/prov#Activity> .
+  ?provenance_id <http://purl.org/dc/terms/issued> ?created .
+  ?provenance_id <http://www.w3.org/ns/prov#generated> ?id
+}
+QUERY;
 
-  $sparql_connection = Database::getConnection('default', 'sparql_default');
-  $connection_options = $sparql_connection->getConnectionOptions();
-  $connect_string = "http://{$connection_options['host']}:{$connection_options['port']}/sparql-graph-crud";
-  $graph_store = new GraphStore($connect_string);
-  foreach ($vids as $vocabulary) {
-    $filepath = __DIR__ . "/../../../../resources/fixtures/{$vocabulary}.rdf";
-    $graph_uri = "http://{$vocabulary}";
-    $graph = new Graph($graph_uri);
-    $sparql_connection->update("CLEAR GRAPH <{$graph_uri}>");
-    $graph->parse(file_get_contents($filepath));
-    $graph_store->insert($graph);
+  $database = \Drupal::getContainer()->get('sparql.endpoint');
+  $results = $database->query($query);
+  $ids_to_clear = [];
+
+  foreach ($results as $result) {
+    $graph = $result->graph->getUri();
+    $id = $result->id->getUri();
+    $ids_to_clear[] = $id;
+    $created = $result->created->toRdfPhp();
+    $value = $created['value'];
+    $type = $created['datatype'];
+
+    $insert_query = <<<QUERY
+WITH <{$graph}>
+INSERT { <$id> <http://purl.org/dc/terms/issued> "{$value}"^^<{$type}> }
+QUERY;
+    $database->query($insert_query);
   }
 
-  $entity_type_manager = \Drupal::entityTypeManager();
-  $storage = $entity_type_manager->getStorage('taxonomy_term');
-  $tids = $storage->getQuery()->condition('vid', $vids, 'IN')->execute();
-  /** @var \Drupal\taxonomy\TermInterface $term */
-  foreach ($storage->loadMultiple($tids) as $term) {
-    ContentEntity::indexEntity($term);
-  }
-  /** @var \Drupal\search_api\IndexInterface $index */
-  $index = $entity_type_manager->getStorage('search_api_index')->load('published');
-  $index->reindex();
-  $index->indexItems(-1, 'entity:taxonomy_term');
+  \Drupal::entityTypeManager()->getStorage('rdf_entity')->resetCache($ids_to_clear);
 }
