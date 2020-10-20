e76fa9a47136b4f177d8c3822ef1d1bf54fd45b0
Ilias Dimopoulos <idimopoulos@hotmail.com>
2020-10-20 13:40:53 +0300

    ISAICP-5911: Move the publication date to the document entity.

--- web/modules/custom/joinup_document/src/Entity/Document.php
+++ web/modules/custom/joinup_document/src/Entity/Document.php
@@ -4,11 +4,28 @@ declare(strict_types = 1);
 
 namespace Drupal\joinup_document\Entity;
 
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\joinup_community_content\Entity\CommunityContentBase;
 
 /**
  * Entity subclass for the 'document' bundle.
  */
 class Document extends CommunityContentBase implements DocumentInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPublicationDate(): ?int {
+    $publication_date_item_list = $this->get('field_document_publication_date');
+    if (($publication_date_item_list instanceof FieldItemListInterface) && !$publication_date_item_list->isEmpty() && $value = $publication_date_item_list->first()->value) {
+      return strtotime($value);
+    }
+
+    if ($this->get('published_at')->isEmpty() || empty($this->get('published_at')->value) || $this->get('published_at')->value == PUBLICATION_DATE_DEFAULT) {
+      return NULL;
+    }
+
+    return $this->get('published_at')->value;
+  }
 
 }
