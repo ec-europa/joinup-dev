ba361ef734fa52a5b5612b6ca0dcd9f82ebb3e4a
Pieter Frenssen <pieter@frenssen.be>
2020-10-27 17:02:29 +0200

    ISAICP-6257: Update documentation.

--- web/modules/custom/joinup_core/src/LocalTaskLinksHelperInterface.php
+++ web/modules/custom/joinup_core/src/LocalTaskLinksHelperInterface.php
@@ -8,6 +8,14 @@ use Drupal\Core\Access\AccessResultInterface;
 
 /**
  * Provides an interface for 'joinup_core.local_task_links_helper' service.
+ *
+ * Local tasks can be displayed in two ways:
+ * - As horizontal tabs: for example see user/login and user/password.
+ * - As a three dots menu: for example a collection page viewed as facilitator.
+ *
+ * By default the local tasks are displayed as a three dots menu. If a route
+ * needs to show them as horizontal tasks, this can be achieved by adding the
+ * route to the `block.block.horizontal_tabs` block.
  */
 interface LocalTaskLinksHelperInterface {
 
