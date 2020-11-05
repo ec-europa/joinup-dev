c5844927a2712e70361f1d0ccc703f5019edb5e7
Pieter Frenssen <pieter@frenssen.be>
2020-11-05 16:17:56 +0200

    ISAICP-6099: Transform the BooleanFieldToggleController into a dedicated controller for featuring content.
    We are no longer manipulating field values directly but instead are exposing an
    API to feature and unfeature content. The original controller is no longer used
    so it is repurposed.

--- web/modules/custom/collection/src/Entity/Collection.php
+++ web/modules/custom/collection/src/Entity/Collection.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\collection\Entity;
 
 use Drupal\joinup_bundle_class\JoinupBundleClassFieldAccessTrait;
+use Drupal\joinup_bundle_class\JoinupBundleClassMetaEntityTrait;
 use Drupal\joinup_bundle_class\ShortIdTrait;
 use Drupal\joinup_featured\FeaturedContentTrait;
 use Drupal\joinup_group\Entity\GroupTrait;
@@ -20,6 +21,7 @@ class Collection extends Rdf implements CollectionInterface {
   use FeaturedContentTrait;
   use GroupTrait;
   use JoinupBundleClassFieldAccessTrait;
+  use JoinupBundleClassMetaEntityTrait;
   use ShortIdTrait;
 
   /**
--- web/modules/custom/joinup_featured/src/FeaturedContentTrait.php
+++ web/modules/custom/joinup_featured/src/FeaturedContentTrait.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace Drupal\joinup_featured;
 
+use Drupal\meta_entity\Entity\MetaEntityInterface;
+
 /**
  * Reusable methods for entities that can be featured site wide.
  *
@@ -16,14 +18,33 @@ trait FeaturedContentTrait {
    * {@inheritdoc}
    */
   public function isFeatured(): bool {
-    return (bool) $this->getMainPropertyValue('field_site_featured');
+    $meta_entity = $this->getMetaEntity('featured');
+    if (!$meta_entity instanceof MetaEntityInterface) {
+      return FALSE;
+    }
+
+    /** @var \Drupal\Core\Field\FieldItemListInterface $item_list */
+    $item_list = $meta_entity->get('field_featured');
+
+    if ($item_list->isEmpty()) {
+      return FALSE;
+    }
+
+    return (bool) $item_list->value;
   }
 
   /**
    * {@inheritdoc}
    */
   public function feature(): FeaturedContentInterface {
-    $this->set('field_site_featured', TRUE);
+    // Only update the featured status in the database if needed.
+    if (!$this->isFeatured()) {
+      $meta_entity = $this->getMetaEntity('featured');
+      if (empty($meta_entity)) {
+        $meta_entity = $this->createMetaEntity('featured');
+      }
+      $meta_entity->set('field_featured', TRUE)->save();
+    }
 
     return $this;
   }
@@ -32,7 +53,11 @@ trait FeaturedContentTrait {
    * {@inheritdoc}
    */
   public function unfeature(): FeaturedContentInterface {
-    $this->set('field_site_featured', FALSE);
+    $meta_entity = $this->getMetaEntity('featured');
+
+    if (!empty($meta_entity)) {
+      $meta_entity->delete();
+    }
 
     return $this;
   }
