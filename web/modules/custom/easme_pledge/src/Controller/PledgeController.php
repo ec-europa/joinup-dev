140c41e5bad7cab7f2e3582c37a2bf8d9b1ec0bf
Siggy <cainethanatos@gmail.com>
2020-11-24 14:11:39 +0100

    EIC/feature/SMEIMKT-3035: refactoring of the pledge module, regarding implementations changes of the CommunityContentController.

--- web/modules/custom/easme_pledge/src/Controller/PledgeController.php
+++ web/modules/custom/easme_pledge/src/Controller/PledgeController.php
@@ -6,8 +6,10 @@ namespace Drupal\easme_pledge\Controller;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResultInterface;
 use Drupal\joinup_community_content\Controller\CommunityContentController;
 use Drupal\rdf_entity\RdfInterface;
+use Drupal\node\NodeTypeInterface;
 
 /**
  * Controller that handles the form to add pledge entities to a solution.
@@ -28,17 +30,15 @@ class PledgeController extends CommunityContentController {
    *
    * @param \Drupal\rdf_entity\RdfInterface $rdf_entity
    *   The RDF entity for which the document entity is created.
+   * @param \Drupal\node\NodeTypeInterface $node_type
+   *   The type of node to be added.
    * @param \Drupal\Core\Session\AccountInterface|null $account
    *   The account to check access for. The current user will be used if NULL.
    *
    * @return \Drupal\Core\Access\AccessResult
    *   The access result object.
    */
-  public function createAccess(RdfInterface $rdf_entity, ?AccountInterface $account = NULL): AccessResult {
-    if (empty($account)) {
-      $account = $this->currentUser();
-    }
-
+  public function createAccess(RdfInterface $rdf_entity, NodeTypeInterface $node_type, AccountInterface $account): AccessResultInterface {
     return AccessResult::allowedIf($rdf_entity->bundle() === 'solution' && $account->isAuthenticated());
   }
 
