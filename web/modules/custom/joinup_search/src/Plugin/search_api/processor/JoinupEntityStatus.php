2d8c09994592a1e4eb1521c0e49b41c9b9a33aa7
Pieter Frenssen <pieter@frenssen.be>
2020-12-09 13:16:08 +0200

    ISAICP-6320: Account for the fact that group content can be orphaned.
    This can happen for example when entities are being cleaned up at the end
    of a test.

--- web/modules/custom/joinup_search/src/Plugin/search_api/processor/JoinupEntityStatus.php
+++ web/modules/custom/joinup_search/src/Plugin/search_api/processor/JoinupEntityStatus.php
@@ -4,10 +4,12 @@ declare(strict_types = 1);
 
 namespace Drupal\joinup_search\Plugin\search_api\processor;
 
+use Drupal\Core\Entity\EntityPublishedInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\PluginFormInterface;
 use Drupal\joinup_group\Entity\GroupContentInterface;
+use Drupal\joinup_group\Exception\MissingGroupException;
 use Drupal\node\NodeInterface;
 use Drupal\rdf_entity\RdfInterface;
 use Drupal\search_api\IndexInterface;
@@ -120,11 +122,18 @@ class JoinupEntityStatus extends ProcessorPluginBase implements PluginFormInterf
           // on the copy that is present in $object->og_audience->entity since
           // this might be stale. This ensures that if the parent has been
           // published in this request we will act on the actual updated state.
-          $parent_id = $object instanceof GroupContentInterface ? $object->getGroup()->id() : NULL;
-          if (!empty($parent_id)) {
-            $parent = $rdf_storage->load($parent_id);
-            if (!empty($parent) && $parent->isPublished()) {
-              $enabled = TRUE;
+          if ($object instanceof GroupContentInterface) {
+            $parent_id = NULL;
+            try {
+              $parent_id = $object->getGroupId();
+            }
+            catch (MissingGroupException $e) {}
+
+            if (!empty($parent_id)) {
+              $parent = $rdf_storage->load($parent_id);
+              if ($parent instanceof EntityPublishedInterface && $parent->isPublished()) {
+                $enabled = TRUE;
+              }
             }
           }
         }
