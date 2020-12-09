9d5431d8289938cdb2031625dbbf2fd2ac667e79
Ludovic Ramarojaona <ludovic.ramarojaona@blue4you.be>
2020-12-09 16:59:37 +0000

    SMEIMKT-3017: create a helper class for Notifications and implement a signature for notifications

---
+++ web/modules/custom/easme_helper/src/Utility/NotificationsHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\easme_helper\Utility;
+
+use \Drupal\Core\Config\ConfigFactory;
+
+/**
+ * Provides helper functions for EASME project.
+ */
+class NotificationsHelper {
+
+  /**
+   * Drupal\Core\Config\ConfigFactory definition.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $configFactory;
+
+  /**
+   * Class constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactory $config_factory
+   *   An instance of ConfigFactoryInterface.
+   */
+  public function __construct(ConfigFactory $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Provides the general notifications' signature.
+   *
+   * @return string
+   *   The signature in HTML format.
+   */
+  public function getNotificationsSignature() {
+    $easme_helper_config = $this->configFactory->get('easme_helper.settings');
+    return t('<p>Kind regards,</p><p>The @site_name Team</p>', ['@site_name' => $easme_helper_config->get('notifications.site_name')]);
+  }
+
+}
