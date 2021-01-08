bb00e84845327a11ddacf4e82069b8f9a05e7f33
Pieter Frenssen <pieter@frenssen.be>
2021-01-08 15:47:56 +0200

    ISAICP-5806: Streamline the SolutionController.
    This class has reached a venerable age and was in need of some cleaning and
    minor surgeries.

--- web/modules/custom/solution/src/Controller/SolutionController.php
+++ web/modules/custom/solution/src/Controller/SolutionController.php
@@ -5,10 +5,11 @@ declare(strict_types = 1);
 namespace Drupal\solution\Controller;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\collection\Entity\CollectionInterface;
-use Drupal\og\Og;
 use Drupal\rdf_entity\RdfInterface;
+use Drupal\solution\Entity\SolutionInterface;
 
 /**
  * Controller providing the form to add a new solution inside a collection.
@@ -22,62 +23,56 @@ class SolutionController extends ControllerBase {
    * that include the rdf_entity id in the url so that the og audience field
    * is auto completed.
    *
-   * @param \Drupal\rdf_entity\RdfInterface $rdf_entity
-   *   The collection rdf_entity.
+   * @param \Drupal\collection\Entity\CollectionInterface $collection
+   *   The collection.
    *
    * @return array
    *   Return the form array to be rendered.
    */
-  public function add(RdfInterface $rdf_entity) {
-    $solution = $this->createNewSolution($rdf_entity);
+  public function add(CollectionInterface $collection): array {
+    $solution = $this->createNewSolution($collection);
 
     // Pass the collection to the form state so that the parent connection is
     // established.
     // @see solution_add_form_parent_submit()
-    $form = $this->entityFormBuilder()->getForm($solution, 'default', ['collection' => $rdf_entity->id()]);
-    return $form;
+    return $this->entityFormBuilder()->getForm($solution, 'default', ['collection' => $collection->id()]);
   }
 
   /**
    * Handles access to the solution add form through collection pages.
    *
-   * @param \Drupal\rdf_entity\RdfInterface $rdf_entity
-   *   The RDF entity for which the solution is created.
+   * @param \Drupal\rdf_entity\RdfInterface $collection
+   *   The collection in which the solution is created.
    *
-   * @return \Drupal\Core\Access\AccessResult
+   * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result object.
    */
-  public function createSolutionAccess(RdfInterface $rdf_entity) {
+  public function createSolutionAccess(RdfInterface $collection): AccessResultInterface {
     // If the collection is archived, content creation is not allowed.
-    if ($rdf_entity instanceof CollectionInterface && $rdf_entity->getWorkflowState() === 'archived') {
+    if (!$collection instanceof CollectionInterface || $collection->getWorkflowState() === 'archived') {
       return AccessResult::forbidden();
-    }
-
-    $user = $this->currentUser();
-    if (empty($rdf_entity) && !$user->isAnonymous()) {
-      return AccessResult::neutral();
     }
 
     // Users with 'administer organic groups' permission should have access
     // since this page can only be called from within a group.
+    $user = $this->currentUser();
     if ($user->hasPermission('administer organic groups')) {
       return AccessResult::allowed();
     }
 
-    $membership = Og::getMembership($rdf_entity, $user);
-    return (!empty($membership) && $membership->hasPermission('create solution rdf_entity')) ? AccessResult::allowed() : AccessResult::forbidden();
+    return $collection->hasGroupPermission((int) $user->id(), 'create solution rdf_entity') ? AccessResult::allowed() : AccessResult::forbidden();
   }
 
   /**
    * Creates a new solution entity that is affiliated with the given collection.
    *
-   * @param \Drupal\rdf_entity\RdfInterface $collection
+   * @param \Drupal\collection\Entity\CollectionInterface $collection
    *   The collection to affiliate with the new solution.
    *
-   * @return \Drupal\Core\Entity\EntityInterface
+   * @return \Drupal\solution\Entity\SolutionInterface
    *   The unsaved solution entity.
    */
-  protected function createNewSolution(RdfInterface $collection) {
+  protected function createNewSolution(CollectionInterface $collection): SolutionInterface {
     return $this->entityTypeManager()->getStorage('rdf_entity')->create([
       'rid' => 'solution',
       'collection' => [$collection->id()],
