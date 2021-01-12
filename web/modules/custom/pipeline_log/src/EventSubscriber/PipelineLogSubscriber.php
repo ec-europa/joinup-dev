b52c96f052725946e500fc2316d3afd714f9123a
Ilias Dimopoulos <idimopoulos@hotmail.com>
2021-01-12 19:07:16 +0200

    ISAICP-5867: Subscribe to the pipeline completion event to log last execution time.

---
+++ web/modules/custom/pipeline_log/src/EventSubscriber/PipelineLogSubscriber.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\pipeline_log\EventSubscriber;
+
+use Drupal\Component\Datetime\Time;
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Drupal\joinup_federation\Event\PipelineCompleteEvent;
+use Drupal\joinup_federation\JoinupFederationEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Pipeline logging event subscriber.
+ *
+ * The sole purpose of this subscriber is to note down when was the last time
+ * each pipeline was ran.
+ */
+class PipelineLogSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The key value factory service.
+   *
+   * @var \Drupal\Core\KeyValueStore\KeyValueFactoryInterface
+   */
+  protected $keyValue;
+
+  /**
+   * The time service.
+   *
+   * @var \Drupal\Component\Datetime\Time
+   */
+  protected $time;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param \Drupal\Core\KeyValueStore\KeyValueFactoryInterface $keyValue
+   *   The key value factory service.
+   * @param \Drupal\Component\Datetime\Time $time
+   *   The time service.
+   */
+  public function __construct(KeyValueFactoryInterface $keyValue, Time $time) {
+    $this->keyValue = $keyValue;
+    $this->time = $time;
+  }
+
+  /**
+   * Acts on pipeline completion.
+   *
+   * @param \Drupal\joinup_federation\Event\PipelineCompleteEvent $event
+   *   The pipeline event.
+   */
+  public function onPipelineComplete(PipelineCompleteEvent $event): void {
+    if (!$event->isSuccess()) {
+      // We do not care about failed attempts.
+      return;
+    }
+    $pipeline_id = $event->getPipeline()->getPluginId();
+    $collection = $this->keyValue->get('pipeline_log');
+    $collection->set($pipeline_id, $this->time->getRequestTime());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      JoinupFederationEvents::PIPELINE_COMPLETE => ['onPipelineComplete'],
+    ];
+  }
+
+}
