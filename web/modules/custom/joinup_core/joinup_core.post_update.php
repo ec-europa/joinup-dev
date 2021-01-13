7ef2ba547d8a172a675f76744d1f964a978df4c0
Ilias Dimopoulos <idimopoulos@hotmail.com>
2021-01-13 17:35:07 +0200

    SMEIMKT-3060: Fix php codesniffs.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -6,8 +6,3 @@
  */
 
 declare(strict_types = 1);
-
-use Drupal\Core\Database\Database;
-use Drupal\search_api\Plugin\search_api\datasource\ContentEntity;
-use EasyRdf\Graph;
-use EasyRdf\GraphStore;
