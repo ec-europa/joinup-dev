a13002e714a6875bfc006c09d24b0956786700cb
Pieter Frenssen <pieter@frenssen.be>
2021-01-18 15:33:28 +0200

    ISAICP-6332: Use correct hook name for deploy script.
    It appears this was moved from the post-update to the deploy file during a merge
    but the hook name change was not applied.

--- web/modules/custom/joinup_core/joinup_core.deploy.php
+++ web/modules/custom/joinup_core/joinup_core.deploy.php
@@ -65,7 +65,7 @@ function joinup_core_deploy_0106700(&$sandbox): TranslatableMarkup {
 /**
  * Fix the EIF recommendation menu link route.
  */
-function joinup_core_post_update_0106701(): void {
+function joinup_core_deploy_0106701(): void {
   \Drupal::entityTypeManager()->getStorage('menu_link_content')->load(11390)
     ->set('link', 'route:view.eif_recommendation.all;rdf_entity=http_e_f_fdata_ceuropa_ceu_fw21_f405d8980_b3f06_b4494_bb34a_b46c388a38651')
     ->save();
