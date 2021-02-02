c8fffe11986cdb52db72e0a468605fc5283ae786
Pieter Frenssen <pieter@frenssen.be>
2021-02-02 19:33:31 +0200

    ISAICP-6348: Implement a trait that will return the created date in lieu of the publication date.
    Use it in the collection entity as a proof of concept.

--- web/modules/custom/collection/src/Entity/Collection.php
+++ web/modules/custom/collection/src/Entity/Collection.php
@@ -10,6 +10,7 @@ use Drupal\joinup_bundle_class\LogoTrait;
 use Drupal\joinup_bundle_class\ShortIdTrait;
 use Drupal\joinup_featured\FeaturedContentTrait;
 use Drupal\joinup_group\Entity\GroupTrait;
+use Drupal\joinup_publication_date\Entity\EntityPublicationDateFallbackTrait;
 use Drupal\joinup_workflow\ArchivableEntityTrait;
 use Drupal\joinup_workflow\EntityWorkflowStateTrait;
 use Drupal\rdf_entity\Entity\Rdf;
@@ -20,6 +21,7 @@ use Drupal\rdf_entity\Entity\Rdf;
 class Collection extends Rdf implements CollectionInterface {
 
   use ArchivableEntityTrait;
+  use EntityPublicationDateFallbackTrait;
   use EntityWorkflowStateTrait;
   use FeaturedContentTrait;
   use GroupTrait;
