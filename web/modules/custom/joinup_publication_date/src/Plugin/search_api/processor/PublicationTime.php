3bd32e4ed329136e5ab9d762e92e07c079f44d12
Pieter Frenssen <pieter@frenssen.be>
2021-02-03 20:55:22 +0200

    ISAICP-6348: Implement a Search API processor that exposes the publication time.

---
+++ web/modules/custom/joinup_publication_date/src/Plugin/search_api/processor/PublicationTime.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_publication_date\Plugin\search_api\processor;
+
+use Drupal\joinup_publication_date\Entity\EntityPublicationTimeInterface;
+use Drupal\search_api\Datasource\DatasourceInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Processor\ProcessorProperty;
+
+/**
+ * Adds the timestamp indicating when the entity was first published.
+ *
+ * This works for all entities that implement EntityPublicationTimeInterface.
+ *
+ * @SearchApiProcessor(
+ *   id = "publication_time",
+ *   label = @Translation("Publication time"),
+ *   description = @Translation("The timestamp indicating when the entity was first published."),
+ *   stages = {
+ *     "add_properties" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = true,
+ * )
+ */
+class PublicationTime extends ProcessorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
+    $properties = [];
+
+    if (!$datasource) {
+      $definition = [
+        'label' => $this->t('Publication timestamp'),
+        'description' => $this->t('The timestamp indicating when the entity was first published.'),
+        'type' => 'integer',
+        'processor_id' => $this->getPluginId(),
+      ];
+      $properties['publication_time'] = new ProcessorProperty($definition);
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+    $entity = $item->getOriginalObject();
+    if ($entity instanceof EntityPublicationTimeInterface) {
+      $publication_time = $entity->getPublicationTime();
+      if ($publication_time) {
+        $index_fields = $this->index->getFields();
+        $fields = $this->getFieldsHelper()->filterForPropertyPath($index_fields, NULL, 'publication_time');
+        foreach ($fields as $field) {
+          $field->addValue($publication_time);
+        }
+      }
+    }
+  }
+
+}
