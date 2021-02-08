77ca79db7b06c9275de0312596312abdace5443f
Pieter Frenssen <pieter@frenssen.be>
2021-02-08 22:10:23 +0200

    ISAICP-6361: Create bundle classes for statistics meta entities.

---
+++ web/modules/custom/joinup_stats/src/Entity/DownloadCount.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\joinup_stats\Entity;
+
+/**
+ * Bundle class for meta entities that track download counts.
+ */
+class DownloadCount extends StatisticBase implements VisitCountInterface {
+}
