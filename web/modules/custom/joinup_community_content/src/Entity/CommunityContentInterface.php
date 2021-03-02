4862ee479cc71aac242662fecf64ffb7cab64a59
Claudiu Cristea <clau.cristea@gmail.com>
2021-03-02 13:19:08 +0200

    ISAICP-6329: Collections & solutions no more outdate-able.

--- web/modules/custom/joinup_community_content/src/Entity/CommunityContentInterface.php
+++ web/modules/custom/joinup_community_content/src/Entity/CommunityContentInterface.php
@@ -8,7 +8,6 @@ use Drupal\collection\Entity\CollectionContentInterface;
 use Drupal\joinup_core\Entity\OutdatedContentInterface;
 use Drupal\joinup_featured\FeaturedContentInterface;
 use Drupal\joinup_group\Entity\PinnableGroupContentInterface;
-use Drupal\joinup_publication_date\Entity\EntityPublicationTimeInterface;
 use Drupal\joinup_stats\Entity\VisitCountAwareInterface;
 use Drupal\joinup_workflow\EntityWorkflowStateInterface;
 use Drupal\node\NodeInterface;
@@ -16,5 +15,5 @@ use Drupal\node\NodeInterface;
 /**
  * Interface for community content entities.
  */
-interface CommunityContentInterface extends NodeInterface, EntityPublicationTimeInterface, FeaturedContentInterface, PinnableGroupContentInterface, CollectionContentInterface, EntityWorkflowStateInterface, VisitCountAwareInterface, OutdatedContentInterface {
+interface CommunityContentInterface extends NodeInterface, FeaturedContentInterface, PinnableGroupContentInterface, CollectionContentInterface, EntityWorkflowStateInterface, OutdatedContentInterface, VisitCountAwareInterface {
 }
--- web/modules/custom/joinup_core/tests/src/ExistingSite/OutdatedContentTest.php
+++ web/modules/custom/joinup_core/tests/src/ExistingSite/OutdatedContentTest.php
@@ -29,8 +29,6 @@ class OutdatedContentTest extends JoinupExistingSiteTestBase {
    */
   public function testOutdatedContent(): void {
     \Drupal::configFactory()->getEditable('joinup_core.outdated_content_threshold')
-      ->set('rdf_entity.collection', 10)
-      ->set('rdf_entity.solution', 7)
       ->set('node.discussion', 3)
       ->set('node.document', 3)
       ->set('node.event', NULL)
@@ -42,15 +40,7 @@ class OutdatedContentTest extends JoinupExistingSiteTestBase {
     /** @var \Drupal\collection\Entity\CollectionInterface $collection */
     $collection = $this->createRdfEntity([
       'rid' => 'collection',
-      'created' => strtotime('-4 years', $this->now),
       'field_ar_state' => 'validated',
-    ]);
-    /** @var \Drupal\solution\Entity\SolutionInterface $solution */
-    $solution = $this->createRdfEntity([
-      'rid' => 'solution',
-      'collection' => $collection,
-      'created' => strtotime('-19 years', $this->now),
-      'field_is_state' => 'validated',
     ]);
     /** @var \Drupal\joinup_discussion\Entity\DiscussionInterface $discussion */
     $discussion = $this->createNode([
@@ -77,10 +67,6 @@ class OutdatedContentTest extends JoinupExistingSiteTestBase {
       'published_at' => strtotime('-5 years', $this->now),
     ]);
 
-    // The collection is not outdated.
-    $this->assertIsNotOutdated($collection);
-    // The solution is outdated.
-    $this->assertIsOutdated($solution);
     // The discussion is outdated.
     $this->assertIsOutdated($discussion);
     // The document is not outdated.
