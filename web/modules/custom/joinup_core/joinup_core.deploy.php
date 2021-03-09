3741a98b444b83662d08fc36aa1ed0fe212f0fa3
Ilias Dimopoulos <idimopoulos@hotmail.com>
2021-03-09 15:23:47 +0200

    ISAICP-5963: Fix the datatype of the integer values.

--- web/modules/custom/joinup_core/joinup_core.deploy.php
+++ web/modules/custom/joinup_core/joinup_core.deploy.php
@@ -13,3 +13,29 @@
  */
 
 declare(strict_types = 1);
+
+/**
+ * Fix the datatype of the owner ID in owners and contact information.
+ */
+function joinup_core_deploy_0106900(array &$sandbox): void {
+  $database = \Drupal::getContainer()->get('sparql.endpoint');
+  $variables = [
+    'http://joinup.eu/owner/published' => 'http://joinup.eu/owner/uid',
+    'http://joinup.eu/contact_information/published' => 'http://joinup.eu/contact_information/uid',
+  ];
+
+  foreach ($variables as $graph => $predicate) {
+    $query = <<<QUERY
+WITH <{$graph}>
+DELETE { ?owner <{$predicate}> ?value }
+INSERT { ?owner <{$predicate}> ?new_value }
+WHERE {
+ ?owner <{$predicate}> ?value .
+ FILTER (datatype(?value) = <integer>) .
+ BIND(STRDT(STR(?value), xsd:integer) AS ?new_value)
+}
+QUERY;
+
+    $database->query($query);
+  }
+}
