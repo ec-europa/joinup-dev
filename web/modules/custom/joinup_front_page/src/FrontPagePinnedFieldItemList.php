26b1dbe4e1c42e82fc9823e9efed1518bc745366
Pieter Frenssen <pieter@frenssen.be>
2021-03-22 23:51:20 +0200

    ISAICP-6274: Only retrieve pinnable status from pinnable entities.

--- web/modules/custom/joinup_front_page/src/FrontPagePinnedFieldItemList.php
+++ web/modules/custom/joinup_front_page/src/FrontPagePinnedFieldItemList.php
@@ -6,6 +6,7 @@ namespace Drupal\joinup_front_page;
 
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\TypedData\ComputedItemListTrait;
+use Drupal\joinup_front_page\Entity\PinnableToFrontpageInterface;
 
 /**
  * Computed field that returns if the entity is pinned on the front page.
@@ -19,7 +20,9 @@ class FrontPagePinnedFieldItemList extends FieldItemList {
    */
   protected function computeValue(): void {
     if (!isset($this->list[0])) {
-      $this->list[0] = $this->createItem(0, (int) $this->getEntity()->isPinnedToFrontpage());
+      $entity = $this->getEntity();
+      $value = $entity instanceof PinnableToFrontpageInterface ? (int) $entity->isPinnedToFrontPage() : 0;
+      $this->list[0] = $this->createItem(0, $value);
     }
   }
 
