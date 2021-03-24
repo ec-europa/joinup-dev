46c4bafd0ac8b59ebaf67016f4a1abaa6e045878
Pieter Frenssen <pieter@frenssen.be>
2021-03-24 22:29:14 +0200

    ISAICP-6194: Provide a more descriptive interface when we know we are dealing with discussions.

--- web/modules/custom/joinup_discussion/src/Event/DiscussionEventBase.php
+++ web/modules/custom/joinup_discussion/src/Event/DiscussionEventBase.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\joinup_discussion\Event;
 
-use Drupal\node\NodeInterface;
+use Drupal\joinup_discussion\Entity\DiscussionInterface;
 use Symfony\Component\EventDispatcher\Event;
 
 /**
@@ -15,27 +15,27 @@ abstract class DiscussionEventBase extends Event {
   /**
    * The discussion node.
    *
-   * @var \Drupal\node\NodeInterface
+   * @var \Drupal\joinup_discussion\Entity\DiscussionInterface
    */
   protected $node;
 
   /**
    * Creates a new discussion event object.
    *
-   * @param \Drupal\node\NodeInterface $node
+   * @param \Drupal\joinup_discussion\Entity\DiscussionInterface $node
    *   The discussion node subject of event.
    */
-  public function __construct(NodeInterface $node) {
+  public function __construct(DiscussionInterface $node) {
     $this->node = $node;
   }
 
   /**
    * Returns the discussion node.
    *
-   * @return \Drupal\node\NodeInterface
+   * @return \Drupal\joinup_discussion\Entity\DiscussionInterface
    *   The discussion node.
    */
-  public function getNode(): NodeInterface {
+  public function getNode(): DiscussionInterface {
     return $this->node;
   }
 
