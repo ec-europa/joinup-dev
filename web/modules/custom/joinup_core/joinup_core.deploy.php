33073bfef432513261f665875db5295ae5ec3d33
Pieter Frenssen <pieter@frenssen.be>
2021-03-31 22:43:14 +0300

    ISAICP-6122: Transform existing collection abstracts to use basic HTML.

--- web/modules/custom/joinup_core/joinup_core.deploy.php
+++ web/modules/custom/joinup_core/joinup_core.deploy.php
@@ -13,3 +13,32 @@
  */
 
 declare(strict_types = 1);
+
+/**
+ * Switch the filter format of the collection abstract to basic HTML.
+ */
+function joinup_core_deploy_0107000(array &$sandbox): string {
+  $storage = \Drupal::entityTypeManager()->getStorage('rdf_entity');
+
+  if (!isset($sandbox['total'])) {
+    $query = $storage->getQuery()
+      ->condition('rid', 'collection')
+      ->exists('field_ar_abstract');
+    $sandbox['ids'] = array_values($query->execute());
+    $sandbox['total'] = count($sandbox['ids']);
+    $sandbox['processed'] = 0;
+  }
+
+  $ids = array_splice($sandbox['ids'], 0, 19);
+  /** @var \Drupal\collection\Entity\CollectionInterface[] $collections */
+  $collections = $storage->loadMultiple($ids);
+  foreach ($collections as $collection) {
+    $collection->field_ar_abstract->format = 'basic_html';
+    $collection->save();
+
+  }
+  $sandbox['processed'] += count($ids);
+  $sandbox['#finished'] = empty($sandbox['ids']) ? 1 : $sandbox['processed'] / $sandbox['total'];
+
+  return "Processed {$sandbox['processed']} out of {$sandbox['total']}";
+}
