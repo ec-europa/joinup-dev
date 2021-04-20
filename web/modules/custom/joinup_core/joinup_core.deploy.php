e4be61774c7414753cb50f03a714b952abc7e455
Ilias Dimopoulos <idimopoulos@hotmail.com>
2021-04-20 19:25:09 +0300

    ISAICP-6431: Update the URIs of the topic terms.

--- web/modules/custom/joinup_core/joinup_core.deploy.php
+++ web/modules/custom/joinup_core/joinup_core.deploy.php
@@ -21,6 +21,7 @@ use Drupal\asset_distribution\Entity\DownloadEvent;
  */
 function joinup_core_deploy_0107000(&$sandbox) {
   $schema = \Drupal::database()->schema();
+  $database = \Drupal::database();
   $type = [
     'type' => 'varchar',
     'length' => 128,
@@ -34,6 +35,7 @@ function joinup_core_deploy_0107000(&$sandbox) {
   // to perform the changes because the command to change the field name differs
   // even from mariaDB to MySQL.
   $schema->changeField('node__field_topic', 'field_policy_domain_target_id', 'field_topic_target_id', $type);
+
   $schema->dropTable('node_revision__field_topic');
   $schema->renameTable('node_revision__field_topic_backup', 'node_revision__field_topic');
   $schema->changeField('node_revision__field_topic', 'field_policy_domain_target_id', 'field_topic_target_id', $type);
@@ -56,6 +58,49 @@ INSERT { GRAPH ?g { ?entity <http://joinup.eu/vocabulary/topic> ?value } }
 WHERE { GRAPH ?g { ?entity <http://policy_domain> ?value } }
 QUERY;
   $sparql_endpoint->query($query);
+
+  // Change the policy-domain term in the ID of the topics.
+  // The "http://joinup.eu/ontology/policy-domain" contains 39 characters. So
+  // We start from character 40 but we subtract only 39 characters from the
+  // length due because the characters start at 0.
+  $query = <<<QUERY
+WITH <http://topic>
+DELETE { ?oldUri ?p ?o }
+INSERT { ?newUri ?p ?o }
+WHERE {
+  ?oldUri ?p ?o .
+  BIND(IRI(CONCAT("http://joinup.eu/ontology/topic", SUBSTR(STR(?oldUri), 40, STRLEN(STR(?oldUri)) - 39))) as ?newUri)
+}
+QUERY;
+  $sparql_endpoint->query($query);
+
+  // Update all references to the policy domain terms.
+  $query = <<<QUERY
+DELETE { GRAPH ?g { ?s ?p ?oldUri } }
+INSERT { GRAPH ?g { ?s ?p ?newUri } }
+WHERE {
+  GRAPH ?g {
+    ?s ?p ?oldUri .
+    FILTER isIri(?oldUri) .
+    FILTER (CONTAINS(STR(?oldUri), "http://joinup.eu/ontology/policy-domain")) .
+    BIND(IRI(CONCAT("http://joinup.eu/ontology/topic", SUBSTR(STR(?oldUri), 40, STRLEN(STR(?oldUri)) - 39))) as ?newUri)
+  }
+}
+QUERY;
+  $sparql_endpoint->query($query);
+
+  $tables = [
+    'node__field_topic',
+    'node_revision__field_topic',
+  ];
+  foreach ($tables as $table) {
+    $query = <<<QUERY
+UPDATE {{$table}}
+SET `field_topic_target_id` =
+    REPLACE(field_topic_target_id, 'http://joinup.eu/ontology/policy-domain', 'http://joinup.eu/ontology/topic');
+QUERY;
+    $database->query($query);
+  }
 }
 
 /**
