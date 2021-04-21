3054e0beb9df946f3711c48c24cbaa942ac06b68
Ilias Dimopoulos <idimopoulos@hotmail.com>
2021-04-21 08:56:55 +0300

    ISAICP-6379: Provide a simple test for indexing hierarchy.

---
+++ web/modules/custom/joinup_core/tests/src/ExistingSite/TopicHierarchyIndexingTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\joinup_core\ExistingSite;
+
+use Drupal\Tests\joinup_test\ExistingSite\JoinupExistingSiteTestBase;
+use Drupal\Tests\rdf_entity\Traits\DrupalTestTraits\RdfEntityCreationTrait;
+use Drupal\Tests\taxonomy\Traits\TaxonomyTestTrait;
+use Drupal\search_api\Entity\Index;
+use Drupal\taxonomy\Entity\Vocabulary;
+
+/**
+ * Tests hierarchy indexing for the aggregated field 'Topic'.
+ *
+ * @group joinup_core
+ */
+class TopicHierarchyIndexingTest extends JoinupExistingSiteTestBase {
+
+  use RdfEntityCreationTrait;
+  use TaxonomyTestTrait;
+
+  /**
+   * A list of terms used in the test.
+   *
+   * @var \Drupal\rdf_taxonomy\Entity\RdfTerm[]
+   */
+  protected $terms = [];
+
+  /**
+   * Tests indexing of parent terms for Topic aggregated field.
+   */
+  public function testParentTermsAggregatedIndexing(): void {
+    $topic_vocabulary = Vocabulary::load('topic');
+    $this->terms['parent'] = $this->coreCreateTerm($topic_vocabulary, [
+      'name' => 'Parent',
+    ]);
+    $this->terms['child'] = $this->coreCreateTerm($topic_vocabulary, [
+      'name' => 'Child',
+      'parent' => $this->terms['parent']->id(),
+    ]);
+    $collection = $this->createRdfEntity([
+      'rid' => 'collection',
+      'field_ar_state' => 'validated',
+      'field_topic' => $this->terms['child']->id(),
+    ]);
+    $index = Index::load('published');
+    $index->indexItems();
+
+    foreach ($this->terms as $term) {
+      $query = $index->query();
+      $query->addCondition('topic', $term->id());
+      $results = $query->execute()->getResultItems();
+      $this->assertCount(1, $results);
+      $item = reset($results);
+      $this->assertSame($item->getOriginalObject()->getEntity()->id(), $collection->id());
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tearDown(): void {
+    parent::tearDown();
+    foreach ($this->terms as $term) {
+      $term->delete();
+    }
+  }
+
+}
