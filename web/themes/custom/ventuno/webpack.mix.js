172382bc399d0d408daffed3f324a7197a40fdf1
Pieter Frenssen <pieter@frenssen.be>
2021-05-07 17:53:43 +0300

    ISAICP-6504: Migrate the Classy-based theme to Radix-based.

--- web/themes/custom/ventuno/webpack.mix.js
+++ web/themes/custom/ventuno/webpack.mix.js
@@ -7,7 +7,7 @@
  | for your application. See https://github.com/JeffreyWay/laravel-mix.
  |
  */
-const proxy = 'http://drupal.local';
+const proxy = 'http://joinup.local';
 const mix = require('laravel-mix');
 
 /*
