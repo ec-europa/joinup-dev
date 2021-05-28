4c43fc819ea7f129154a23c7fb54383b263db161
Pieter Frenssen <pieter@frenssen.be>
2021-05-28 13:59:56 +0300

    ISAICP-6476: Transform the EntityQueueBlock into a derivative block.

--- web/modules/custom/entityqueue_block/src/Plugin/Block/EntityQueueBlock.php
+++ web/modules/custom/entityqueue_block/src/Plugin/Block/EntityQueueBlock.php
@@ -17,10 +17,11 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @Block(
  *   id = "entityqueue_block",
  *   admin_label = @Translation("Entityqueue block"),
- *   category = @Translation("Entityqueue")
+ *   category = @Translation("Entityqueue"),
+ *   deriver = "Drupal\entityqueue_block\Plugin\Derivative\EntityQueueBlock"
  * )
  */
-class EntityqueueBlock extends BlockBase implements ContainerFactoryPluginInterface {
+class EntityQueueBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
    * The entity type manager.
@@ -37,7 +38,7 @@ class EntityqueueBlock extends BlockBase implements ContainerFactoryPluginInterf
   protected $entityDisplayRepository;
 
   /**
-   * Constructs a new EntityqueueBlock.
+   * Constructs a new EntityQueueBlock.
    *
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
@@ -77,7 +78,7 @@ class EntityqueueBlock extends BlockBase implements ContainerFactoryPluginInterf
     /** @var \Drupal\entityqueue\EntitySubqueueInterface $subqueue */
     $subqueue = $this->entityTypeManager
       ->getStorage('entity_subqueue')
-      ->load($this->configuration['entityqueue']);
+      ->load($this->getDerivativeId());
 
     /** @var \Drupal\entityqueue\EntitySubqueueItemsFieldItemList $item_list */
     $item_list = $subqueue->get('items');
@@ -94,7 +95,6 @@ class EntityqueueBlock extends BlockBase implements ContainerFactoryPluginInterf
    */
   public function defaultConfiguration() {
     return parent::defaultConfiguration() + [
-      'entityqueue' => NULL,
       'view_mode' => NULL,
     ];
   }
@@ -103,21 +103,11 @@ class EntityqueueBlock extends BlockBase implements ContainerFactoryPluginInterf
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildConfigurationForm($form, $form_state);
-    $form['entityqueue'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Entityqueue'),
-      '#default_value' => $this->configuration['entityqueue'],
-      '#required' => TRUE,
-    ];
+    /** @var \Drupal\entityqueue\EntityQueueInterface $queue */
+    $queue = $this->entityTypeManager->getStorage('entity_queue')->load($this->getDerivativeId());
+    $target_type = $queue->getTargetEntityTypeId();
 
-    /** @var \Drupal\entityqueue\EntityQueueInterface[] $queues */
-    $queues = $this->entityTypeManager->getStorage('entity_queue')->loadMultiple();
-    $target_types = [];
-    foreach ($queues as $queue) {
-      $form['entityqueue']['#options'][$queue->id()] = $queue->label();
-      $target_types[$queue->getTargetEntityTypeId()] = $queue->getTargetEntityTypeId();
-    }
+    $form = parent::buildConfigurationForm($form, $form_state);
 
     $form['view_mode'] = [
       '#type' => 'select',
@@ -127,15 +117,12 @@ class EntityqueueBlock extends BlockBase implements ContainerFactoryPluginInterf
     ];
 
     /** @var \Drupal\entityqueue\EntityQueueInterface[] $queues */
-    $viewmodes = array_intersect_key($this->entityDisplayRepository->getAllViewModes(), $target_types);
-    foreach ($viewmodes as $entity_viewmodes) {
-      foreach ($entity_viewmodes as $machine_name => $viewmode_info) {
-        $form['view_mode']['#options'][$machine_name] = $viewmode_info['label'];
-      }
+    $viewmodes = $this->entityDisplayRepository->getAllViewModes()[$target_type] ?? [];
+    foreach ($viewmodes as $machine_name => $viewmode_info) {
+      $form['view_mode']['#options'][$machine_name] = $viewmode_info['label'];
     }
 
     // Sort the dropdown options alphabetically to make them easier to navigate.
-    asort($form['entityqueue']['#options']);
     asort($form['view_mode']['#options']);
 
     return $form;
@@ -147,7 +134,6 @@ class EntityqueueBlock extends BlockBase implements ContainerFactoryPluginInterf
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
 
-    $this->configuration['entityqueue'] = (string) $form_state->getValue('entityqueue');
     $this->configuration['view_mode'] = (string) $form_state->getValue('view_mode');
   }
 
