bd9b2dfc5c1a5fafc92a1c609b702fbffba8675f
Alexandre Dias <alex.jm.dias@gmail.com>
2021-08-11 17:38:54 +0100

    ISAICP-6468: Update custom pages if no filters or query is set in Content listing paragraph.

--- web/modules/custom/joinup_core/joinup_core.post_update.php
+++ web/modules/custom/joinup_core/joinup_core.post_update.php
@@ -14,3 +14,30 @@
  */
 
 declare(strict_types = 1);
+
+/**
+ * Update existing custom pages if no filters or query is set.
+ */
+function joinup_core_post_update_0107400(&$sandbox): void {
+  $nids = \Drupal::entityQuery('node')->condition('type', 'custom_page')->execute();
+  foreach ($nids as $nid) {
+    /** @var \Drupal\Core\Entity\ContentEntityBase $entity */
+    $entity = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+    if ($entity->hasField('field_paragraphs_body')) {
+      $elements = $entity->get('field_paragraphs_body');
+      for ($i = 0; $i < $elements->count(); $i++) {
+        $paragraph = \Drupal::entityTypeManager()->getStorage('paragraph')->load($elements->get($i)->target_id);
+        if ($paragraph->bundle() == 'content_listing') {
+          $values = $paragraph->field_content_listing->value;
+          if (empty($values['query_presets']) && !array_key_exists('query_builder', $values)) {
+            $elements->removeItem($i);
+            // Caution: decrement the counter as removeItem()
+            // also does a rekey().
+            $i--;
+          }
+        }
+      }
+      $entity->save();
+    }
+  }
+}
