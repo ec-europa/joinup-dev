4e278ab85e7837282e131e8e1e407de72c433f2e
gingapa <mail@paulogingao.com>
2021-08-26 15:26:59 +0100

    ISAICP-6595: Add swipe functionality to Explore block.

--- web/themes/ventuno/bcl-builder.config.js
+++ web/themes/ventuno/bcl-builder.config.js
@@ -8,6 +8,26 @@ const nodeModules = "./node_modules";
 const includePaths = [nodeModules];
 
 module.exports = {
+  scripts: [
+    {
+      entry: path.resolve(outputFolder, "src/js/splide-config.js"),
+      dest: path.resolve(outputFolder, "assets/js/splide-config.js"),
+      options: {
+        name: "splide",
+        minify: true,
+        sourceMap: true,
+        minifyOptions: {
+          mangle: true,
+          format: {
+            comments: /^!/,
+          },
+          compress: {
+            passes: 2,
+          },
+        },
+      },
+    },
+  ],
   styles: [
     {
       entry: path.resolve(outputFolder, "src/scss/styles.scss"),
@@ -27,6 +47,26 @@ module.exports = {
         ),
       ],
       to: path.resolve(outputFolder, "assets/icons"),
+      options: { up: true },
+    },
+    {
+      from: [
+        path.resolve(
+          nodeModules,
+          "@splidejs/splide/dist/css/splide.min.css"
+        ),
+      ],
+      to: path.resolve(outputFolder, "css"),
+      options: { up: true },
+    },
+    {
+      from: [
+        path.resolve(
+          nodeModules,
+          "@splidejs/splide/dist/js/splide.min.js"
+        ),
+      ],
+      to: path.resolve(outputFolder, "assets/js"),
       options: { up: true },
     },
   ],
---
+++ web/themes/ventuno/src/js/splide-config.js
@@ -0,0 +1,27 @@
+/**
+ * Splide.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.splide = {
+    attach: function (context) {
+      var elms = document.getElementsByClassName( 'splide' );
+      for ( var i = 0, len = elms.length; i < len; i++ ) {
+        new Splide( '.splide', {
+           type   : 'loop',
+           perPage: 3,
+           perMove: 1,
+           pagination: false,
+           breakpoints: {
+            980: {
+              perPage: 2,
+            },
+            640: {
+              perPage: 1,
+            },
+          }
+        } ).mount();
+      }
+    }
+  };
+})(jQuery, Drupal);
