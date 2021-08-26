ac9846947f740004728bab8e8eb72d6f5e9ef069
gingapa <mail@paulogingao.com>
2021-08-26 15:54:36 +0100

    ISAICP-6595: Small adjustment.

--- web/themes/ventuno/assets/js/splide-config.js
+++ web/themes/ventuno/assets/js/splide-config.js
@@ -8,7 +8,6 @@
       var elms = document.getElementsByClassName( 'splide' );
       for ( var i = 0, len = elms.length; i < len; i++ ) {
         new Splide( '.splide', {
-           type   : 'loop',
            perPage: 3,
            perMove: 1,
            pagination: false,
