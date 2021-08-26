282824c68be25a610f1917f4616afe7d364975cd
Theo Suja <theo.suja@twtspain.com>
2021-08-26 18:27:06 +0200

    ISAICP-6595: Compile splide-config.js via NPM.

--- web/themes/ventuno/assets/js/splide-config.js
+++ web/themes/ventuno/assets/js/splide-config.js
@@ -1,19 +1,35 @@
-/**
- * Splide.
- */
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+}((function () { 'use strict';
 
-(function ($, Drupal) {
-  Drupal.behaviors.splide = {
-    attach: function (context) {
-      var elms = document.getElementsByClassName( 'splide' );
-      for ( var i = 0, len = elms.length; i < len; i++ ) {
-        new Splide( elms[i], {
-           type   : 'loop',
-           perPage: 4,
-           perMove: 1,
-           pagination: false,
-        } ).mount();
+  /**
+   * Splide.
+   */
+  (function ($, Drupal) {
+    Drupal.behaviors.splide = {
+      attach: function (context) {
+        var elms = document.getElementsByClassName('splide');
+
+        for (var i = 0, len = elms.length; i < len; i++) {
+          new Splide('.splide', {
+            type: 'loop',
+            perPage: 4,
+            perMove: 1,
+            pagination: false,
+            breakpoints: {
+              980: {
+                perPage: 2
+              },
+              640: {
+                perPage: 1
+              }
+            }
+          }).mount();
+        }
       }
-    }
-  };
-})(jQuery, Drupal);
+    };
+  })(jQuery, Drupal);
+
+})));
+//# sourceMappingURL=splide-config.js.map
