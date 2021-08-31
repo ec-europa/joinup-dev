0bcdaf257a67ba7629810f215f3ed6e8f2d6cbd2
gingapa <mail@paulogingao.com>
2021-08-31 15:37:46 +0100

    ISAICP-6595: Add Slick library to Explore block (Work in progress).

--- web/themes/ventuno/src/js/slick-config.js
+++ web/themes/ventuno/src/js/slick-config.js
@@ -1,64 +1,54 @@
-(function (factory) {
-  typeof define === 'function' && define.amd ? define(factory) :
-  factory();
-}((function () { 'use strict';
-
-  /**
-   * Splide.
-   */
-  (function ($, Drupal) {
-    Drupal.behaviors.splide = {
-      attach: function (context) {
-
-        var elms = document.getElementsByClassName('explore-slider');
-        for (var i = 0, len = elms.length; i < len; i++) {
-          $(elms[i]).slick({
-            infinite: true,
-            speed: 300,
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            responsive: [
-              {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 4,
-                  slidesToScroll: 1,
-                  infinite: true,
-                  dots: true
-                }
-              },
-              {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 1
-                }
-              },
-              {
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 1
-                }
+/**
+ * Slick.
+ */
+(function ($, Drupal) {
+  Drupal.behaviors.splide = {
+    attach: function (context) {
+      var elms = document.getElementsByClassName('explore-slider');
+      for (var i = 0, len = elms.length; i < len; i++) {
+        $(elms[i]).slick({
+          infinite: false,
+          speed: 300,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
               }
-              // You can unslick at a given breakpoint now by adding:
-              // settings: "unslick"
-              // instead of a settings object
-            ]
-          });
-        };
-
-        // Refresh carousels position on tabs click.
-        // This is a workaround to get correct size and positions on slides even on hidden elements.
-        $(function(){
-          $(".explore-block .nav-link").click(function(){
-            $('.explore-slider').slick('setPosition');
-          });  
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
         });
+      };
+      // Refresh carousels position on tabs click.
+      // This is a workaround to get correct size and positions on slides even on hidden elements.
+      $(function(){
+        $(".explore-block .nav-link").click(function(){
+          $('.explore-slider').slick('setPosition');
+        });  
+      });
 
-      }
-    };
-  })(jQuery, Drupal);
-
-})));
-//# sourceMappingURL=splide-config.js.map
+    }
+  };
+})(jQuery, Drupal);
