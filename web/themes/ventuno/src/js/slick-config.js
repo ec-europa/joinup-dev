3937a6e7d991f0c26cb9169abd55bf084db4044c
Theo Suja <theo.suja@twtspain.com>
2021-08-31 18:23:07 +0200

    ISAICP-6595: WIP implementation of new carousel.

--- web/themes/ventuno/src/js/slick-config.js
+++ web/themes/ventuno/src/js/slick-config.js
@@ -7,31 +7,39 @@
       var elms = document.getElementsByClassName('explore-slider');
       for (var i = 0, len = elms.length; i < len; i++) {
         $(elms[i]).slick({
-          infinite: false,
+          infinite: true,
           speed: 300,
           slidesToShow: 5,
           slidesToScroll: 1,
+          arrows: true,
+          prevArrow: '<button type="button" class="slick-controls slick-next btn btn-secondary rounded-circle"><span>Previous</span></button>',
+          nextArrow: '<button type="button" class="slick-controls slick-prev btn btn-secondary rounded-circle"><span>Next</span></button>',
           responsive: [
             {
-              breakpoint: 1024,
+              breakpoint: 1200,
               settings: {
                 slidesToShow: 4,
                 slidesToScroll: 1,
-                infinite: true,
-                dots: true
               }
             },
             {
-              breakpoint: 600,
+              breakpoint: 1024,
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 768,
               settings: {
                 slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
                 slidesToScroll: 1
               }
             }
