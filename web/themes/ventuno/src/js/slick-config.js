63e550c55b1b0e5f0105de63f461e80e55d043f4
Pieter Frenssen <pieter@frenssen.be>
2021-09-03 16:57:14 +0300

    ISAICP-6595: Find a good balance between 'finger' and 'mouse' swiping.
    On screen sizes typical for mobile devices we always scroll the number of
    visible items. This gives a good balance between the feeling of inertia present
    on touch enabled devices, while still feeling natural when using the mouse on a
    desktop device.
    
    On the largest screen size we are scrolling to the actual swiped content item,
    because in this case 5 items are shown out of a total of 12. If we would try to
    scroll 5 items this becomes an unnatural, because scrolling the third time
    would only scroll items 11 + 12 into view, which is a very short scroll
    distance.
    
    This seems to be a limitation or possibly a configuration problem in the Slick
    JS library. Even though endless scrolling is enabled, when reaching the end of
    the list it will initiate a scroll to the last (12th) item, instead of
    respecting the configured scroll distance, and scrolling past the repeat point.
    
    Scrolling 2 items on a very wide screen feels abrupt and unnatural. Possible
    solutions are to either show 6 items, fix the looping problem in Slick JS, or
    scrolling to the actual swiped content item. The last option is the easiest
    solution, but has the drawback that it doesn't feel fluid when finger-swiping
    on a touch enabled device. This is acceptable though since this screen size
    is very uncommon on mobile devices.

--- web/themes/ventuno/src/js/slick-config.js
+++ web/themes/ventuno/src/js/slick-config.js
@@ -10,7 +10,7 @@
           infinite: true,
           speed: 300,
           slidesToShow: 5,
-          slidesToScroll: 1,
+          swipeToSlide: true,
           arrows: true,
           appendArrows:  $(elms[i]).prev().prev('.append-buttons'),
           nextArrow: '<button type="button" class="slick-controls slick-next rounded-circle"><span class="icon">&nbsp;</span><span class="visually-hidden">Next</span></button>',
@@ -20,28 +20,32 @@
               breakpoint: 1399,
               settings: {
                 slidesToShow: 4,
-                slidesToScroll: 1,
+                slidesToScroll: 4,
+                swipeToSlide: false
               }
             },
             {
               breakpoint: 1199,
               settings: {
                 slidesToShow: 3,
-                slidesToScroll: 1
+                slidesToScroll: 3,
+                swipeToSlide: false
               }
             },
             {
               breakpoint: 991,
               settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1
+                slidesToScroll: 2,
+                swipeToSlide: false
               }
             },
             {
               breakpoint: 767,
               settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                swipeToSlide: false
               }
             },
           ]
@@ -52,7 +56,7 @@
       $(function(){
         $(".explore-block .nav-link").click(function(){
           $('.explore-slider').slick('setPosition');
-        });  
+        });
       });
 
     }
