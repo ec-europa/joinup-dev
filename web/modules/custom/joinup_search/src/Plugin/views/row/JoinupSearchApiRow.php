93492901f72918cad71e6e6f54b85faf92d3c223
Alexandre Dias <alex.jm.dias@gmail.com>
2021-10-26 09:20:04 +0100

    ISAICP-6576: Extend search_api class with JoinupSearchApiRow.

--- web/modules/custom/joinup_search/src/Plugin/views/row/JoinupSearchApiRow.php
+++ web/modules/custom/joinup_search/src/Plugin/views/row/JoinupSearchApiRow.php
@@ -2,13 +2,13 @@
 
 declare(strict_types = 1);
 
-namespace Drupal\joinup_seach\Plugin\views\row;
+namespace Drupal\joinup_search\Plugin\views\row;
 
 use Drupal\search_api\Plugin\views\row\SearchApiRow;
 use Drupal\search_api\SearchApiException;
 
 /**
- * Provides a row plugin for displaying a result as a rendered item.
+ * Provides a row plugin for displaying a result as a search result item.
  *
  * @ViewsRow(
  *   id = "joinup_search_api",
@@ -24,16 +24,22 @@ class JoinupSearchApiRow extends SearchApiRow {
    * {@inheritdoc}
    */
   public function render($row) {
-    parent::render($row);
+    $parent = parent::render($row);
 
-    $datasource_id = $row->search_api_datasource;
-    $view_mode = 'search_result_featured';
-    try {
-      return $this->index->getDatasource($datasource_id)->viewItem($row->_object, $view_mode);
-    }
-    catch (SearchApiException $e) {
-      $this->logException($e);
-      return '';
+    if ($this->view->id() == 'search') {
+      $datasource_id = $row->search_api_datasource;
+      if ($row->index <= 2 && $this->view->getCurrentPage() <= 0) {
+        $view_mode = 'search_result_featured';
+        try {
+          return $this->index->getDatasource($datasource_id)->viewItem($row->_object, $view_mode);
+        }
+        catch (SearchApiException $e) {
+          $this->logException($e);
+          return '';
+        }
+      }
+
+      return $parent;
     }
   }
 
